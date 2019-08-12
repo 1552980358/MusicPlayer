8a2c720a7c3e856e71d4dd920ee122a6cc8e6331
HuLi <1552980358@qq.com>
2019-08-12 22:35:38 +0800

    Add picture at main according to time

--- app/src/main/java/app/skynight/musicplayer/fragment/activity_main/MainFragment.kt
+++ app/src/main/java/app/skynight/musicplayer/fragment/activity_main/MainFragment.kt
@@ -1,10 +1,10 @@
 package app.skynight.musicplayer.fragment.activity_main
 
+import android.graphics.BitmapFactory
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import app.skynight.musicplayer.util.log
 import app.skynight.musicplayer.R
@@ -28,18 +28,26 @@ class MainFragment : Fragment() {
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        log("MainFragment", "- onViewCreated")
         super.onViewCreated(view, savedInstanceState)
-        /*when (SimpleDateFormat("HH", Locale.getDefault()).format(Date(System.currentTimeMillis())).toInt()) {
-
-            12, 13, 14, 18, 19, 20 -> {
-                imageView_status.setImageDrawable(ContextCompat.getDrawable(activity!!, R.drawable.ic_eatting))
+        try {
+            when (SimpleDateFormat("HH", Locale.getDefault()).format(Date(System.currentTimeMillis())).toInt()) {
+                6,7,8, 12, 13, 14, 18, 19, 20 -> {
+                    textView_status.text = "~ 每一天的营养从好好把自己次了开始 ~"
+                    imageView_status.setImageBitmap(BitmapFactory.decodeStream(context!!.assets.open("ic_eating.jpg")))
+                }
+                22, 23, 24, 0, 1, 2, 3, 4, 5-> {
+                    textView_status.text = "~ 去睡觉觉咯 ~"
+                    imageView_status.setImageBitmap(BitmapFactory.decodeStream(context!!.assets.open("ic_sleep.jpg")))
+                }
+                else -> {
+                    textView_status.text = "~ 早睡晚起身体好 ~"
+                    imageView_status.setImageBitmap(BitmapFactory.decodeStream(context!!.assets.open("ic_touch_fish.jpg")))
+                }
             }
-            6,7,8 -> {
-                imageView_status
-            }
-            else -> {
-            }
-        }*/
+        } catch (e: Exception) {
+            //
+        }
     }
 
     override fun onStart() {
