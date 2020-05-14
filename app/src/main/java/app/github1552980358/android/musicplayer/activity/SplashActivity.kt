12c059651808ac7ecc38a3fd74622597b121cdbe
1552980358 <1552980358@qq.com>
2020-05-14 22:32:57 +0800

    SplashActivity.kt: Update icon

--- app/src/main/java/app/github1552980358/android/musicplayer/activity/SplashActivity.kt
+++ app/src/main/java/app/github1552980358/android/musicplayer/activity/SplashActivity.kt
@@ -3,12 +3,15 @@ package app.github1552980358.android.musicplayer.activity
 import android.Manifest
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.graphics.Color
 import android.os.Bundle
 import android.os.Handler
 import android.util.Log
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import app.github1552980358.android.musicplayer.R
 import app.github1552980358.android.musicplayer.base.AudioData
 import app.github1552980358.android.musicplayer.base.Constant.Companion.AudioDataDir
 import app.github1552980358.android.musicplayer.base.Constant.Companion.AudioDataListFile
@@ -38,8 +41,18 @@ class SplashActivity : AppCompatActivity() {
     )
     
     override fun onCreate(savedInstanceState: Bundle?) {
+        window.decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+            or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+            or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+            or View.SYSTEM_UI_FLAG_LIGHT_NAVIGATION_BAR
+            or View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR)
+    
+        window.navigationBarColor = Color.TRANSPARENT
+        window.statusBarColor = Color.TRANSPARENT
+        
         super.onCreate(savedInstanceState)
         Log.e("SplashActivity", "onCreate")
+        setContentView(R.layout.activity_splash)
         
         for (i in permissions) {
             if (ContextCompat.checkSelfPermission(this, i) == PackageManager.PERMISSION_GRANTED)
