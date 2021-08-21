94f7436c7b3eb414840fbde4af6a846ff7ab1bb3
1552980358 <1552980358@qq.com>
2021-08-21 20:45:26 +0800

    AudioInfo.kt: Update Album art bitmap loading

--- app/src/main/java/sakuraba/saki/player/music/service/util/AudioInfo.kt
+++ app/src/main/java/sakuraba/saki/player/music/service/util/AudioInfo.kt
@@ -1,6 +1,12 @@
 package sakuraba.saki.player.music.service.util
 
+import android.content.ContentUris
+import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.net.Uri
 import com.github.promeg.pinyinhelper.Pinyin
+import lib.github1552980358.ktExtension.jvm.keyword.tryOnly
 
 class AudioInfo(
     val audioId: String,
@@ -25,4 +31,24 @@ class AudioInfo(
             audioAlbumId,
             audioDuration
         )
+    
+    companion object {
+        private val URI = Uri.parse("content://media/external/audio/albumart")
+    }
+    
+    private var isFetched = false
+    private var bitmap: Bitmap? = null
+    
+    fun loadBitmap(context: Context) {
+        if (!isFetched) {
+            isFetched = !isFetched
+            tryOnly { bitmap = BitmapFactory.decodeFileDescriptor(context.contentResolver.openFileDescriptor(ContentUris.withAppendedId(URI, audioAlbumId), "r")?.fileDescriptor) }
+        }
+    }
+    
+    fun getBitmap(context: Context): Bitmap? {
+        loadBitmap(context)
+        return bitmap
+    }
+    
 }
