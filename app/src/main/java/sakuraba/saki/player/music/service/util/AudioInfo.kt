5ddf9a3ee0f0f57365cfda0c3fe60756eefa83e2
1552980358 <1552980358@qq.com>
2021-08-21 18:56:33 +0800

    Update AudioInfo.kt

---
+++ app/src/main/java/sakuraba/saki/player/music/service/util/AudioInfo.kt
@@ -0,0 +1,28 @@
+package sakuraba.saki.player.music.service.util
+
+import com.github.promeg.pinyinhelper.Pinyin
+
+class AudioInfo(
+    val audioId: String,
+    val audioTitle: String,
+    val audioTitlePinyin: String,
+    val audioArtist: String,
+    val audioArtistPinyin: String,
+    val audioAlbum: String,
+    val audioAlbumPinyin: String,
+    val audioAlbumId: Long,
+    val audioDuration: Long
+    ) {
+    constructor(audioId: String, audioTitle: String, audioArtist: String, audioAlbum: String, audioAlbumId: Long, audioDuration: Long):
+        this(
+            audioId,
+            audioTitle,
+            Pinyin.toPinyin(audioTitle, ""),
+            audioArtist,
+            Pinyin.toPinyin(audioArtist, ""),
+            audioAlbum,
+            Pinyin.toPinyin(audioAlbum, ""),
+            audioAlbumId,
+            audioDuration
+        )
+}
