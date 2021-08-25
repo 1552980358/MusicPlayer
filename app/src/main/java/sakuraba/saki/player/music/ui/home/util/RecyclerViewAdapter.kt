862cdeb87abcdd502d1472921e9225d336b0e62e
1552980358 <1552980358@qq.com>
2021-08-25 12:42:08 +0800

    RecyclerViewAdapter.kt: Add background click block

--- app/src/main/java/sakuraba/saki/player/music/ui/home/util/RecyclerViewAdapter.kt
+++ app/src/main/java/sakuraba/saki/player/music/ui/home/util/RecyclerViewAdapter.kt
@@ -4,6 +4,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
+import android.widget.RelativeLayout
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import sakuraba.saki.player.music.R
@@ -13,9 +14,10 @@ class ViewHolder(view: View): RecyclerView.ViewHolder(view) {
     val image: ImageView = view.findViewById(R.id.image_view)
     val title: TextView = view.findViewById(R.id.text_view_title)
     val summary: TextView = view.findViewById(R.id.text_view_summary)
+    val background: RelativeLayout = view.findViewById(R.id.relative_layout_root)
 }
 
-class RecyclerViewAdapter: RecyclerView.Adapter<ViewHolder>() {
+class RecyclerViewAdapter(private val selection: (pos: Int) -> Unit): RecyclerView.Adapter<ViewHolder>() {
     
     private var audioInfoList: List<AudioInfo>? = null
     
@@ -37,6 +39,7 @@ class RecyclerViewAdapter: RecyclerView.Adapter<ViewHolder>() {
                 holder.summary.text = "$audioArtist - $audioAlbum"
                 holder.image.setImageBitmap(getBitmap(holder.image.context))
             }
+            holder.background.setOnClickListener { selection(position) }
         }
         
     }
