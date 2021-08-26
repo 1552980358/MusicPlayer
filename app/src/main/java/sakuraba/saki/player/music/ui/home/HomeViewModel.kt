bf6deb21eff1914ffdf9a8e8c024573b1de0c023
1552980358 <1552980358@qq.com>
2021-08-26 20:13:16 +0800

    HomeViewModel.kt: Update required function and lambda

--- app/src/main/java/sakuraba/saki/player/music/ui/home/HomeViewModel.kt
+++ app/src/main/java/sakuraba/saki/player/music/ui/home/HomeViewModel.kt
@@ -1,7 +1,36 @@
 package sakuraba.saki.player.music.ui.home
 
+import android.graphics.Bitmap
+import android.view.View
+import android.view.ViewTreeObserver
+import androidx.coordinatorlayout.widget.CoordinatorLayout
+import androidx.fragment.app.FragmentActivity
 import androidx.lifecycle.ViewModel
+import com.google.android.material.snackbar.BaseTransientBottomBar
+import com.google.android.material.snackbar.Snackbar
+import lib.github1552980358.ktExtension.androidx.coordinatorlayout.widget.makeSnack
+import sakuraba.saki.player.music.R
+import sakuraba.saki.player.music.service.util.AudioInfo
 
 class HomeViewModel: ViewModel() {
-
+    
+    val audioInfoList = arrayListOf<AudioInfo>()
+    private var _snackbar: Snackbar? = null
+    fun initSnackBar(fragmentActivity: FragmentActivity, listener: View.OnClickListener) {
+        _snackbar = fragmentActivity.findViewById<CoordinatorLayout>(R.id.coordinator_layout)
+            ?.makeSnack(R.string.home_snack_open_setting_text, BaseTransientBottomBar.LENGTH_INDEFINITE)
+            ?.setAction(R.string.home_snack_open_setting_button, listener)?.apply {
+                view.viewTreeObserver.addOnPreDrawListener(object: ViewTreeObserver.OnPreDrawListener {
+                    override fun onPreDraw(): Boolean {
+                        snackbar.view.viewTreeObserver.removeOnPreDrawListener(this)
+                        (snackbar.view.layoutParams as CoordinatorLayout.LayoutParams).behavior = null
+                        return true
+                    }
+                })
+            }
+    }
+    val snackbar get() = _snackbar!!
+    
+    val bitmaps = mutableMapOf<Long, Bitmap?>()
+    
 }
