318ab8009ebb9dcc55a3a28f72e85cf78f986199
1552980358 <1552980358@qq.com>
2021-08-26 18:44:16 +0800

    PlayService.kt: Add Log.e() to each life state

--- app/src/main/java/sakuraba/saki/player/music/service/PlayService.kt
+++ app/src/main/java/sakuraba/saki/player/music/service/PlayService.kt
@@ -1,5 +1,6 @@
 package sakuraba.saki.player.music.service
 
+import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
 import android.support.v4.media.MediaBrowserCompat
@@ -12,6 +13,7 @@ import android.support.v4.media.session.PlaybackStateCompat.ACTION_SKIP_TO_NEXT
 import android.support.v4.media.session.PlaybackStateCompat.ACTION_SKIP_TO_PREVIOUS
 import android.support.v4.media.session.PlaybackStateCompat.ACTION_SKIP_TO_QUEUE_ITEM
 import android.support.v4.media.session.PlaybackStateCompat.STATE_NONE
+import android.util.Log
 import androidx.media.MediaBrowserServiceCompat
 import sakuraba.saki.player.music.BuildConfig
 
@@ -27,26 +29,35 @@ class PlayService: MediaBrowserServiceCompat() {
     private lateinit var mediaSession: MediaSessionCompat
     private var mediaSessionCallback = object : MediaSessionCompat.Callback() {
         override fun onPlay() {
+            Log.e(TAG, "onPlay")
         }
         override fun onPause() {
+            Log.e(TAG, "onPause")
         }
         override fun onSkipToPrevious() {
+            Log.e(TAG, "onSkipToPrevious")
         }
         override fun onSkipToNext() {
+            Log.e(TAG, "onSkipToNext")
         }
         override fun onSkipToQueueItem(id: Long) {
+            Log.e(TAG, "onSkipToQueueItem $id")
         }
         override fun onPlayFromUri(uri: Uri?, extras: Bundle?) {
+            Log.e(TAG, "onPlayFromUri $uri")
         }
         override fun onPlayFromMediaId(mediaId: String?, extras: Bundle?) {
+            Log.e(TAG, "onPlayFromMediaId $mediaId")
         }
         override fun onSeekTo(pos: Long) {
+            Log.e(TAG, "onSeekTo $pos")
         }
     }
     
     private lateinit var playbackStateCompat: PlaybackStateCompat
     
     override fun onCreate() {
+        Log.e(TAG, "onCreate")
         super.onCreate()
     
         playbackStateCompat = PlaybackStateCompat.Builder()
@@ -62,12 +73,18 @@ class PlayService: MediaBrowserServiceCompat() {
         sessionToken = mediaSession.sessionToken
     }
     
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        Log.e(TAG, "onStartCommand $flags $startId")
+        return super.onStartCommand(intent, flags, startId)
+    }
+    
     override fun onGetRoot(clientPackageName: String, clientUid: Int, rootHints: Bundle?) = when (clientPackageName) {
         BuildConfig.APPLICATION_ID -> BrowserRoot(TAG, null)
         else -> null
     }
     
     override fun onLoadChildren(parentId: String, result: Result<MutableList<MediaBrowserCompat.MediaItem>>) {
+        Log.e(TAG, "onLoadChildren $parentId")
         result.sendResult(null)
     }
     
