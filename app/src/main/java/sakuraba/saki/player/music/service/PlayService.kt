2d230458fc0240f7ca4bf6d5b811a32ab94dc013
1552980358 <1552980358@qq.com>
2021-08-27 12:28:07 +0800

    PlayService.kt: Implement mediaSessionCallback functions

--- app/src/main/java/sakuraba/saki/player/music/service/PlayService.kt
+++ app/src/main/java/sakuraba/saki/player/music/service/PlayService.kt
@@ -11,11 +11,17 @@ import android.support.v4.media.session.PlaybackStateCompat.ACTION_PLAY_PAUSE
 import android.support.v4.media.session.PlaybackStateCompat.ACTION_SEEK_TO
 import android.support.v4.media.session.PlaybackStateCompat.ACTION_SKIP_TO_NEXT
 import android.support.v4.media.session.PlaybackStateCompat.ACTION_SKIP_TO_PREVIOUS
-import android.support.v4.media.session.PlaybackStateCompat.ACTION_SKIP_TO_QUEUE_ITEM
+import android.support.v4.media.session.PlaybackStateCompat.STATE_BUFFERING
 import android.support.v4.media.session.PlaybackStateCompat.STATE_NONE
+import android.support.v4.media.session.PlaybackStateCompat.STATE_PAUSED
+import android.support.v4.media.session.PlaybackStateCompat.STATE_PLAYING
 import android.util.Log
 import androidx.media.MediaBrowserServiceCompat
 import sakuraba.saki.player.music.BuildConfig
+import sakuraba.saki.player.music.service.util.AudioInfo
+import sakuraba.saki.player.music.service.util.syncPlayAndPrepareMediaId
+import sakuraba.saki.player.music.util.Constants.EXTRAS_AUDIO_INFO_LIST
+import sakuraba.saki.player.music.util.Constants.EXTRAS_AUDIO_INFO_POS
 
 class PlayService: MediaBrowserServiceCompat() {
     
@@ -23,36 +29,81 @@ class PlayService: MediaBrowserServiceCompat() {
         private const val TAG = "BackgroundPlayService"
         const val ROOT_ID = TAG
         private const val PlaybackStateActions =
-            ACTION_PLAY_PAUSE or ACTION_SEEK_TO or ACTION_PLAY_FROM_MEDIA_ID or ACTION_SKIP_TO_NEXT or ACTION_SKIP_TO_PREVIOUS or ACTION_SKIP_TO_QUEUE_ITEM
+            ACTION_PLAY_PAUSE or ACTION_SEEK_TO or ACTION_PLAY_FROM_MEDIA_ID or ACTION_SKIP_TO_NEXT or ACTION_SKIP_TO_PREVIOUS
     }
     
     private lateinit var mediaPlayer: MediaPlayer
+    
+    private lateinit var audioInfoList: List<AudioInfo>
+    private var listPos = 0
+    
     
     private lateinit var mediaSession: MediaSessionCompat
     private val mediaSessionCallback = object : MediaSessionCompat.Callback() {
         override fun onPlay() {
             Log.e(TAG, "onPlay")
+            if (playbackStateCompat.state != STATE_PAUSED) {
+                return
+            }
+            playbackStateCompat = PlaybackStateCompat.Builder()
+                .setState(STATE_PLAYING, playbackStateCompat.position, 1F)
+                .build()
+            mediaSession.setPlaybackState(playbackStateCompat)
+            mediaPlayer.start()
         }
         override fun onPause() {
             Log.e(TAG, "onPause")
+            if (playbackStateCompat.state != STATE_PLAYING) {
+                return
+            }
+            playbackStateCompat = PlaybackStateCompat.Builder(playbackStateCompat)
+                .setState(STATE_PAUSED, mediaPlayer.currentPosition.toLong(), 1F)
+                .build()
+            mediaSession.setPlaybackState(playbackStateCompat)
+            mediaPlayer.pause()
         }
         override fun onSkipToPrevious() {
             Log.e(TAG, "onSkipToPrevious")
+            if (!::audioInfoList.isInitialized || listPos == -1) {
+                return
+            }
+            onPlayFromMediaId(audioInfoList[++listPos].audioId, null)
         }
         override fun onSkipToNext() {
             Log.e(TAG, "onSkipToNext")
-        }
-        override fun onSkipToQueueItem(id: Long) {
-            Log.e(TAG, "onSkipToQueueItem $id")
-        }
-        override fun onPlayFromUri(uri: Uri?, extras: Bundle?) {
-            Log.e(TAG, "onPlayFromUri $uri")
+            if (!::audioInfoList.isInitialized || listPos == -1) {
+                return
+            }
+            onPlayFromMediaId(audioInfoList[--listPos].audioId, null)
         }
         override fun onPlayFromMediaId(mediaId: String?, extras: Bundle?) {
             Log.e(TAG, "onPlayFromMediaId $mediaId")
+            mediaId?:return
+            if (extras != null) {
+                @Suppress("UNCHECKED_CAST")
+                audioInfoList = (extras.getSerializable(EXTRAS_AUDIO_INFO_LIST) as ArrayList<AudioInfo>?) ?: return
+                listPos = extras.getInt(EXTRAS_AUDIO_INFO_POS)
+            }
+            
+            playbackStateCompat = PlaybackStateCompat.Builder(playbackStateCompat)
+                .setState(STATE_BUFFERING, 0, 1F)
+                .build()
+            mediaSession.setPlaybackState(playbackStateCompat)
+            
+            mediaPlayer.syncPlayAndPrepareMediaId(this@PlayService, mediaId) {
+                playbackStateCompat = PlaybackStateCompat.Builder(playbackStateCompat)
+                    .setState(STATE_PLAYING, 0, 1F)
+                    .build()
+                mediaSession.setPlaybackState(playbackStateCompat)
+            }
         }
         override fun onSeekTo(pos: Long) {
             Log.e(TAG, "onSeekTo $pos")
+            playbackStateCompat = PlaybackStateCompat.Builder(playbackStateCompat)
+                .setState(playbackStateCompat.state, pos, 1F)
+                .build()
+            mediaSession.setPlaybackState(playbackStateCompat)
+            mediaPlayer.seekTo(pos.toInt())
         }
     }
     
