8fbb2d9dff86953ab8d2063964eb8706b95f7c13
1552980358 <1552980358@qq.com>
2021-08-27 12:18:28 +0800

    Update Constants.kt

---
+++ app/src/main/java/sakuraba/saki/player/music/util/Constants.kt
@@ -0,0 +1,9 @@
+package sakuraba.saki.player.music.util
+
+object Constants {
+    
+    const val EXTRAS_AUDIO_INFO_POS = "audio_info_pos"
+    const val EXTRAS_AUDIO_INFO = "audio_info"
+    const val EXTRAS_AUDIO_INFO_LIST = "audio_info_list"
+    
+}
