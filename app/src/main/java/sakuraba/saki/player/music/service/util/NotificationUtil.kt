fbf78cc11619325b5828252fd1c26323c39dbc08
1552980358 <1552980358@qq.com>
2021-08-28 17:29:50 +0800

    NotificationUtil.kt: Update getNotification()

--- app/src/main/java/sakuraba/saki/player/music/service/util/NotificationUtil.kt
+++ app/src/main/java/sakuraba/saki/player/music/service/util/NotificationUtil.kt
@@ -2,14 +2,20 @@ package sakuraba.saki.player.music.service.util
 
 import android.app.Notification
 import android.app.NotificationChannel
-import android.app.NotificationManager
 import android.app.Service
 import android.content.Context
-import android.content.Context.NOTIFICATION_SERVICE
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.os.Build
+import android.support.v4.media.session.MediaSessionCompat.Token
+import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
 import androidx.core.app.NotificationManagerCompat.IMPORTANCE_HIGH
+import androidx.media.app.NotificationCompat.MediaStyle
+import lib.github1552980358.ktExtension.jvm.keyword.tryOnly
 import sakuraba.saki.player.music.BuildConfig
+import sakuraba.saki.player.music.R
+import sakuraba.saki.player.music.util.BitmapUtil.loadAlbumArt
 
 private const val ChannelName = "PlayServiceNotification"
 
@@ -26,3 +32,24 @@ val NotificationManagerCompat.createChannel get(): NotificationManagerCompat = a
 }
 
 fun Service.startForeground(notification: Notification) = startForeground(NotificationId, notification)
+
+fun Notification?.getNotification(context: Context, token: Token, audioInfo: AudioInfo) = if (this == null) {
+    NotificationCompat.Builder(context, ChannelId)
+        .setSmallIcon(R.mipmap.ic_launcher_round)
+        .setOngoing(false)
+        .setPriority(NotificationCompat.PRIORITY_MAX)
+        .setStyle(MediaStyle().setMediaSession(token))
+} else {
+    NotificationCompat.Builder(context, this)
+}.apply {
+    setWhen(System.currentTimeMillis())
+    setContentTitle(audioInfo.audioTitle)
+    setContentText("${audioInfo.audioArtist} - ${audioInfo.audioAlbum}")
+    var bitmap: Bitmap? = null
+    tryOnly { bitmap = context.loadAlbumArt(audioInfo.audioAlbumId) }
+    if (bitmap != null) {
+        setLargeIcon(bitmap)
+    } else {
+        setLargeIcon(BitmapFactory.decodeResource(context.resources, R.mipmap.ic_launcher))
+    }
+}.build()
