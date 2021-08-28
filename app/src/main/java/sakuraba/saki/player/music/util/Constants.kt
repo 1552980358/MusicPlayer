da1d1ff86872c8aca602fe38225a19413ff03fa7
1552980358 <1552980358@qq.com>
2021-08-28 17:26:54 +0800

    Constants.kt: Update constants

--- app/src/main/java/sakuraba/saki/player/music/util/Constants.kt
+++ app/src/main/java/sakuraba/saki/player/music/util/Constants.kt
@@ -5,5 +5,13 @@ object Constants {
     const val EXTRAS_AUDIO_INFO_POS = "audio_info_pos"
     const val EXTRAS_AUDIO_INFO = "audio_info"
     const val EXTRAS_AUDIO_INFO_LIST = "audio_info_list"
+    const val EXTRAS_STATUS = "status"
+    const val EXTRAS_PROGRESS = "progress"
+    
+    const val ACTION_REQUEST_STATUS = "request_status"
+    
+    const val ACTION_START = "action_start"
+    const val START_EXTRAS_PLAY = "play"
+    const val START_EXTRAS_PAUSE = "pause"
     
 }
