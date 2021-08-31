6c4638abe2c8fc95347a0e2fe3a10971b0f0ed57
1552980358 <1552980358@qq.com>
2021-08-31 15:56:45 +0800

    Constants.kt: Update constants

--- app/src/main/java/sakuraba/saki/player/music/util/Constants.kt
+++ app/src/main/java/sakuraba/saki/player/music/util/Constants.kt
@@ -9,9 +9,19 @@ object Constants {
     const val EXTRAS_PROGRESS = "progress"
     
     const val ACTION_REQUEST_STATUS = "request_status"
-    
+    const val ACTION_UPDATE_PLAY_MODE = "update_play_mode"
     const val ACTION_START = "action_start"
+    
     const val START_EXTRAS_PLAY = "play"
     const val START_EXTRAS_PAUSE = "pause"
+    
+    const val EXTRAS_PLAY_MODE = "play_mode"
+    const val PLAY_MODE_SINGLE = 1
+    const val PLAY_MODE_SINGLE_CYCLE = 2
+    const val PLAY_MODE_LIST = 4
+    const val PLAY_MODE_RANDOM = 8
+    
+    const val TRANSITION_IMAGE_VIEW = "image_view"
+    const val TRANSITION_TEXT_VIEW = "text_view"
     
 }
