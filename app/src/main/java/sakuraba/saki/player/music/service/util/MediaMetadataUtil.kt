9d40d56f91a6bb75c4d78493a88980c60a13b8b9
1552980358 <1552980358@qq.com>
2021-08-31 18:51:08 +0800

    MediaMetadataUtil.kt: Add string extra METADATA_KEY_ALBUM_ART_URI

--- app/src/main/java/sakuraba/saki/player/music/service/util/MediaMetadataUtil.kt
+++ app/src/main/java/sakuraba/saki/player/music/service/util/MediaMetadataUtil.kt
@@ -2,10 +2,12 @@ package sakuraba.saki.player.music.service.util
 
 import android.support.v4.media.MediaMetadataCompat.Builder
 import android.support.v4.media.MediaMetadataCompat.METADATA_KEY_ALBUM
+import android.support.v4.media.MediaMetadataCompat.METADATA_KEY_ALBUM_ART_URI
 import android.support.v4.media.MediaMetadataCompat.METADATA_KEY_ARTIST
 import android.support.v4.media.MediaMetadataCompat.METADATA_KEY_DURATION
 import android.support.v4.media.MediaMetadataCompat.METADATA_KEY_MEDIA_ID
 import android.support.v4.media.MediaMetadataCompat.METADATA_KEY_TITLE
+import sakuraba.saki.player.music.util.BitmapUtil.getURI
 
 object MediaMetadataUtil {
     
@@ -14,6 +16,7 @@ object MediaMetadataUtil {
         putString(METADATA_KEY_TITLE, audioInfo.audioTitle)
         putString(METADATA_KEY_ARTIST, audioInfo.audioArtist)
         putString(METADATA_KEY_ALBUM, audioInfo.audioAlbum)
+        putString(METADATA_KEY_ALBUM_ART_URI, audioInfo.audioAlbumId.getURI.toString())
         putLong(METADATA_KEY_DURATION, audioInfo.audioDuration)
         return this
     }
