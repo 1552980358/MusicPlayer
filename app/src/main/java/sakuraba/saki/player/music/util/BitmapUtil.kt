ad00af5eb2794f8ada91f228d8af06dd71d8c882
1552980358 <1552980358@qq.com>
2021-08-31 18:50:43 +0800

    BitmapUtil.kt: Update overloaded loadAlbumArt()

--- app/src/main/java/sakuraba/saki/player/music/util/BitmapUtil.kt
+++ app/src/main/java/sakuraba/saki/player/music/util/BitmapUtil.kt
@@ -9,11 +9,18 @@ import androidx.fragment.app.Fragment
 object BitmapUtil {
     
     private val ALBUM_ART_URI = Uri.parse("content://media/external/audio/albumart")
-    fun Context.loadAlbumArt(albumId: Long) = BitmapFactory.decodeFileDescriptor(
-        contentResolver
-            .openFileDescriptor(ContentUris.withAppendedId(ALBUM_ART_URI, albumId), "r")
-            ?.fileDescriptor
-    )
+    
+    val Long.getURI get() = ContentUris.withAppendedId(ALBUM_ART_URI, this)
+    
+    @Suppress("HasPlatformType")
+    fun Context.loadAlbumArt(albumId: Long) = loadAlbumArt(albumId.getURI)
+    
+    @Suppress("HasPlatformType")
+    fun Context.loadAlbumArt(uri: String) = loadAlbumArt(Uri.parse(uri))
+    
+    @Suppress("HasPlatformType")
+    fun Context.loadAlbumArt(uri: Uri) =
+        BitmapFactory.decodeFileDescriptor(contentResolver.openFileDescriptor(uri, "r")?.fileDescriptor)
     
     fun Fragment.loadAlbumArt(albumId: Long) = requireContext().loadAlbumArt(albumId)
     
