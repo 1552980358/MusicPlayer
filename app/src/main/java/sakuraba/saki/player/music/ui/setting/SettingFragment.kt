6f72d8835aa035569e3eb5b51abee62c485f686c
1552980358 <1552980358@qq.com>
2021-09-23 13:16:41 +0800

    Update SettingFragment

---
+++ app/src/main/java/sakuraba/saki/player/music/ui/setting/SettingFragment.kt
@@ -0,0 +1,30 @@
+package sakuraba.saki.player.music.ui.setting
+
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import androidx.navigation.fragment.findNavController
+import androidx.preference.Preference
+import androidx.preference.PreferenceFragmentCompat
+import sakuraba.saki.player.music.R
+import sakuraba.saki.player.music.util.SettingUtil.KEY_AUDIO
+
+class SettingFragment: PreferenceFragmentCompat() {
+    
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+        setPreferencesFromResource(R.xml.xml_setting, rootKey)
+        
+        setHasOptionsMenu(true)
+        
+        findPreference<Preference>(KEY_AUDIO)?.setOnPreferenceClickListener {
+            findNavController().navigate(R.id.nav_setting_audio_filter)
+            return@setOnPreferenceClickListener true
+        }
+        
+    }
+    
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        menu.clear()
+    }
+    
+}
