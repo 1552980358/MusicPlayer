ad466d85a549ee26c0e185294374f745df99989a
1552980358 <1552980358@qq.com>
2021-10-02 16:06:08 +0800

    Move ui.album.util.RecyclerViewAdapter into ui.album.util.RecyclerViewAdapterUtil and implement

--- app/src/main/java/sakuraba/saki/player/music/ui/album/AlbumFragment.kt
+++ app/src/main/java/sakuraba/saki/player/music/ui/album/AlbumFragment.kt
@@ -17,9 +17,8 @@ import kotlinx.coroutines.launch
 import lib.github1552980358.ktExtension.jvm.keyword.tryRun
 import sakuraba.saki.player.music.database.AudioDatabaseHelper
 import sakuraba.saki.player.music.databinding.FragmentAlbumBinding
-import sakuraba.saki.player.music.ui.album.util.RecyclerViewAdapter
+import sakuraba.saki.player.music.ui.album.util.RecyclerViewAdapterUtil
 import sakuraba.saki.player.music.util.BitmapUtil.loadAlbumArt
-import sakuraba.saki.player.music.util.MediaAlbum
 import java.util.concurrent.TimeUnit
 
 class AlbumFragment: Fragment() {
@@ -27,19 +26,19 @@ class AlbumFragment: Fragment() {
     private var _fragmentAlbumBinding: FragmentAlbumBinding? = null
     private val fragmentAlbum get() = _fragmentAlbumBinding!!
     
-    private val mediaAlbumList = ArrayList<MediaAlbum>()
-    private val bitmapMap = mutableMapOf<Long, Bitmap>()
+    private lateinit var recyclerViewAdapter: RecyclerViewAdapterUtil
     
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         _fragmentAlbumBinding = FragmentAlbumBinding.inflate(layoutInflater)
         sharedElementReturnTransition = TransitionInflater.from(context).inflateTransition(android.R.transition.move)
         fragmentAlbum.recyclerView.layoutManager = GridLayoutManager(requireContext(), 2)
-        fragmentAlbum.recyclerView.adapter = RecyclerViewAdapter(mediaAlbumList) { imageView, textView, mediaAlbum ->
+        recyclerViewAdapter = RecyclerViewAdapterUtil { imageView, textView, mediaAlbum ->
             findNavController().navigate(
                 AlbumFragmentDirections.actionNavAlbumToNavAlbumList(mediaAlbum),
                 FragmentNavigatorExtras(imageView to "${mediaAlbum.albumId}_image", textView to "${mediaAlbum.albumId}_text")
             )
         }
+        recyclerViewAdapter.setAdapterToRecyclerView(fragmentAlbum.recyclerView)
         fragmentAlbum.root.isRefreshing = true
         postponeEnterTransition(100, TimeUnit.MILLISECONDS)
         return fragmentAlbum.root
@@ -48,25 +47,25 @@ class AlbumFragment: Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         CoroutineScope(Dispatchers.IO).launch {
-            if (mediaAlbumList.isEmpty()) {
-                AudioDatabaseHelper(requireContext()).queryMediaAlbum(mediaAlbumList)
-                mediaAlbumList.sortBy { mediaAlbum -> mediaAlbum.titlePinyin }
-                launch(Dispatchers.Main) { fragmentAlbum.recyclerView.adapter?.notifyDataSetChanged() }
-                if (mediaAlbumList.isNotEmpty()) {
+            if (recyclerViewAdapter.mediaAlbumList.isEmpty()) {
+                AudioDatabaseHelper(requireContext()).queryMediaAlbum(recyclerViewAdapter.mediaAlbumList)
+                recyclerViewAdapter.mediaAlbumList.sortBy { mediaAlbum -> mediaAlbum.titlePinyin }
+                launch(Dispatchers.Main) { recyclerViewAdapter.notifyDataSetChanged() }
+                if (recyclerViewAdapter.mediaAlbumList.isNotEmpty()) {
                     var bitmap: Bitmap?
-                    mediaAlbumList.forEach { mediaAlbum ->
+                    recyclerViewAdapter.mediaAlbumList.forEach { mediaAlbum ->
                         bitmap = null
-                        if (bitmapMap[mediaAlbum.albumId] == null) {
+                        if (recyclerViewAdapter.bitmapMap[mediaAlbum.albumId] == null) {
                             bitmap = tryRun { loadAlbumArt(mediaAlbum.albumId) }
                             if (bitmap != null) {
-                                bitmapMap[mediaAlbum.albumId] = bitmap!!
+                                recyclerViewAdapter.bitmapMap[mediaAlbum.albumId] = bitmap
                             }
                         }
                     }
                 }
             }
             launch(Dispatchers.Main) {
-                (fragmentAlbum.recyclerView.adapter as RecyclerViewAdapter).updateBitmapMap(bitmapMap)
+                recyclerViewAdapter.notifyDataSetChanged()
                 fragmentAlbum.root.isRefreshing = false
                 fragmentAlbum.recyclerView.doOnPreDraw {
                     startPostponedEnterTransition()
---
+++ app/src/main/java/sakuraba/saki/player/music/ui/album/util/RecyclerViewAdapterUtil.kt
@@ -0,0 +1,64 @@
+package sakuraba.saki.player.music.ui.album.util
+
+import android.graphics.Bitmap
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import sakuraba.saki.player.music.R
+import sakuraba.saki.player.music.util.MediaAlbum
+
+class RecyclerViewAdapterUtil(listener: (imageView: ImageView, textView: TextView, mediaAlbum: MediaAlbum) -> Unit) {
+    
+    private class AlbumViewHolder(view: View): RecyclerView.ViewHolder(view) {
+        
+        val imageView: ImageView = view.findViewById(R.id.image_view)
+        
+        val textView: TextView = view.findViewById(R.id.text_view)
+        
+        val linearLayout: LinearLayout = view.findViewById(R.id.linear_layout)
+        
+    }
+    
+    private class RecyclerViewAdapter(
+        val mediaAlbumList: List<MediaAlbum>,
+        val bitmapMap: Map<Long, Bitmap?>,
+        private val listener: (imageView: ImageView, textView: TextView, mediaAlbum: MediaAlbum) -> Unit
+    ): RecyclerView.Adapter<AlbumViewHolder>() {
+        
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int)
+            = AlbumViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.content_album, parent, false))
+        
+        override fun onBindViewHolder(holder: AlbumViewHolder, position: Int) {
+            holder.textView.text = mediaAlbumList[position].title
+            holder.linearLayout.setOnClickListener {
+                listener(holder.imageView, holder.textView, mediaAlbumList[position])
+            }
+            holder.imageView.transitionName = "${mediaAlbumList[position].albumId}_image"
+            holder.textView.transitionName = "${mediaAlbumList[position].albumId}_text"
+            val bitmap = bitmapMap[mediaAlbumList[position].albumId]
+            if (bitmap != null) {
+                holder.imageView.setImageBitmap(bitmap)
+            }
+        }
+        
+        override fun getItemCount() = mediaAlbumList.size
+        
+    }
+    
+    private val adapter = RecyclerViewAdapter(arrayListOf(), mutableMapOf(), listener)
+    
+    fun setAdapterToRecyclerView(recyclerView: RecyclerView) {
+        recyclerView.adapter = adapter
+    }
+    
+    fun notifyDataSetChanged() = adapter.notifyDataSetChanged()
+    
+    val mediaAlbumList get() = adapter.mediaAlbumList as ArrayList
+    
+    val bitmapMap get() = adapter.bitmapMap as MutableMap
+    
+}
