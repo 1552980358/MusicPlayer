8e318437731015864f278d8dc6f6999ea02d5176
1552980358 <1552980358@qq.com>
2021-10-04 15:55:05 +0800

    ActivityFragmentInterface.kt: Update and implement OnHomeFragmentPausedListener

--- app/src/main/java/sakuraba/saki/player/music/util/ActivityFragmentInterface.kt
+++ app/src/main/java/sakuraba/saki/player/music/util/ActivityFragmentInterface.kt
@@ -1,5 +1,6 @@
 package sakuraba.saki.player.music.util
 
+import android.graphics.Bitmap
 import sakuraba.saki.player.music.service.util.AudioInfo
 import java.io.Serializable
 
@@ -9,7 +10,12 @@ class ActivityFragmentInterface(): Serializable {
         fun onItemClicked(pos: Int, audioInfo: AudioInfo?, arrayList: ArrayList<AudioInfo>?)
     }
     
+    private fun interface OnHomeFragmentPausedListener: Serializable {
+        fun onPaused(arrayList: ArrayList<AudioInfo>, mutableMap: MutableMap<Long, Bitmap?>)
+    }
+    
     private var fragmentListItemClickListener: OnFragmentListItemClickListener? = null
+    private var homeFragmentPausedListener: OnHomeFragmentPausedListener? = null
     
     constructor(block: (Int, AudioInfo?, ArrayList<AudioInfo>?) -> Unit): this() {
         fragmentListItemClickListener = OnFragmentListItemClickListener(block)
@@ -17,5 +23,16 @@ class ActivityFragmentInterface(): Serializable {
     
     fun onFragmentListItemClick(pos: Int, audioInfo: AudioInfo?, audioInfoList: ArrayList<AudioInfo>?) =
         fragmentListItemClickListener?.onItemClicked(pos, audioInfo, audioInfoList)
+    
+    fun setOnHomeFragmentPausedListener(block: (arrayList: ArrayList<AudioInfo>, mutableMap: MutableMap<Long, Bitmap?>) -> Unit) {
+        homeFragmentPausedListener = OnHomeFragmentPausedListener(block)
+    }
+    
+    fun removeOnHomeFragmentPausedListener() {
+        homeFragmentPausedListener = null
+    }
+    
+    fun onHomeFragmentPaused(arrayList: ArrayList<AudioInfo>, mutableMap: MutableMap<Long, Bitmap?>) =
+        homeFragmentPausedListener?.onPaused(arrayList, mutableMap)
     
 }
