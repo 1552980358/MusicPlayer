b26be555c0a1537affe6e53c79ec3115db37382e
1552980358 <1552980358@qq.com>
2021-10-30 19:54:50 +0800

    WebService.kt: Update notification notify method

--- app/src/main/java/sakuraba/saki/player/music/web/WebService.kt
+++ app/src/main/java/sakuraba/saki/player/music/web/WebService.kt
@@ -18,6 +18,7 @@ import sakuraba.saki.player.music.service.util.startService
 import sakuraba.saki.player.music.util.Constants.EXTRA_WEBSERVER
 import sakuraba.saki.player.music.util.Constants.EXTRA_WEBSERVER_START
 import sakuraba.saki.player.music.util.Constants.EXTRA_WEBSERVER_STOP
+import sakuraba.saki.player.music.util.ServiceUtil.isForegroundService
 import sakuraba.saki.player.music.util.SettingUtil.getBooleanSetting
 import sakuraba.saki.player.music.web.server.WebServer
 import sakuraba.saki.player.music.web.util.NetworkUtil.hasConnection
@@ -25,6 +26,7 @@ import sakuraba.saki.player.music.web.util.NetworkUtil.ipAddress
 import sakuraba.saki.player.music.web.util.NotificationUtil.createNotificationManager
 import sakuraba.saki.player.music.web.util.NotificationUtil.getNotification
 import sakuraba.saki.player.music.web.util.NotificationUtil.startForeground
+import sakuraba.saki.player.music.web.util.NotificationUtil.updateNotification
 import sakuraba.saki.player.music.web.util.WebControlUtil
 
 class WebService: Service() {
@@ -61,9 +63,7 @@ class WebService: Service() {
     }
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
-        intent ?: return super.onStartCommand(intent, flags, startId)
-
-        when (intent.getIntExtra(EXTRA_WEBSERVER, EXTRA_WEBSERVER_STOP)) {
+        when (intent?.getIntExtra(EXTRA_WEBSERVER, EXTRA_WEBSERVER_STOP)) {
             EXTRA_WEBSERVER_START -> {
                 if (!connectivityManager.hasConnection) {
                     stopForeground(true)
@@ -78,8 +78,10 @@ class WebService: Service() {
                 }
 
                 webServer = WebServer(serverPort, this, webControlUtil).apply { start() }
-
-                startForeground(getNotification("${connectivityManager.ipAddress}:$serverPort"))
+                when {
+                    isForegroundService -> notificationManager.updateNotification(getNotification("${connectivityManager.ipAddress}:$serverPort"))
+                    else -> startForeground(getNotification("${connectivityManager.ipAddress}:$serverPort"))
+                }
             }
             EXTRA_WEBSERVER_STOP -> {
                 stopForeground(true)
