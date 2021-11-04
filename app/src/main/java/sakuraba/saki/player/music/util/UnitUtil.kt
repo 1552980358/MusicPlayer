ad9c40c105967459534ca87c2ddbf3ac8f67ad8e
1552980358 <1552980358@qq.com>
2021-11-04 15:40:14 +0800

    UnitUtil.kt: Update modifiers

--- app/src/main/java/sakuraba/saki/player/music/util/UnitUtil.kt
+++ app/src/main/java/sakuraba/saki/player/music/util/UnitUtil.kt
@@ -5,15 +5,15 @@ object UnitUtil {
     private const val BYTES = "Bytes"
     val Long.asMiB get() = "${String.format("%.2f", this / 1048576F)} M$BYTES"
 
-    private const val UNIT_BITS = "bits"
+    const val UNIT_BITS = "bits"
     private const val UNIT_KILO = "K"
-    private const val UNIT_Hertz = "Hz"
-    private const val UNIT_SEC = "s"
-    private const val UNIT_SAMPLE = "sample"
-    private const val PER = "/"
-    private val String.getAsKilo get() = toInt().getAsKilo
-    private val Int.getAsKilo get() = "${(this / 1000)} $UNIT_KILO"
-    private val Long.addZero get() = if (this > 9) this.toString() else "0${this}"
-    private val Long.toTimeFormat get() = (this / 60000).toString() + ':' + (this / 1000 % 60).addZero
+    const val UNIT_Hertz = "Hz"
+    const val UNIT_SEC = "s"
+    const val UNIT_SAMPLE = "sample"
+    const val PER = "/"
+    val String.getAsKilo get() = toInt().getAsKilo
+    val Int.getAsKilo get() = "${(this / 1000)} $UNIT_KILO"
+    val Long.addZero get() = if (this > 9) this.toString() else "0${this}"
+    val Long.toTimeFormat get() = (this / 60000).toString() + ':' + (this / 1000 % 60).addZero
 
 }
