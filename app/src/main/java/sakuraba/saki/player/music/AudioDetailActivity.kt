a96aafe33803255d40f743c2b1637edf0e44ed00
1552980358 <1552980358@qq.com>
2021-11-13 20:34:21 +0800

    AudioDetailActivity.kt: Update FragmentLifecycleCallbacks

--- app/src/main/java/sakuraba/saki/player/music/AudioDetailActivity.kt
+++ app/src/main/java/sakuraba/saki/player/music/AudioDetailActivity.kt
@@ -1,31 +1,79 @@
 package sakuraba.saki.player.music
 
+import android.app.Instrumentation
+import android.content.Context
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
 import androidx.navigation.findNavController
 import sakuraba.saki.player.music.databinding.ActivityAudioDetailBinding
-import sakuraba.saki.player.music.util.ActivityUtil.translateExit
 
 class AudioDetailActivity: AppCompatActivity() {
 
     private var _activityAudioDetailBinding: ActivityAudioDetailBinding? = null
     private val activityAudioDetail get() = _activityAudioDetailBinding!!
 
+    private val fragmentLifecycleCallbacks =  object : FragmentManager.FragmentLifecycleCallbacks() {
+        override fun onFragmentAttached(fm: FragmentManager, f: Fragment, context: Context) {
+            Log.e(f::class.java.simpleName, "onFragmentAttached")
+        }
+        override fun onFragmentCreated(fm: FragmentManager, f: Fragment, savedInstanceState: Bundle?) {
+            Log.e(f::class.java.simpleName, "onFragmentCreated")
+        }
+        override fun onFragmentViewCreated(fm: FragmentManager, f: Fragment, v: View, savedInstanceState: Bundle?) {
+            Log.e(f::class.java.simpleName, "onFragmentViewCreated")
+        }
+        override fun onFragmentStarted(fm: FragmentManager, f: Fragment) {
+            Log.e(f::class.java.simpleName, "onFragmentStarted")
+        }
+        override fun onFragmentResumed(fm: FragmentManager, f: Fragment) {
+            Log.e(f::class.java.simpleName, "onFragmentResumed")
+        }
+        override fun onFragmentPaused(fm: FragmentManager, f: Fragment) {
+            Log.e(f::class.java.simpleName, "onFragmentPaused")
+        }
+        override fun onFragmentStopped(fm: FragmentManager, f: Fragment) {
+            Log.e(f::class.java.simpleName, "onFragmentStopped")
+        }
+        override fun onFragmentViewDestroyed(fm: FragmentManager, f: Fragment) {
+            Log.e(f::class.java.simpleName, "onFragmentViewDestroyed")
+        }
+        override fun onFragmentDestroyed(fm: FragmentManager, f: Fragment) {
+            Log.e(f::class.java.simpleName, "onFragmentDestroyed")
+        }
+        override fun onFragmentDetached(fm: FragmentManager, f: Fragment) {
+            Log.e(f::class.java.simpleName, "onFragmentDetached")
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        supportFragmentManager.registerFragmentLifecycleCallbacks(fragmentLifecycleCallbacks, true)
         _activityAudioDetailBinding = ActivityAudioDetailBinding.inflate(layoutInflater)
         setContentView(activityAudioDetail.root)
         setSupportActionBar(activityAudioDetail.toolbar)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
         activityAudioDetail.toolbar.setNavigationOnClickListener { onBackPressed() }
+        postponeEnterTransition()
+        startPostponedEnterTransition()
     }
 
-    override fun onBackPressed() {
-        super.onBackPressed()
-        if (findNavController(R.id.fragment).currentDestination?.id == R.id.nav_audio_detail) {
-            // fadeAnim()
-            translateExit()
-        }
+    override fun onStop() {
+        Instrumentation().callActivityOnSaveInstanceState(this, Bundle())
+        super.onStop()
+    }
+
+    override fun onBackPressed() = when (findNavController(R.id.fragment).currentDestination?.id) {
+        R.id.nav_audio_detail -> supportFinishAfterTransition()
+        else -> super.onBackPressed()
+    }
+
+    override fun onDestroy() {
+        supportFragmentManager.unregisterFragmentLifecycleCallbacks(fragmentLifecycleCallbacks)
+        super.onDestroy()
     }
 
 }
