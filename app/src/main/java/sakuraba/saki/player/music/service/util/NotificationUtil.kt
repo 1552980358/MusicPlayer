b41d22a6b6e9aca3c6a96c8d7b3961b58cd9d7b5
1552980358 <1552980358@qq.com>
2021-11-20 11:26:14 +0800

    NotificationUtil.kt: NotificationManagerCompat.IMPORTANCE_HIGH -> NotificationManager.IMPORTANCE_HIGH

--- app/src/main/java/sakuraba/saki/player/music/service/util/NotificationUtil.kt
+++ app/src/main/java/sakuraba/saki/player/music/service/util/NotificationUtil.kt
@@ -2,6 +2,7 @@ package sakuraba.saki.player.music.service.util
 
 import android.app.Notification
 import android.app.NotificationChannel
+import android.app.NotificationManager.IMPORTANCE_HIGH
 import android.app.PendingIntent
 import android.app.PendingIntent.FLAG_IMMUTABLE
 import android.app.Service
@@ -10,7 +11,6 @@ import android.content.Intent
 import android.os.Build
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
-import androidx.core.app.NotificationManagerCompat.IMPORTANCE_HIGH
 import androidx.media.MediaBrowserServiceCompat
 import androidx.media.app.NotificationCompat.MediaStyle
 import lib.github1552980358.ktExtension.android.graphics.toBitmap
