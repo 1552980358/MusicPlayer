af752bfb301fcfe57d2988a833182ba907c607c7
1552980358 <1552980358@qq.com>
2021-11-30 21:04:14 +0800

    BitmapUtil.kt: Remove redundant methods and imports

--- app/src/main/java/sakuraba/saki/player/music/util/BitmapUtil.kt
+++ app/src/main/java/sakuraba/saki/player/music/util/BitmapUtil.kt
@@ -5,40 +5,22 @@ import android.content.Context
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.net.Uri
-import androidx.fragment.app.Fragment
 import lib.github1552980358.ktExtension.android.graphics.getByteArray
 import lib.github1552980358.ktExtension.android.java.readAsBitmap
-import lib.github1552980358.ktExtension.jvm.keyword.tryRun
 import java.io.File
 
 object BitmapUtil {
     
     private val ALBUM_ART_URI = Uri.parse("content://media/external/audio/albumart")
-    
     val Long.getURI get() = ContentUris.withAppendedId(ALBUM_ART_URI, this)
-    
     @Suppress("HasPlatformType")
-    fun Context.loadAlbumArt(albumId: Long) = loadAlbumArt(albumId.getURI)
-    
-    @Suppress("HasPlatformType")
-    fun Context.loadAlbumArt(uri: String) = loadAlbumArt(Uri.parse(uri))
-    
-    @Suppress("HasPlatformType")
-    fun Context.loadAlbumArt(uri: Uri) =
-        BitmapFactory.decodeFileDescriptor(contentResolver.openFileDescriptor(uri, "r")?.fileDescriptor)
-    
-    fun Fragment.loadAlbumArt(albumId: Long) = requireContext().loadAlbumArt(albumId)
+    fun Context.loadAlbumArt(albumId: Long) = BitmapFactory.decodeFileDescriptor(contentResolver.openFileDescriptor(albumId.getURI, "r")?.fileDescriptor)
 
     private const val ALBUM_ART_40DP_DIR = "album_art_40dp"
     private val Context.albumArt40DpDir get() = getExternalFilesDir(ALBUM_ART_40DP_DIR)
 
     private fun Bitmap.writeBitmap(file: File) =
             getByteArray(format = Bitmap.CompressFormat.JPEG)?.apply { file.writeBytes(this) }
-
-    fun Context.readAlbumArt(albumId: Long) = tryRun {
-        val byteArray = File(albumArt40DpDir, albumId.toString()).readBytes()
-        BitmapFactory.decodeByteArray(byteArray, 0, byteArray.size)
-    }
 
     fun Context.writeAlbumArt40Dp(albumId: Long, bitmap: Bitmap?) =
             bitmap?.writeBitmap(File(albumArt40DpDir, albumId.toString()))
