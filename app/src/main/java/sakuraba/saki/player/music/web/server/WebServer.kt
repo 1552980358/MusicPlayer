84daea4beec2396079a0d0a4ab891bc9c249db87
1552980358 <1552980358@qq.com>
2021-12-17 17:38:50 +0800

    WebServer.kt: Update method for CROS request

--- app/src/main/java/sakuraba/saki/player/music/web/server/WebServer.kt
+++ app/src/main/java/sakuraba/saki/player/music/web/server/WebServer.kt
@@ -5,6 +5,8 @@ import android.os.Environment.DIRECTORY_MUSIC
 import android.os.Environment.getExternalStoragePublicDirectory
 import android.util.Log
 import fi.iki.elonen.NanoHTTPD
+import fi.iki.elonen.NanoHTTPD.Method.OPTIONS
+import fi.iki.elonen.NanoHTTPD.Method.POST
 import fi.iki.elonen.NanoHTTPD.Response.Status.BAD_REQUEST
 import fi.iki.elonen.NanoHTTPD.Response.Status.OK
 import java.io.File
@@ -25,6 +27,7 @@ class WebServer(port: Int, private val context: Context): NanoHTTPD(port) {
         private const val UPLOAD_NAME_NOT_SPECIFIED = "Name Not Specified"
         private val String.UPLOAD_POST_ALLOW get() = "$this: Post Allowed"
         private val String.UPLOAD_ACCEPTED get() = "$this: Accepted"
+        private const val UPLOAD_REQUIRE_POST = "Should use POST to upload"
     }
 
     override fun serve(session: IHTTPSession?): Response {
@@ -45,11 +48,27 @@ class WebServer(port: Int, private val context: Context): NanoHTTPD(port) {
 
     private fun upload(session: IHTTPSession): Response {
         Log.e(TAG, "upload")
-        val fileName = session.parameters[UPLOAD_FILE_NAME]?.first() ?: return newFixedLengthResponse(BAD_REQUEST, "$MIME_HTML$CONTENT_TYPE_CHARSET", UPLOAD_NAME_NOT_SPECIFIED)
-        val file = HashMap<String, String>().apply { session.parseBody(this) }[UPLOAD_FILE]
-            ?: return newFixedLengthResponse(OK, "$MIME_HTML$CONTENT_TYPE_CHARSET", fileName.UPLOAD_POST_ALLOW).withHeaders(session)
-        File(file).copyTo(File(getExternalStoragePublicDirectory(DIRECTORY_MUSIC), fileName))
-        return newFixedLengthResponse(OK, "$MIME_HTML$CONTENT_TYPE_CHARSET", fileName.UPLOAD_ACCEPTED).withHeaders(session)
+        val fileName = session.parameters[UPLOAD_FILE_NAME]?.first()
+            ?: return newFixedLengthResponse(BAD_REQUEST, "$MIME_PLAINTEXT$CONTENT_TYPE_CHARSET", UPLOAD_NAME_NOT_SPECIFIED)
+        return when (session.method) {
+            OPTIONS -> responseCROS(session, fileName)
+            POST -> {
+                val file = HashMap<String, String>().apply { session.parseBody(this) }[UPLOAD_FILE]
+                    ?: return responseCROS(session, fileName)
+                File(getExternalStoragePublicDirectory(DIRECTORY_MUSIC), fileName).writeBytes(File(file).readBytes())
+                return newFixedLengthResponse(OK, "$MIME_PLAINTEXT$CONTENT_TYPE_CHARSET", fileName.UPLOAD_ACCEPTED).withHeaders(session)
+            }
+            else -> newFixedLengthResponse(BAD_REQUEST, "$MIME_PLAINTEXT$CONTENT_TYPE_CHARSET", UPLOAD_REQUIRE_POST).withHeaders(session)
+        }
+    }
+
+    private fun responseCROS(session: IHTTPSession, fileName: String): Response = newFixedLengthResponse(OK, "$MIME_PLAINTEXT$CONTENT_TYPE_CHARSET", fileName.UPLOAD_POST_ALLOW).apply {
+        addHeader("Access-Control-Allow-Credentials", "true")
+        addHeader("Access-Control-Allow-Origin", session.headers["origin"] ?: "*")
+        session.headers["access-control-request-headers"]?.let { addHeader("Access-Control-Allow-Headers", it) }
+        addHeader("Access-Control-Allow-Methods","POST,GET,OPTIONS")
+        addHeader("Access-Control-Allow-Headers", session.headers["access-control-request-headers"] ?: "*")
+        addHeader("Access-Control-Max-Age", "0")
     }
 
     private val String.mimeType get() = when {
@@ -61,7 +80,7 @@ class WebServer(port: Int, private val context: Context): NanoHTTPD(port) {
 
     private fun Response.withHeaders(session: IHTTPSession) = apply {
         addHeader("Access-Control-Allow-Origin", session.headers["Access-Control-Allow-Origin"] ?: "*")
-        addHeader("Access-Control-Allow-Methods", "POST,GET,OPTIONS")
+        addHeader("Access-Control-Allow-Methods", session.headers["Access-Control-Allow-Methods"] ?: "*")
         addHeader("Access-Control-Allow-Headers", session.headers["Access-Control-Allow-Headers"] ?: "*")
         addHeader("Access-Control-Allow-Credentials", "true")
         addHeader("Access-Control-Max-Age", "0")
