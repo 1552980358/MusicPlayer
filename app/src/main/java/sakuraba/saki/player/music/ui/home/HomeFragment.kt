b0d0ce5e8461f6ef418df36db84db798551f6e8f
1552980358 <1552980358@qq.com>
2022-02-06 04:49:57 +0800

    HomeFragment.kt: Update AudioDetailActivity launching method

--- app/src/main/java/sakuraba/saki/player/music/ui/home/HomeFragment.kt
+++ app/src/main/java/sakuraba/saki/player/music/ui/home/HomeFragment.kt
@@ -1,5 +1,6 @@
 package sakuraba.saki.player.music.ui.home
 
+import android.app.Activity.RESULT_OK
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.Menu
@@ -7,13 +8,19 @@ import android.view.MenuInflater
 import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.result.contract.ActivityResultContracts.StartActivityForResult
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
+import lib.github1552980358.ktExtension.android.content.getSerializableOf
+import lib.github1552980358.ktExtension.android.content.intent
+import sakuraba.saki.player.music.AudioDetailActivity
 import sakuraba.saki.player.music.R
 import sakuraba.saki.player.music.databinding.FragmentHomeBinding
 import sakuraba.saki.player.music.ui.home.util.DividerItemDecoration
 import sakuraba.saki.player.music.ui.home.util.RecyclerViewAdapterUtil
 import sakuraba.saki.player.music.base.BaseMainFragment
+import sakuraba.saki.player.music.service.util.AudioInfo
+import sakuraba.saki.player.music.util.Constants.EXTRAS_DATA
 
 class HomeFragment: BaseMainFragment() {
     
@@ -33,6 +40,21 @@ class HomeFragment: BaseMainFragment() {
         fragmentHome.recyclerView.layoutManager = LinearLayoutManager(requireContext(), LinearLayoutManager.VERTICAL, false)
         recyclerViewAdapter = RecyclerViewAdapterUtil(this, activityInterface) { pos ->
             activityInterface.onFragmentListItemClick(pos, activityInterface.audioInfoList[pos], activityInterface.audioInfoList)
+        }
+        val audioDetailActivityLauncher = registerForActivityResult(StartActivityForResult()) {
+            if (it.resultCode == RESULT_OK) {
+                it.data?.getSerializableOf<AudioInfo>(EXTRAS_DATA)?.let { audioInfo ->
+                    activityInterface.onArtUpdate(audioInfo)
+                }
+            }
+        }
+        recyclerViewAdapter.setLongClickListener { audioInfo, activityOptionsCompat ->
+            audioDetailActivityLauncher.launch(
+                intent(requireContext(), AudioDetailActivity::class.java) {
+                    putExtra(EXTRAS_DATA, audioInfo)
+                },
+                activityOptionsCompat
+            )
         }
         recyclerViewAdapter.setAdapterToRecyclerView(fragmentHome.recyclerView)
         fragmentHome.recyclerView.addItemDecoration(DividerItemDecoration())
