d556f9c82050c2572c93fead1fdead06deb21025
1552980358 <1552980358@qq.com>
2022-02-28 21:11:32 +0800

    Playlist.kt: Update data class

---
+++ app/src/main/java/sakuraba/saki/player/music/util/Playlist.kt
@@ -0,0 +1,30 @@
+package sakuraba.saki.player.music.util
+
+import projekt.cloud.piece.c2pinyin.pinyin
+import sakuraba.saki.player.music.service.util.AudioInfo
+import java.io.Serializable
+
+data class Playlist(var title: String, var titlePinyin: String, var description: String): Serializable {
+    constructor(title: String, description: String = ""): this(title, title.pinyin, description)
+
+    val audioInfoList = arrayListOf<AudioInfo>()
+
+    var size = 0
+
+    fun forEach(forEachBlock: (AudioInfo) -> Unit) = audioInfoList.forEach(forEachBlock)
+
+    operator fun plusAssign(audioInfo: AudioInfo) {
+        if (!audioInfoList.contains(audioInfo)) {
+            audioInfoList.add(audioInfo)
+        }
+    }
+
+    operator fun get(audioId: String) = audioInfoList.find { it.audioId == audioId }
+
+    operator fun get(index: Int) = audioInfoList[index]
+
+    operator fun set(index: Int, audioInfo: AudioInfo) {
+        audioInfoList[index] = audioInfo
+    }
+
+}
