bf0b3a89ab348f06114519cb9ebda7972adb2f57
1552980358 <1552980358@qq.com>
2022-02-28 21:41:01 +0800

    PlaylistFragment.kt: Update fragment

---
+++ app/src/main/java/sakuraba/saki/player/music/ui/playlist/PlaylistFragment.kt
@@ -0,0 +1,94 @@
+package sakuraba.saki.player.music.ui.playlist
+
+import android.graphics.Color.WHITE
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import kotlinx.coroutines.Dispatchers.IO
+import kotlinx.coroutines.async
+import lib.github1552980358.ktExtension.androidx.fragment.app.show
+import lib.github1552980358.ktExtension.kotlinx.coroutines.io
+import sakuraba.saki.player.music.databinding.FragmentPlaylistBinding
+import sakuraba.saki.player.music.R
+import sakuraba.saki.player.music.database.AudioDatabaseHelper
+import sakuraba.saki.player.music.ui.playlist.dialog.AddPlaylistDialogFragment
+import sakuraba.saki.player.music.ui.playlist.util.RecyclerViewAdapterUtil
+import sakuraba.saki.player.music.util.BitmapUtil.loadPlaylist40Dp
+import sakuraba.saki.player.music.util.CoroutineUtil.ui
+
+class PlaylistFragment: Fragment() {
+
+    private var _fragmentPlaylistBinding: FragmentPlaylistBinding? = null
+    private val layout get() = _fragmentPlaylistBinding!!
+
+    private lateinit var recyclerViewAdapterUtil: RecyclerViewAdapterUtil
+    private val playlistList get() = recyclerViewAdapterUtil.playlistList
+    private val bitmapMap get() = recyclerViewAdapterUtil.bitmapMap
+
+    private lateinit var audioDatabaseHelper: AudioDatabaseHelper
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        _fragmentPlaylistBinding = FragmentPlaylistBinding.inflate(layoutInflater)
+        setHasOptionsMenu(true)
+        return layout.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        recyclerViewAdapterUtil = RecyclerViewAdapterUtil(layout.recyclerView) {
+
+        }
+        layout.root.apply {
+            isEnabled = false
+            isRefreshing = true
+        }
+        audioDatabaseHelper = AudioDatabaseHelper(requireContext())
+        io {
+            val loadImage = async(IO) { requireContext().loadPlaylist40Dp(bitmapMap) }
+            loadImage.start()
+
+            audioDatabaseHelper.queryAllPlaylist(playlistList)
+            playlistList.forEach { audioDatabaseHelper.queryPlaylistContent(it) }
+
+            loadImage.await()
+
+            ui {
+                recyclerViewAdapterUtil.notifyDataSetChanged()
+                layout.root.isRefreshing = false
+            }
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        menu.clear()
+        inflater.inflate(R.menu.menu_playlist, menu)
+        menu.getItem(0).icon.setTint(WHITE)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.menu_add -> AddPlaylistDialogFragment { playlist ->
+                if (playlistList.indexOfFirst { it.title == playlist.title } == -1) {
+                    audioDatabaseHelper.apply {
+                        createPlaylist(playlist)
+                        writeComplete()
+                    }
+                    playlistList.add(playlist)
+                    recyclerViewAdapterUtil.notifyDataSetChanged()
+                }
+            }.show(this)
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    override fun onDestroyView() {
+        audioDatabaseHelper.close()
+        _fragmentPlaylistBinding = null
+        super.onDestroyView()
+    }
+
+}
