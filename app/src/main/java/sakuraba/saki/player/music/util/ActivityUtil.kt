0385874307364b56e7b1eef29c3fa6d373948c0a
1552980358 <1552980358@qq.com>
2022-03-02 21:48:50 +0800

    ActivityUtil.kt: Remove unused methods and update method setLightNavigationBar()

--- app/src/main/java/sakuraba/saki/player/music/util/ActivityUtil.kt
+++ app/src/main/java/sakuraba/saki/player/music/util/ActivityUtil.kt
@@ -2,23 +2,19 @@ package sakuraba.saki.player.music.util
 
 import android.app.Activity
 import android.content.Context
-import sakuraba.saki.player.music.R
+import android.os.Build.VERSION.SDK_INT
+import android.os.Build.VERSION_CODES.Q
+import android.view.View.SYSTEM_UI_FLAG_LIGHT_NAVIGATION_BAR
+import android.view.WindowInsetsController.APPEARANCE_LIGHT_NAVIGATION_BARS
 
 object ActivityUtil {
 
-    fun Context.translateEnter() {
-        if (this is Activity) {
-            translateEnter()
+    fun Activity.setLightNavigationBar() {
+        when  {
+            SDK_INT > Q -> window.decorView.windowInsetsController?.setSystemBarsAppearance(APPEARANCE_LIGHT_NAVIGATION_BARS, APPEARANCE_LIGHT_NAVIGATION_BARS)
+            else -> @Suppress("DEPRECATION", "InlinedApi")
+            window.decorView.systemUiVisibility = SYSTEM_UI_FLAG_LIGHT_NAVIGATION_BAR or window.decorView.systemUiVisibility
         }
     }
-
-    fun Context.translateExit() {
-        if (this is Activity) {
-            translateExit()
-        }
-    }
-
-    fun Activity.translateEnter() = overridePendingTransition(R.anim.translate_enter, R.anim.translate_exit)
-    fun Activity.translateExit() = overridePendingTransition(R.anim.translate_pop_enter, R.anim.translate_pop_exit)
 
 }
