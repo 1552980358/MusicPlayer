e137c697a0fec5fd706c324fc1d73bb00818b150
1552980358 <1552980358@qq.com>
2022-03-02 18:02:35 +0800

    Playlist.kt: Update and implement PlaylistIterator

--- app/src/main/java/sakuraba/saki/player/music/util/Playlist.kt
+++ app/src/main/java/sakuraba/saki/player/music/util/Playlist.kt
@@ -4,7 +4,7 @@ import projekt.cloud.piece.c2pinyin.pinyin
 import sakuraba.saki.player.music.service.util.AudioInfo
 import java.io.Serializable
 
-data class Playlist(var title: String, var titlePinyin: String, var description: String): Serializable {
+data class Playlist(var title: String, var titlePinyin: String, var description: String): Serializable, Iterable<AudioInfo> {
     constructor(title: String, description: String = ""): this(title, title.pinyin, description)
 
     val audioInfoList = arrayListOf<AudioInfo>()
@@ -26,5 +26,13 @@ data class Playlist(var title: String, var titlePinyin: String, var description:
     operator fun set(index: Int, audioInfo: AudioInfo) {
         audioInfoList[index] = audioInfo
     }
+
+    inner class PlaylistIterator: Serializable, Iterator<AudioInfo> {
+        private var current = -1
+        override fun hasNext() = current + 1 < audioInfoList.size
+        override fun next() = audioInfoList[++current]
+    }
+
+    override fun iterator() = PlaylistIterator()
 
 }
