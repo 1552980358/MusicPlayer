0d015988bbfc027a07452001867a42be835e0afb
1552980358 <1552980358@qq.com>
2022-03-07 16:05:26 +0800

    PlayViewModel.kt: Remove unused variables and methods

--- app/src/main/java/sakuraba/saki/player/music/PlayViewModel.kt
+++ app/src/main/java/sakuraba/saki/player/music/PlayViewModel.kt
@@ -6,20 +6,7 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class PlayViewModel: ViewModel() {
-    
-    private val _progress = MutableLiveData<Long>()
-    fun updateProgress(newProgress: Long) {
-        _progress.value = newProgress
-    }
-    val progress get() = _progress as LiveData<Long>
-    val progressValue get() = progress.value!!
-    
-    private val _duration = MutableLiveData<Long>()
-    fun updateDuration(newDuration: Long) {
-        _duration.value = newDuration
-    }
-    val duration get() = _duration as LiveData<Long>
-    
+
     private val _state = MutableLiveData<Int>()
     fun updateState(newState: Int) {
         if (newState != STATE_BUFFERING && _state.value != newState) {
