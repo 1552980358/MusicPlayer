da4b8d863059b7927f73bf550545aa9666d6537d
1552980358 <1552980358@qq.com>
2022-03-08 22:45:56 +0800

    MainActivity.kt: Update loading audio data from system database

--- app/src/main/java/projekt/cloud/piece/music/player/MainActivity.kt
+++ app/src/main/java/projekt/cloud/piece/music/player/MainActivity.kt
@@ -1,6 +1,17 @@
 package projekt.cloud.piece.music.player
 
 import android.os.Bundle
+import android.provider.MediaStore
+import android.provider.MediaStore.Audio.AudioColumns._ID
+import android.provider.MediaStore.Audio.AudioColumns.ARTIST
+import android.provider.MediaStore.Audio.AudioColumns.ALBUM_ID
+import android.provider.MediaStore.Audio.AudioColumns.ARTIST_ID
+import android.provider.MediaStore.Audio.AudioColumns.DATA
+import android.provider.MediaStore.Audio.AudioColumns.DURATION
+import android.provider.MediaStore.Audio.AudioColumns.IS_MUSIC
+import android.provider.MediaStore.Audio.AudioColumns.SIZE
+import android.provider.MediaStore.Audio.AudioColumns.TITLE
+import android.provider.MediaStore.Audio.Media.EXTERNAL_CONTENT_URI
 import android.view.Menu
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
@@ -10,6 +21,13 @@ import androidx.navigation.ui.setupWithNavController
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
+import androidx.room.Room.databaseBuilder
+import lib.github1552980358.ktExtension.kotlinx.coroutines.io
+import projekt.cloud.piece.music.player.database.AudioDatabase
+import projekt.cloud.piece.music.player.database.AudioDatabase.Companion.DATABASE_NAME
+import projekt.cloud.piece.music.player.database.item.AlbumItem
+import projekt.cloud.piece.music.player.database.item.ArtistItem
+import projekt.cloud.piece.music.player.database.item.AudioItem
 import projekt.cloud.piece.music.player.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -33,6 +51,57 @@ class MainActivity : AppCompatActivity() {
 
         setupActionBarWithNavController(navController, appBarConfiguration)
         binding.navView.setupWithNavController(navController)
+    }
+
+    private fun initialApplication() = io {
+        loadFromDatabase()
+        audioDatabase.audio.query()
+        audioDatabase.album.query()
+        audioDatabase.artist.query()
+    }
+
+    private fun loadFromDatabase() {
+        val audioList = arrayListOf<AudioItem>()
+        val albumList = arrayListOf<AlbumItem>()
+        val artistList = arrayListOf<ArtistItem>()
+        querySystemDatabase(audioList, albumList, artistList)
+        audioDatabase.artist.insert(*artistList.toTypedArray())
+        audioDatabase.album.insert(*albumList.toTypedArray())
+        audioDatabase.audio.insert(*audioList.toTypedArray())
+    }
+
+    private fun querySystemDatabase(audioList: ArrayList<AudioItem>, albumList: ArrayList<AlbumItem>, artistList: ArrayList<ArtistItem>) {
+        arrayListOf<AudioItem>().apply {
+            contentResolver.query(EXTERNAL_CONTENT_URI, null, null, null, IS_MUSIC)?.apply {
+                var artist: String
+                var album: String
+                while (moveToNext()) {
+                    artist = getString(getColumnIndexOrThrow(ARTIST_ID))
+                    if (artistList.find { it.id == artist } == null) {
+                        artistList.add(ArtistItem(artist, getString(getColumnIndexOrThrow(ARTIST))))
+                    }
+                    album = getString(getColumnIndexOrThrow(ALBUM_ID))
+                    if (albumList.find { it.id == album} == null) {
+                        albumList.add(AlbumItem(
+                            album,
+                            getString(getColumnIndexOrThrow(MediaStore.Audio.AudioColumns.ALBUM))
+                        ))
+                    }
+                    audioList.add(
+                        AudioItem(
+                            id = getString(getColumnIndexOrThrow(_ID)),
+                            title = getString(getColumnIndexOrThrow(TITLE)),
+                            artist = artist,
+                            album = album,
+                            duration = getLong(getColumnIndexOrThrow(DURATION)),
+                            size = getLong(getColumnIndexOrThrow(SIZE)),
+                            path = getString(getColumnIndexOrThrow(DATA))
+                        )
+                    )
+                }
+                close()
+            }
+        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
