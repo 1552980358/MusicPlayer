fbfd52ff913f72e126c4e89b8b12feced4b16130
1552980358 <1552980358@qq.com>
2022-03-09 02:06:18 +0800

    HomeFragment.kt: Implement RecyclerView

--- app/src/main/java/projekt/cloud/piece/music/player/ui/home/HomeFragment.kt
+++ app/src/main/java/projekt/cloud/piece/music/player/ui/home/HomeFragment.kt
@@ -4,16 +4,54 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.Fragment
+import androidx.core.graphics.drawable.toBitmap
+import lib.github1552980358.ktExtension.androidx.fragment.app.getDrawable
+import projekt.cloud.piece.music.player.R
+import projekt.cloud.piece.music.player.base.BaseMainFragment
 import projekt.cloud.piece.music.player.databinding.FragmentHomeBinding
+import projekt.cloud.piece.music.player.ui.home.util.RecyclerViewAdapterUtil
 
 class HomeFragment : BaseMainFragment() {
 
     private var _binding: FragmentHomeBinding? = null
     private val binding get() = _binding!!
 
+    private val audioList get() = activityInterface.audioList
+    private val artistList get() = activityInterface.artistList
+    private val albumList get() = activityInterface.albumList
+    private val audioBitmap40DpMap get() = activityInterface.audioBitmap40DpMap
+    private val albumBitmap40DpMap get() = activityInterface.albumBitmap40DpMap
+
+    private lateinit var recyclerViewAdapterUtil: RecyclerViewAdapterUtil
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
+    
+        binding.isRefreshing = true
+
+        activityInterface.setRefreshListener(
+            refreshStageChanged = {
+                recyclerViewAdapterUtil = RecyclerViewAdapterUtil(binding.recyclerView,
+                    audioList,
+                    artistList,
+                    albumList,
+                    getDrawable(R.drawable.ic_music, null)!!.toBitmap(),
+                    audioBitmap40DpMap,
+                    albumBitmap40DpMap,
+                    rootClick = {
+                    
+                    },
+                    optionClick = { index, relativeLayout ->
+                    
+                    }
+                )
+            },
+            refreshCompleted = {
+                recyclerViewAdapterUtil.notifyUpdate()
+                binding.isRefreshing = false
+            }
+        )
+
         return binding.root
     }
 
