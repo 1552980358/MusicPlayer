5e92f5b43a830e09ca710f30a16d8e4f0fd1bc99
1552980358 <1552980358@qq.com>
2022-03-08 23:09:34 +0800

    MainActivity.kt: Update initial methods

--- app/src/main/java/projekt/cloud/piece/music/player/MainActivity.kt
+++ app/src/main/java/projekt/cloud/piece/music/player/MainActivity.kt
@@ -1,27 +1,35 @@
 package projekt.cloud.piece.music.player
 
+import android.Manifest.permission.READ_EXTERNAL_STORAGE
+import android.content.pm.PackageManager.PERMISSION_GRANTED
+import android.graphics.Bitmap
+import android.graphics.Matrix
 import android.os.Bundle
 import android.provider.MediaStore
+import android.provider.MediaStore.Audio.ArtistColumns.ARTIST
 import android.provider.MediaStore.Audio.AudioColumns._ID
-import android.provider.MediaStore.Audio.AudioColumns.ARTIST
 import android.provider.MediaStore.Audio.AudioColumns.ALBUM_ID
 import android.provider.MediaStore.Audio.AudioColumns.ARTIST_ID
 import android.provider.MediaStore.Audio.AudioColumns.DATA
-import android.provider.MediaStore.Audio.AudioColumns.DURATION
 import android.provider.MediaStore.Audio.AudioColumns.IS_MUSIC
 import android.provider.MediaStore.Audio.AudioColumns.SIZE
 import android.provider.MediaStore.Audio.AudioColumns.TITLE
 import android.provider.MediaStore.Audio.Media.EXTERNAL_CONTENT_URI
+import android.provider.MediaStore.MediaColumns.DURATION
 import android.view.Menu
-import androidx.navigation.findNavController
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat.checkSelfPermission
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.room.Room.databaseBuilder
+import lib.github1552980358.ktExtension.android.graphics.heightF
+import lib.github1552980358.ktExtension.android.graphics.widthF
+import lib.github1552980358.ktExtension.jvm.keyword.tryRun
 import lib.github1552980358.ktExtension.kotlinx.coroutines.io
 import projekt.cloud.piece.music.player.database.AudioDatabase
 import projekt.cloud.piece.music.player.database.AudioDatabase.Companion.DATABASE_NAME
@@ -29,6 +37,10 @@ import projekt.cloud.piece.music.player.database.item.AlbumItem
 import projekt.cloud.piece.music.player.database.item.ArtistItem
 import projekt.cloud.piece.music.player.database.item.AudioItem
 import projekt.cloud.piece.music.player.databinding.ActivityMainBinding
+import projekt.cloud.piece.music.player.util.ImageUtil.loadAlbumArt
+import projekt.cloud.piece.music.player.util.ImageUtil.writeAlbumArt40Dp
+import projekt.cloud.piece.music.player.util.ImageUtil.writeAlbumArtRaw
+import projekt.cloud.piece.music.player.util.MainActivityInterface
 
 class MainActivity : AppCompatActivity() {
 
@@ -38,7 +50,16 @@ class MainActivity : AppCompatActivity() {
     private val fragmentContainerView get() = binding.appBarMain.contentMain.fragmentContainerView
     private lateinit var navController: NavController
 
+    private lateinit var activityInterface: MainActivityInterface
+    private val albumByteArrayRawMap get() = activityInterface.albumByteArrayRawMap
+    private val albumBitmap40DpMap get() = activityInterface.albumBitmap40DpMap
+
+    private lateinit var audioDatabase: AudioDatabase
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
+        activityInterface = MainActivityInterface()
+
         super.onCreate(savedInstanceState)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -51,23 +72,52 @@ class MainActivity : AppCompatActivity() {
 
         setupActionBarWithNavController(navController, appBarConfiguration)
         binding.navView.setupWithNavController(navController)
+
+        audioDatabase = databaseBuilder(this, AudioDatabase::class.java, DATABASE_NAME).build()
+
+        val requestPermission = registerForActivityResult(ActivityResultContracts.RequestPermission()) {
+            when {
+                it -> initialApplication()
+            }
+        }
+
+        when (checkSelfPermission(this, READ_EXTERNAL_STORAGE)) {
+            PERMISSION_GRANTED -> {}
+            else -> requestPermission.launch(READ_EXTERNAL_STORAGE)
+        }
+
     }
 
     private fun initialApplication() = io {
-        loadFromDatabase()
-        audioDatabase.audio.query()
-        audioDatabase.album.query()
-        audioDatabase.artist.query()
-    }
-
-    private fun loadFromDatabase() {
         val audioList = arrayListOf<AudioItem>()
         val albumList = arrayListOf<AlbumItem>()
         val artistList = arrayListOf<ArtistItem>()
+        loadFromDatabase(audioList, albumList, artistList)
+        initialImage(albumList)
+    }
+
+    private fun launchApplication() = io {
+        loadDatabase()
+    }
+
+    private fun loadFromDatabase(audioList: ArrayList<AudioItem>, albumList: ArrayList<AlbumItem>, artistList: ArrayList<ArtistItem>) {
         querySystemDatabase(audioList, albumList, artistList)
         audioDatabase.artist.insert(*artistList.toTypedArray())
         audioDatabase.album.insert(*albumList.toTypedArray())
         audioDatabase.audio.insert(*audioList.toTypedArray())
+    }
+
+    private fun initialImage(albumList: ArrayList<AlbumItem>) {
+        var bitmap: Bitmap?
+        val matrix = Matrix()
+        val bitmapSize = resources.getDimensionPixelSize(R.dimen.dp_40)
+        for (albumItem in albumList) {
+            bitmap = tryRun { loadAlbumArt(albumItem.id) } ?: continue
+            albumByteArrayRawMap[albumItem.id] = writeAlbumArtRaw(albumItem.id, bitmap)
+            matrix.apply { setScale(bitmapSize / bitmap.widthF, bitmapSize / bitmap.heightF) }
+            writeAlbumArt40Dp(albumItem.id, bitmap)
+            albumBitmap40DpMap[albumItem.id] = bitmap
+        }
     }
 
     private fun querySystemDatabase(audioList: ArrayList<AudioItem>, albumList: ArrayList<AlbumItem>, artistList: ArrayList<ArtistItem>) {
@@ -104,8 +154,13 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    private fun loadDatabase() {
+        activityInterface.audioList = audioDatabase.audio.query()
+        // activityInterface.artistList = audioDatabase.artist.query()
+        activityInterface.albumList = audioDatabase.album.query()
+    }
+
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
         menuInflater.inflate(R.menu.main, menu)
         return true
     }
