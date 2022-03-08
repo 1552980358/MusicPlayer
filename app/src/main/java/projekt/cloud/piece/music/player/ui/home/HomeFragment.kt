f068bdc737db9b213405a15339351ac569b54826
1552980358 <1552980358@qq.com>
2022-03-09 02:05:29 +0800

    HomeFragment.kt: Inherit to BaseMainFragment

--- app/src/main/java/projekt/cloud/piece/music/player/ui/home/HomeFragment.kt
+++ app/src/main/java/projekt/cloud/piece/music/player/ui/home/HomeFragment.kt
@@ -7,7 +7,7 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import projekt.cloud.piece.music.player.databinding.FragmentHomeBinding
 
-class HomeFragment : Fragment() {
+class HomeFragment : BaseMainFragment() {
 
     private var _binding: FragmentHomeBinding? = null
     private val binding get() = _binding!!
