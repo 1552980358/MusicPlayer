f8dd79e1fc10a417ad7d541dfe41ca9b40121bf4
1552980358 <1552980358@qq.com>
2022-03-08 22:44:31 +0800

    database: Entity items implement Serializable

--- app/src/main/java/projekt/cloud/piece/music/player/database/item/ArtistItem.kt
+++ app/src/main/java/projekt/cloud/piece/music/player/database/item/ArtistItem.kt
@@ -4,10 +4,11 @@ import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 import projekt.cloud.piece.c2pinyin.pinyin
+import java.io.Serializable
 
 @Entity(tableName = "artist")
 data class ArtistItem(
     @PrimaryKey @ColumnInfo(name = "id") val id: String,
     @ColumnInfo(name = "name") val name: String,
     @ColumnInfo(name = "pinyin") val pinyin: String = name.pinyin
-)
+): Serializable
