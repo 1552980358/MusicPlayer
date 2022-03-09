cd21d545f5a636bf348c55f86f47aaf81aa34391
1552980358 <1552980358@qq.com>
2022-03-09 19:50:30 +0800

    Constant.kt: Update constants

--- app/src/main/java/projekt/cloud/piece/music/player/util/Constant.kt
+++ app/src/main/java/projekt/cloud/piece/music/player/util/Constant.kt
@@ -5,4 +5,7 @@ object Constant {
     const val ANIMATION_DURATION = 500
     const val ANIMATION_DURATION_LONG = 500L
     
+    const val ANIMATION_DURATION_HALF = 250
+    const val ANIMATION_DURATION_HALF_LONG = 250L
+    
 }
