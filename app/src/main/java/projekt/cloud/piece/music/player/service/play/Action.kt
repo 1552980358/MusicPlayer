c8205e14e9f2d07dd6978664550192e6e2d48b8a
1552980358 <1552980358@qq.com>
2022-03-09 13:35:07 +0800

    Action.kt: Update constants

---
+++ app/src/main/java/projekt/cloud/piece/music/player/service/play/Action.kt
@@ -0,0 +1,11 @@
+package projekt.cloud.piece.music.player.service.play
+
+object Action {
+
+    const val START_COMMAND_ACTION = "START_COMMAND_ACTION"
+    const val START_COMMAND_ACTION_PLAY = "play"
+    const val START_COMMAND_ACTION_PAUSE = "pause"
+    
+    
+
+}
