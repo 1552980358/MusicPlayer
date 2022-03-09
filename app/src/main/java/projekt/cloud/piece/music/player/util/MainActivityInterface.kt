385f713612d9fab97024157e7122490f71e24cd3
1552980358 <1552980358@qq.com>
2022-03-09 19:51:49 +0800

    MainActivityInterface.kt: Update variable and method

--- app/src/main/java/projekt/cloud/piece/music/player/util/MainActivityInterface.kt
+++ app/src/main/java/projekt/cloud/piece/music/player/util/MainActivityInterface.kt
@@ -1,12 +1,13 @@
 package projekt.cloud.piece.music.player.util
 
 import android.graphics.Bitmap
+import android.support.v4.media.MediaBrowserCompat
 import projekt.cloud.piece.music.player.database.item.AlbumItem
 import projekt.cloud.piece.music.player.database.item.ArtistItem
 import projekt.cloud.piece.music.player.database.item.AudioItem
 import projekt.cloud.piece.music.player.database.item.PlaylistItem
 
-class MainActivityInterface(val itemClick: () -> Unit, val requestRefresh: () -> Unit) {
+class MainActivityInterface(val itemClick: (Int) -> Unit, val requestRefresh: () -> Unit) {
 
     lateinit var audioList: List<AudioItem>
     lateinit var playlistList: List<PlaylistItem>
@@ -23,6 +24,11 @@ class MainActivityInterface(val itemClick: () -> Unit, val requestRefresh: () ->
     fun setRefreshListener(refreshStageChanged: () -> Unit, refreshCompleted: () -> Unit) {
         this.refreshStageChanged = refreshStageChanged
         this.refreshCompleted = refreshCompleted
+    }
+    
+    private lateinit var mediaBrowserCompat: MediaBrowserCompat
+    fun setMediaBrowserCompat(mediaBrowserCompat: MediaBrowserCompat) {
+        this.mediaBrowserCompat = mediaBrowserCompat
     }
     
     lateinit var defaultAudioImage: Bitmap
