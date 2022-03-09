620e04a6f5eea851706115edaa9fc68605b6c264
1552980358 <1552980358@qq.com>
2022-03-09 17:14:53 +0800

    DataBindingUtil.kt: Update methods

--- app/src/main/java/projekt/cloud/piece/music/player/util/DataBindingUtil.kt
+++ app/src/main/java/projekt/cloud/piece/music/player/util/DataBindingUtil.kt
@@ -1,7 +1,19 @@
 package projekt.cloud.piece.music.player.util
 
+import android.animation.ValueAnimator.ofFloat
+import android.graphics.Bitmap
+import android.graphics.drawable.AnimatedVectorDrawable
+import android.graphics.drawable.BitmapDrawable
+import android.graphics.drawable.TransitionDrawable
+import androidx.annotation.DrawableRes
+import androidx.appcompat.widget.AppCompatImageButton
+import androidx.appcompat.widget.AppCompatImageView
+import androidx.appcompat.widget.AppCompatTextView
+import androidx.core.animation.doOnEnd
 import androidx.databinding.BindingAdapter
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
+import projekt.cloud.piece.music.player.util.Constant.ANIMATION_DURATION
+import projekt.cloud.piece.music.player.util.Constant.ANIMATION_DURATION_LONG
 
 object DataBindingUtil {
     
@@ -9,6 +21,48 @@ object DataBindingUtil {
     @BindingAdapter("app:isRefreshing")
     fun SwipeRefreshLayout.setRefresh(isRefreshing: Boolean) {
         this.isRefreshing = isRefreshing
+    }
+    
+    @JvmStatic
+    @BindingAdapter("app:drawable")
+    fun AppCompatImageButton.playbackStateDrawable(@DrawableRes drawableRes: Int) {
+        setImageResource(drawableRes)
+        (drawable as AnimatedVectorDrawable).start()
+    }
+    
+    @JvmStatic
+    @BindingAdapter("app:textAnimated")
+    fun AppCompatTextView.setTextAnimated(newText: String) {
+        ofFloat(1F, 0F).apply {
+            duration = ANIMATION_DURATION_LONG
+            addUpdateListener { alpha = animatedValue as Float }
+            doOnEnd {
+                text = newText
+                ofFloat(0F, 1F).apply {
+                    duration = ANIMATION_DURATION_LONG
+                    addUpdateListener { alpha = animatedValue as Float }
+                }.start()
+            }
+        }.start()
+    }
+    
+    @JvmStatic
+    @BindingAdapter("app:bitmapAnimated")
+    fun AppCompatImageView.setBitmapAnimated(bitmap: Bitmap) {
+        if (drawable == null) {
+            return setImageBitmap(bitmap)
+        }
+        TransitionDrawable(
+            arrayOf(
+                when (val lastDrawable = drawable) {
+                    is TransitionDrawable -> lastDrawable.getDrawable(1)
+                    else -> drawable
+                },
+                BitmapDrawable(resources, bitmap))
+        ).apply {
+            setImageDrawable(this)
+            startTransition(ANIMATION_DURATION)
+        }
     }
     
 }
