08108e20dd192267406ec63af33405dd31a560dc
1552980358 <1552980358@qq.com>
2022-03-11 17:14:49 +0800

    TimeView.kt: Update TimeView

---
+++ app/src/main/java/projekt/cloud/piece/music/player/widget/TimeView.kt
@@ -0,0 +1,56 @@
+package projekt.cloud.piece.music.player.widget
+
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
+import android.util.AttributeSet
+import android.view.View
+import androidx.annotation.ColorInt
+import lib.github1552980358.ktExtension.android.view.heightF
+import lib.github1552980358.ktExtension.android.view.widthF
+import projekt.cloud.piece.music.player.R
+
+class TimeView(context: Context, attributeSet: AttributeSet?): View(context, attributeSet) {
+    
+    private companion object {
+         const val COLON = ":"
+    }
+    
+    private val paint = Paint()
+    private val length: Float
+    
+    init {
+        paint.isAntiAlias = true
+        paint.color = Color.WHITE
+        paint.textSize = resources.getDimension(R.dimen.view_duration_text_size)
+        length = paint.measureText(COLON)
+    }
+    
+    var duration = 0L
+        set(value) {
+            field = value
+            invalidate()
+        }
+    
+    fun updateTextColor(@ColorInt newColor: Int) {
+        paint.color = newColor
+        invalidate()
+    }
+    
+    override fun onDraw(canvas: Canvas?) {
+        super.onDraw(canvas)
+        canvas ?: return
+        
+        val drawY = heightF - paint.fontMetrics.bottom
+        // Draw colon first
+        canvas.drawText(COLON, (widthF - length) / 2, drawY, paint)
+        var text = (duration / 60000).addZero
+        canvas.drawText(text, (widthF - length) / 2 - paint.measureText(text), drawY, paint)
+        text = (duration / 1000 % 60).addZero
+        canvas.drawText(text, (widthF + length) / 2, drawY, paint)
+    }
+    
+    private val Long.addZero get() = if (this < 10) { "0$this" } else { this.toString() }
+    
+}
