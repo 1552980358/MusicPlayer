56ee91fd0556db339658d3d0fe1e5cab9d6275de
1552980358 <1552980358@qq.com>
2022-03-11 14:40:09 +0800

    PlayFragment.kt: Update and implement play config updating

--- app/src/main/java/projekt/cloud/piece/music/player/ui/play/PlayFragment.kt
+++ app/src/main/java/projekt/cloud/piece/music/player/ui/play/PlayFragment.kt
@@ -1,12 +1,17 @@
 package projekt.cloud.piece.music.player.ui.play
 
+import android.animation.ValueAnimator.ofArgb
+import android.graphics.Color.BLACK
+import android.graphics.Color.WHITE
 import android.graphics.drawable.RippleDrawable
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.MotionEvent.ACTION_CANCEL
 import android.view.MotionEvent.ACTION_DOWN
 import android.view.MotionEvent.ACTION_UP
 import android.view.View
 import android.view.ViewGroup
+import androidx.cardview.widget.CardView
 import androidx.databinding.DataBindingUtil
 import com.google.android.material.bottomsheet.BottomSheetBehavior
 import com.google.android.material.bottomsheet.BottomSheetBehavior.STATE_COLLAPSED
@@ -19,7 +24,17 @@ import lib.github1552980358.ktExtension.kotlinx.coroutines.io
 import lib.github1552980358.ktExtension.kotlinx.coroutines.ui
 import projekt.cloud.piece.music.player.R
 import projekt.cloud.piece.music.player.base.BasePlayFragment
+import projekt.cloud.piece.music.player.database.item.AudioItem
 import projekt.cloud.piece.music.player.databinding.FragmentPlayBinding
+import projekt.cloud.piece.music.player.service.play.Config.PLAY_CONFIG_REPEAT
+import projekt.cloud.piece.music.player.service.play.Config.PLAY_CONFIG_REPEAT_ONE
+import projekt.cloud.piece.music.player.service.play.Config.PLAY_CONFIG_SHUFFLE
+import projekt.cloud.piece.music.player.service.play.Config.getConfig
+import projekt.cloud.piece.music.player.service.play.Config.setConfig
+import projekt.cloud.piece.music.player.util.ActivityUtil.pixelHeight
+import projekt.cloud.piece.music.player.util.Constant.ANIMATION_DURATION_HALF_LONG
+import projekt.cloud.piece.music.player.util.Constant.ANIMATION_DURATION_LONG
+import projekt.cloud.piece.music.player.util.ContextUtil.navigationBarHeight
 
 class PlayFragment: BasePlayFragment() {
     
@@ -51,8 +66,11 @@ class PlayFragment: BasePlayFragment() {
         loadMetadata(activityInterface.requestMetadata())
         
         activityInterface.setListener(
-            loadMetadata = { audioItem ->
-            
+            loadMetadata = { audioItem -> loadMetadata(audioItem) },
+            updatePlayConfig = { playConfig ->
+                if (contentPlayFragmentButtons.playConfig != playConfig) {
+                    contentPlayFragmentButtons.playConfig = playConfig
+                }
             },
             loadBitmap = { bitmap -> binding.coverImage = bitmap },
             loadColor = { primaryColor, secondaryColor ->  },
@@ -121,11 +139,53 @@ class PlayFragment: BasePlayFragment() {
                 }
                 ACTION_UP -> {
                     binding.relativeLayout.isPressed = false
+                    val touchX = motionEvent.rawX
+                    val touchY = motionEvent.rawY
+                    val rawAxis = IntArray(2)
+                    when {
+                        compareAxis(touchX, touchY, imageViewCycle, rawAxis) -> {
+                            contentPlayFragmentButtons.playConfig?.let { playConfig ->
+                                val repeat = playConfig.getConfig(PLAY_CONFIG_REPEAT)
+                                val repeatOne = playConfig.getConfig(PLAY_CONFIG_REPEAT_ONE)
+                                when {
+                                    // Repeat with list
+                                    repeat && !repeatOne -> activityInterface.changePlayConfig(
+                                        playConfig.setConfig(PLAY_CONFIG_REPEAT, false)
+                                            .setConfig(PLAY_CONFIG_REPEAT_ONE, true)
+                                    )
+                                    // Repeat with single audio
+                                    !repeat && repeatOne ->
+                                        activityInterface.changePlayConfig(playConfig.setConfig(PLAY_CONFIG_REPEAT_ONE, false))
+                                    // Play list, no repeat
+                                    !repeat && !repeatOne ->
+                                        activityInterface.changePlayConfig(playConfig.setConfig(PLAY_CONFIG_REPEAT, true))
+                                }
+                            }
+                        }
+                        compareAxis(touchX, touchY, imageViewPrev, rawAxis) -> {
+                        }
+                        compareAxis(touchX, touchY, imageViewNext, rawAxis) -> {
+                        }
+                        compareAxis(touchX, touchY, imageViewShuffle, rawAxis) -> {
+                            contentPlayFragmentButtons.playConfig?.let { playConfig ->
+                                activityInterface.changePlayConfig(playConfig.setConfig(PLAY_CONFIG_SHUFFLE, !playConfig.getConfig(PLAY_CONFIG_SHUFFLE)))
+                            }
+                        }
+                    }
                 }
+                ACTION_CANCEL -> { binding.relativeLayout.isPressed = false }
             }
             return@setOnTouchListener true
         }
     }
+    
+    private fun compareAxis(x: Float, y: Float, view: View, rawAxis: IntArray): Boolean {
+        view.getLocationOnScreen(rawAxis)
+        return compareAxis(x, rawAxis.first().toFloat(), view.widthF) && compareAxis(y, rawAxis.last().toFloat(), view.heightF)
+    }
+    
+    private fun compareAxis(axis: Float, targetX: Float, length: Float) =
+        axis in (targetX .. targetX + length)
     
     override fun onStart() {
         super.onStart()
