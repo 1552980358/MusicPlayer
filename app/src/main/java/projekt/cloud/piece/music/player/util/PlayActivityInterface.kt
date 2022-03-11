c7e8f7ef7f26e3bb380d073e57f984a784955aab
1552980358 <1552980358@qq.com>
2022-03-11 14:37:26 +0800

    PlayActivityInterface.kt: Update methods

--- app/src/main/java/projekt/cloud/piece/music/player/util/PlayActivityInterface.kt
+++ app/src/main/java/projekt/cloud/piece/music/player/util/PlayActivityInterface.kt
@@ -3,14 +3,25 @@ package projekt.cloud.piece.music.player.util
 import android.graphics.Bitmap
 import projekt.cloud.piece.music.player.database.item.AudioItem
 
-class PlayActivityInterface(val requestMetadata: () -> AudioItem) {
+class PlayActivityInterface(val requestMetadata: () -> AudioItem,
+                            val changePlayConfig: (Int) -> Unit) {
 
     lateinit var loadMetadata: (AudioItem) -> Unit
+    lateinit var updatePlayConfig: (Int) -> Unit
     lateinit var loadBitmap: (Bitmap) -> Unit
+    lateinit var loadColor: (Int, Int) -> Unit
+    lateinit var loadIsLight: (Boolean) -> Unit
     
-    fun setListener(loadMetadata: (AudioItem) -> Unit, loadBitmap: (Bitmap) -> Unit) {
+    fun setListener(loadMetadata: (AudioItem) -> Unit,
+                    updatePlayConfig: (Int) -> Unit,
+                    loadBitmap: (Bitmap) -> Unit,
+                    loadColor: (Int, Int) -> Unit,
+                    loadIsLight: (Boolean) -> Unit) {
         this.loadMetadata = loadMetadata
+        this.updatePlayConfig = updatePlayConfig
         this.loadBitmap = loadBitmap
+        this.loadColor = loadColor
+        this.loadIsLight = loadIsLight
     }
 
 }
