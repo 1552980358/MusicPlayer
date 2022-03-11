dfb1f85b8ea9039b0a7074b6a34584e851f243d0
1552980358 <1552980358@qq.com>
2022-03-11 14:35:36 +0800

    PlayFragment.kt: Update and implement buttons and bottom sheet

--- app/src/main/java/projekt/cloud/piece/music/player/ui/play/PlayFragment.kt
+++ app/src/main/java/projekt/cloud/piece/music/player/ui/play/PlayFragment.kt
@@ -8,6 +8,15 @@ import android.view.MotionEvent.ACTION_UP
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import com.google.android.material.bottomsheet.BottomSheetBehavior
+import com.google.android.material.bottomsheet.BottomSheetBehavior.STATE_COLLAPSED
+import com.google.android.material.bottomsheet.BottomSheetBehavior.STATE_HIDDEN
+import kotlinx.coroutines.delay
+import lib.github1552980358.ktExtension.android.content.getStatusBarHeight
+import lib.github1552980358.ktExtension.android.view.heightF
+import lib.github1552980358.ktExtension.android.view.widthF
+import lib.github1552980358.ktExtension.kotlinx.coroutines.io
+import lib.github1552980358.ktExtension.kotlinx.coroutines.ui
 import projekt.cloud.piece.music.player.R
 import projekt.cloud.piece.music.player.base.BasePlayFragment
 import projekt.cloud.piece.music.player.databinding.FragmentPlayBinding
@@ -16,6 +25,19 @@ class PlayFragment: BasePlayFragment() {
     
     private var _binding: FragmentPlayBinding? = null
     private val binding get() = _binding!!
+    
+    private val contentPlayFragmentBottomSheet get() =
+        binding.contentPlayFragmentBottomSheet
+    
+    private val contentPlayFragmentButtons get() =
+        binding.contentPlayFragmentButtons
+    
+    private val imageViewCycle get() = contentPlayFragmentButtons.imageViewCycle
+    private val imageViewPrev get() = contentPlayFragmentButtons.imageViewPrev
+    private val imageViewNext get() = contentPlayFragmentButtons.imageViewNext
+    private val imageViewShuffle get() = contentPlayFragmentButtons.imageViewShuffle
+    
+    private lateinit var bottomSheetBehavior: BottomSheetBehavior<CardView>
     
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         _binding = DataBindingUtil.inflate(layoutInflater, R.layout.fragment_play, container, false)
@@ -38,7 +60,27 @@ class PlayFragment: BasePlayFragment() {
     
     @Suppress("ClickableViewAccessibility")
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        binding.contentPlayFragmentButtons.linearLayout.setOnTouchListener { _, motionEvent ->
+        contentPlayFragmentBottomSheet.cardView.apply {
+            layoutParams = layoutParams.apply {
+                height = pixelHeight - requireContext().getStatusBarHeight()
+            }
+            setContentPadding(0, 0, 0, navigationBarHeight)
+        }
+    
+        val bottomHeight = pixelHeight - resources.displayMetrics.widthPixels
+        
+        contentPlayFragmentButtons.linearLayout.apply {
+            layoutParams = layoutParams.apply { height = bottomHeight * 2 / 5 }
+        }
+        
+        bottomSheetBehavior = BottomSheetBehavior.from(contentPlayFragmentBottomSheet.cardView)
+        with(bottomSheetBehavior) {
+            peekHeight = bottomHeight * 3 / 5
+            isHideable = true
+            state = STATE_HIDDEN
+        }
+        
+        contentPlayFragmentButtons.linearLayout.setOnTouchListener { _, motionEvent ->
             when (motionEvent.action) {
                 ACTION_DOWN -> {
                     binding.relativeLayout.apply {
@@ -52,6 +94,22 @@ class PlayFragment: BasePlayFragment() {
             }
             return@setOnTouchListener true
         }
+    }
+    
+    override fun onStart() {
+        super.onStart()
+        io {
+            delay(ANIMATION_DURATION_HALF_LONG)
+            ui {
+                bottomSheetBehavior.state = STATE_COLLAPSED
+                bottomSheetBehavior.isHideable = false
+            }
+        }
+    }
+    
+    private fun loadMetadata(audioItem: AudioItem) {
+        contentPlayFragmentBottomSheet.title = audioItem.title
+        contentPlayFragmentBottomSheet.subtitle = "${audioItem.artistItem.name} - ${audioItem.albumItem.title}"
     }
 
 }
