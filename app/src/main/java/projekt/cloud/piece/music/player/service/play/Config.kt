e3bd413f665c4ec9384555aa4bed18b334a3193c
1552980358 <1552980358@qq.com>
2022-03-11 14:25:07 +0800

    Config.kt: Update methods

--- app/src/main/java/projekt/cloud/piece/music/player/service/play/Config.kt
+++ app/src/main/java/projekt/cloud/piece/music/player/service/play/Config.kt
@@ -2,24 +2,33 @@ package projekt.cloud.piece.music.player.service.play
 
 object Config {
 
+    @JvmStatic
     fun Int.getConfig(config: Int): Boolean =
-        (this and (1 shl config)) == (1 shl config)
+        ((this shr config) and 1) == 1
     
+    @JvmStatic
     fun Int.setConfig(config: Int, newValue: Boolean) = when {
-        newValue -> this or (1 shl config)
-        else -> this and (1 shl config).inv()
+        newValue -> this or config.shl
+        else -> this and config.shl.inv()
     }
     
+    @JvmStatic
     fun getConfigs(vararg pairs: Pair<Int, Boolean>): Int {
         var temp = 0
         pairs.forEach { pair ->
             if (pair.second) {
-                temp += (1 shl pair.first)
+                temp += pair.first.shl
             }
         }
         return temp
     }
     
     const val FOREGROUND_SERVICE = 0
+    
+    val Int.shl get() = (1 shl this)
+    
+    const val PLAY_CONFIG_SHUFFLE = 0
+    const val PLAY_CONFIG_REPEAT = 1
+    const val PLAY_CONFIG_REPEAT_ONE = 2
 
 }
