8e7e45e228c060b4b009bfce8a62906e9b4ea9d3
1552980358 <1552980358@qq.com>
2022-03-12 13:09:37 +0800

    PlayActivityInterface.kt: Update variables and methods

--- app/src/main/java/projekt/cloud/piece/music/player/util/PlayActivityInterface.kt
+++ app/src/main/java/projekt/cloud/piece/music/player/util/PlayActivityInterface.kt
@@ -2,7 +2,9 @@ package projekt.cloud.piece.music.player.util
 
 import android.graphics.Bitmap
 import android.support.v4.media.session.MediaControllerCompat.TransportControls
+import projekt.cloud.piece.music.player.base.BasePlayFragment
 import projekt.cloud.piece.music.player.database.item.AudioItem
+import projekt.cloud.piece.music.player.ui.play.PlayFragment
 
 class PlayActivityInterface(val requestMetadata: () -> AudioItem,
                             val changePlayConfig: (Int) -> Unit) {
@@ -18,22 +20,30 @@ class PlayActivityInterface(val requestMetadata: () -> AudioItem,
     
     lateinit var transportControls: TransportControls
     
-    fun setListener(updateAudioItem: (AudioItem) -> Unit,
-                    updatePlayConfig: (Int) -> Unit,
-                    updateBitmap: (Bitmap) -> Unit,
-                    updateColor: (Int, Int) -> Unit,
-                    updateContrast: (Boolean) -> Unit,
-                    updateProgress: (Long) -> Unit,
-                    updatePlayState: (Boolean) -> Unit,
-                    updateAudioList: (List<AudioItem>) -> Unit) {
-        this.updateAudioItem = updateAudioItem
+    private var currentFragment: BasePlayFragment? = null
+    fun isCurrent(fragment: BasePlayFragment) = currentFragment == fragment
+    val isPlayFragment get() = currentFragment is PlayFragment
+    
+    fun setPlayFragmentListener(updatePlayConfig: (Int) -> Unit,
+                                updateBitmap: (Bitmap) -> Unit,
+                                updateContrast: (Boolean) -> Unit,
+                                updatePlayState: (Boolean) -> Unit,
+                                updateAudioList: (List<AudioItem>) -> Unit) {
         this.updatePlayConfig = updatePlayConfig
         this.updateBitmap = updateBitmap
-        this.updateColor = updateColor
         this.updateContrast = updateContrast
-        this.updateProgress = updateProgress
         this.updatePlayState = updatePlayState
         this.updateAudioList = updateAudioList
+    }
+    
+    fun setCommonListener(fragment: BasePlayFragment,
+                          updateAudioItem: (AudioItem) -> Unit,
+                          updateColor: (Int, Int) -> Unit,
+                          updateProgress: (Long) -> Unit) {
+        this.currentFragment = fragment
+        this.updateAudioItem = updateAudioItem
+        this.updateColor = updateColor
+        this.updateProgress = updateProgress
     }
 
 }
