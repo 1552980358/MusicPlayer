f528b456f7e439a996a8573282297ddaf68a17cc
1552980358 <1552980358@qq.com>
2022-03-13 14:25:18 +0800

    Lyric.kt: Update method indexOf()

--- app/src/main/java/projekt/cloud/piece/music/player/util/Lyric.kt
+++ app/src/main/java/projekt/cloud/piece/music/player/util/Lyric.kt
@@ -26,8 +26,27 @@ class Lyric {
     
     fun sort() = _lyricItemList.sortBy { it.time }
     
-    fun indexOf(position: Long) =
-        _lyricItemList.indexOfFirst { it.time < position } - 1
+    fun indexOf(position: Long): Int {
+        if (isEmpty) {
+            return -2
+        }
+        
+        if (position < _lyricItemList.first().time) {
+            return -2
+        }
+        
+        if (position > _lyricItemList.last().time) {
+            return _lyricItemList.lastIndex
+        }
+        
+        for (i in 1 until _lyricItemList.size) {
+            if (position in _lyricItemList[i - 1].time until _lyricItemList[i].time) {
+                return i - 1
+            }
+        }
+        
+        return -2
+    }
     
     val last get() = _lyricItemList.last()
     
