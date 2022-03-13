ce0660d416f163db3c1ec724f20449c3796d68ab
1552980358 <1552980358@qq.com>
2022-03-13 19:13:55 +0800

    MainActivity.kt: REMOVE BOTTOM SHEET feature

--- app/src/main/java/projekt/cloud/piece/music/player/MainActivity.kt
+++ app/src/main/java/projekt/cloud/piece/music/player/MainActivity.kt
@@ -6,8 +6,6 @@ import android.content.pm.PackageManager.PERMISSION_GRANTED
 import android.graphics.Bitmap
 import android.graphics.Bitmap.createBitmap
 import android.graphics.Matrix
-import android.graphics.Point
-import android.graphics.drawable.RippleDrawable
 import android.os.Bundle
 import android.provider.MediaStore
 import android.provider.MediaStore.Audio.ArtistColumns.ARTIST
@@ -21,22 +19,16 @@ import android.provider.MediaStore.Audio.AudioColumns.SIZE
 import android.provider.MediaStore.Audio.AudioColumns.TITLE
 import android.provider.MediaStore.Audio.Media.EXTERNAL_CONTENT_URI
 import android.support.v4.media.MediaMetadataCompat
-import android.support.v4.media.MediaMetadataCompat.METADATA_KEY_ALBUM_ART_URI
-import android.support.v4.media.MediaMetadataCompat.METADATA_KEY_DURATION
 import android.support.v4.media.MediaMetadataCompat.METADATA_KEY_MEDIA_ID
-import android.support.v4.media.MediaMetadataCompat.METADATA_KEY_TITLE
 import android.support.v4.media.session.PlaybackStateCompat
 import android.support.v4.media.session.PlaybackStateCompat.STATE_BUFFERING
 import android.support.v4.media.session.PlaybackStateCompat.STATE_PAUSED
 import android.support.v4.media.session.PlaybackStateCompat.STATE_PLAYING
 import android.util.Log
 import android.view.Menu
-import android.view.MotionEvent.ACTION_DOWN
-import android.view.MotionEvent.ACTION_UP
 import android.view.View
 import android.widget.RelativeLayout
 import androidx.activity.result.contract.ActivityResultContracts
-import androidx.coordinatorlayout.widget.CoordinatorLayout
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
@@ -52,9 +44,6 @@ import androidx.fragment.app.FragmentManager.FragmentLifecycleCallbacks
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.room.Room.databaseBuilder
-import com.google.android.material.bottomsheet.BottomSheetBehavior
-import com.google.android.material.bottomsheet.BottomSheetBehavior.STATE_COLLAPSED
-import com.google.android.material.bottomsheet.BottomSheetBehavior.STATE_EXPANDED
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 import lib.github1552980358.ktExtension.android.content.intent
@@ -62,7 +51,6 @@ import lib.github1552980358.ktExtension.android.content.isSystemDarkMode
 import lib.github1552980358.ktExtension.android.graphics.heightF
 import lib.github1552980358.ktExtension.android.graphics.widthF
 import lib.github1552980358.ktExtension.android.os.bundle
-import lib.github1552980358.ktExtension.android.view.getDimensionPixelSize
 import lib.github1552980358.ktExtension.jvm.keyword.tryRun
 import lib.github1552980358.ktExtension.kotlinx.coroutines.io
 import lib.github1552980358.ktExtension.kotlinx.coroutines.ui
@@ -78,8 +66,6 @@ import projekt.cloud.piece.music.player.database.item.AudioItem
 import projekt.cloud.piece.music.player.databinding.ActivityMainBinding
 import projekt.cloud.piece.music.player.service.play.Extra.EXTRA_INDEX
 import projekt.cloud.piece.music.player.service.play.Extra.EXTRA_LIST
-import projekt.cloud.piece.music.player.util.Extra.EXTRA_AUDIO_ITEM
-import projekt.cloud.piece.music.player.util.Extra.EXTRA_POINT
 import projekt.cloud.piece.music.player.util.ImageUtil.loadAlbumArt
 import projekt.cloud.piece.music.player.util.ImageUtil.loadAlbumArts40Dp
 import projekt.cloud.piece.music.player.util.ImageUtil.loadAudioArt40Dp
@@ -112,7 +98,6 @@ class MainActivity : BaseMediaControlActivity() {
     
     private val appBarMain get() = binding.appBarMain
     private val contentMain get() = appBarMain.contentMain
-    private val contentBottomSheetMain get() = appBarMain.contentBottomSheetMain
 
     private val fragmentContainerView get() = contentMain.fragmentContainerView
     private lateinit var navController: NavController
@@ -124,8 +109,6 @@ class MainActivity : BaseMediaControlActivity() {
     private val playlistBitmap40DpMap get() = activityInterface.playlistBitmap40DpMap
     
     private lateinit var audioDatabase: AudioDatabase
-    
-    private lateinit var behavior: BottomSheetBehavior<RelativeLayout>
     
     private val transportControls get() = mediaControllerCompat.transportControls
 
@@ -206,10 +189,6 @@ class MainActivity : BaseMediaControlActivity() {
         binding.navView.setupWithNavController(navController)
 
         audioDatabase = databaseBuilder(this, AudioDatabase::class.java, DATABASE_NAME).build()
-    
-        behavior = BottomSheetBehavior.from(contentBottomSheetMain.relativeLayout)
-        behavior.peekHeight = 0
-        behavior.state = STATE_COLLAPSED
         
         initialIsNightMode(isSystemDarkMode)
         
@@ -235,36 +214,6 @@ class MainActivity : BaseMediaControlActivity() {
             else -> requestPermission.launch(READ_EXTERNAL_STORAGE)
         }
         
-        @Suppress("ClickableViewAccessibility")
-        contentBottomSheetMain.relativeLayout.setOnTouchListener { view, motionEvent ->
-            when (motionEvent.action) {
-                ACTION_DOWN -> {
-                    (view.background as RippleDrawable).setHotspot(motionEvent.x, motionEvent.y)
-                    view.isPressed = true
-                }
-                ACTION_UP -> {
-                    view.isPressed = false
-                    @Suppress("SwitchIntDef")
-                    when {
-                        motionEvent.x > contentBottomSheetMain.textView.right -> when (mediaControllerCompat.playbackState.state) {
-                            STATE_PLAYING -> mediaControllerCompat.transportControls.pause()
-                            STATE_PAUSED -> mediaControllerCompat.transportControls.play()
-                        }
-                        else -> {
-                            startActivity(intent(this, PlayActivity::class.java) {
-                                putExtra(EXTRA_AUDIO_ITEM, audioItem)
-                                IntArray(2).apply {
-                                    contentBottomSheetMain.imageViewArt.getLocationOnScreen(this)
-                                    putExtra(EXTRA_POINT, Point(this[0] + contentBottomSheetMain.imageViewArt.width / 2, this[1] + contentBottomSheetMain.imageViewArt.height / 2))
-                                }
-                            })
-                            overridePendingTransition(0, 0)
-                        }
-                    }
-                }
-            }
-            return@setOnTouchListener true
-        }
         
     }
 
@@ -392,12 +341,7 @@ class MainActivity : BaseMediaControlActivity() {
     
     override fun onMetadataChanged(metadata: MediaMetadataCompat?) {
         metadata?.apply {
-            contentBottomSheetMain.title = getString(METADATA_KEY_TITLE)
-            contentBottomSheetMain.bitmap =
-                audioBitmap40DpMap[getString(METADATA_KEY_MEDIA_ID)]
-                    ?: albumBitmap40DpMap[getString(METADATA_KEY_ALBUM_ART_URI)]
-                        ?: activityInterface.defaultAudioImage
-            contentBottomSheetMain.duration = getLong(METADATA_KEY_DURATION)
+            
             audioList.find { it.id == getString(METADATA_KEY_MEDIA_ID) }?.let { audioItem = it }
         }
     }
@@ -407,29 +351,12 @@ class MainActivity : BaseMediaControlActivity() {
             when (playbackState) {
                 STATE_PLAYING -> {
                     startPlaying(state.position)
-                    if (behavior.isDraggable) {
-                        behavior.state = STATE_EXPANDED
-                        behavior.isDraggable = false
-                        contentMain.root.apply {
-                            layoutParams = (layoutParams as CoordinatorLayout.LayoutParams).apply {
-                                setMargins(0, 0, 0, getDimensionPixelSize(R.dimen.main_bottom_sheet_height))
-                            }
-                        }
-                    }
-                    if (contentBottomSheetMain.playbackState != R.drawable.ani_play_pause) {
-                        contentBottomSheetMain.playbackState = R.drawable.ani_play_pause
-                    }
-                     when (contentBottomSheetMain.playbackState) {
-                        null -> contentBottomSheetMain.playbackState = R.drawable.ic_pause
-                        R.drawable.ani_pause_play -> contentBottomSheetMain.playbackState = R.drawable.ani_play_pause
-                    }
+                    
+                    
                 }
                 STATE_PAUSED -> {
                     isPlaying = false
-                    when (contentBottomSheetMain.playbackState) {
-                        null -> contentBottomSheetMain.playbackState = R.drawable.ic_play
-                        R.drawable.ani_play_pause -> contentBottomSheetMain.playbackState = R.drawable.ani_pause_play
-                    }
+                    
                 }
                 STATE_BUFFERING -> {
                     isPlaying = false
@@ -439,7 +366,7 @@ class MainActivity : BaseMediaControlActivity() {
     }
     
     override fun updateTime(currentProgress: Long) {
-        contentBottomSheetMain.progress = currentProgress
+    
     }
     
     override fun onConnected() {
