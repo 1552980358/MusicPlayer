a6f9be116e814c3624da09a970d6754ca6130689
1552980358 <1552980358@qq.com>
2022-03-16 22:00:00 +0800

    MainFragment.kt: Implement audio played callbacks

--- app/src/main/java/projekt/cloud/piece/music/player/ui/main/MainFragment.kt
+++ app/src/main/java/projekt/cloud/piece/music/player/ui/main/MainFragment.kt
@@ -1,11 +1,17 @@
 package projekt.cloud.piece.music.player.ui.main
 
+import android.content.res.ColorStateList.valueOf
+import android.graphics.drawable.BitmapDrawable
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.viewpager2.adapter.FragmentStateAdapter
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.delay
+import lib.github1552980358.ktExtension.kotlinx.coroutines.io
+import lib.github1552980358.ktExtension.kotlinx.coroutines.ui
 import projekt.cloud.piece.music.player.R
 import projekt.cloud.piece.music.player.base.BaseFragment
 import projekt.cloud.piece.music.player.databinding.FragmentMainBinding
@@ -13,11 +19,24 @@ import projekt.cloud.piece.music.player.ui.main.home.HomeFragment
 
 class MainFragment: BaseFragment() {
 
+    companion object {
+        private const val TAG = "MainFragment"
+    }
+
     private var _binding: FragmentMainBinding? = null
     private val binding get() = _binding!!
+    private val extendedFloatingActionButton get() = binding.extendedFloatingActionButton
+
+    private val audioArtMap get() = activityViewModel.audioArtMap
+    private val albumArtMap get() = activityViewModel.albumArtMap
+    private val defaultCoverArt get() = activityViewModel.defaultCoverArt
+    private val database get() = activityViewModel.database
+
+    private var countJob: Job? = null
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         _binding = DataBindingUtil.inflate(inflater, R.layout.fragment_main, container, false)
+        binding.bottomNavigation.background = null
         return binding.root
     }
 
@@ -34,6 +53,45 @@ class MainFragment: BaseFragment() {
                 override fun createFragment(position: Int) = fragments[position]
             }
 
+        }
+
+        with(extendedFloatingActionButton) {
+            hide()
+            setAnimateShowBeforeLayout(true)
+        }
+
+        activityViewModel.setAudioItemObserver(TAG) { audioItem ->
+            io {
+                val drawable = BitmapDrawable(
+                    resources,
+                    audioArtMap[audioItem.id] ?: albumArtMap[audioItem.album] ?: defaultCoverArt
+                )
+
+                database.color.query(audioItem.id, audioItem.album).apply {
+                    with(extendedFloatingActionButton) {
+                        ui {
+                            icon = drawable
+                            setTextColor(primaryColor)
+                            backgroundTintList = valueOf(backgroundColor)
+                            if (text != audioItem.title) {
+                                text = audioItem.title
+                            }
+                            if (!isShown) {
+                                show()
+                            }
+                            if (!isExtended) {
+                                extend()
+                            }
+                        }
+
+                        countJob?.cancel()
+                        countJob = io {
+                            delay(5000L)
+                            ui { shrink() }
+                        }
+                    }
+                }
+            }
         }
 
     }
