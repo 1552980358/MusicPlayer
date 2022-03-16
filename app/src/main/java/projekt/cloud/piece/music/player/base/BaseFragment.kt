c587f0f6a7d609c98c8560a5379d4fcd12d369ce
1552980358 <1552980358@qq.com>
2022-03-16 19:15:24 +0800

    BaseFragment.kt: Update base fragment class

---
+++ app/src/main/java/projekt/cloud/piece/music/player/base/BaseFragment.kt
@@ -0,0 +1,17 @@
+package projekt.cloud.piece.music.player.base
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import projekt.cloud.piece.music.player.MainActivityViewModel
+
+open class BaseFragment: Fragment() {
+
+    protected lateinit var activityViewModel: MainActivityViewModel
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        activityViewModel = ViewModelProvider(requireActivity())[MainActivityViewModel::class.java]
+    }
+
+}
