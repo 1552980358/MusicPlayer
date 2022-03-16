0b0261619b3562487d12a9bad5b7ba27bb9f8e37
1552980358 <1552980358@qq.com>
2022-03-16 21:52:31 +0800

    RecyclerViewAdapterUtil.kt: Update RecyclerViewAdapterUtil

--- app/src/main/java/projekt/cloud/piece/music/player/ui/main/home/util/RecyclerViewAdapterUtil.kt
+++ app/src/main/java/projekt/cloud/piece/music/player/ui/main/home/util/RecyclerViewAdapterUtil.kt
@@ -2,8 +2,11 @@ package projekt.cloud.piece.music.player.ui.main.home.util
 
 import android.graphics.Bitmap
 import android.graphics.drawable.BitmapDrawable
+import android.graphics.drawable.RippleDrawable
 import android.util.Log
 import android.view.LayoutInflater
+import android.view.MotionEvent.ACTION_DOWN
+import android.view.MotionEvent.ACTION_UP
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.RecyclerView
@@ -17,7 +20,9 @@ class RecyclerViewAdapterUtil(recyclerView: RecyclerView,
                               private val audioList: List<AudioItem>,
                               private val audioArtMap: Map<String, Bitmap>,
                               private val albumArtMap: Map<String, Bitmap>,
-                              private val defaultArtBitmap: Bitmap) {
+                              private val defaultArtBitmap: Bitmap,
+                              private val onClick: (Int) -> Unit,
+                              private val onOptionClick: (Int) -> Unit) {
 
     private class RecyclerViewHolder(val binding: LayoutRecyclerHomeBinding): ViewHolder(binding.root) {
         fun setAudioItem(audioItem: AudioItem) {
@@ -25,6 +30,35 @@ class RecyclerViewAdapterUtil(recyclerView: RecyclerView,
         }
         fun setImageBitmap(imageBitmap: Bitmap) {
             binding.imageDrawable = BitmapDrawable(binding.root.context.resources, imageBitmap)
+        }
+        fun setOnClickListener(position: Int, onClick: (Int) -> Unit, onOptionClick: (Int) -> Unit) {
+            binding.root.setOnClickListener {
+                Log.e("RVA", position.toString())
+                onClick(position)
+            }
+            binding.root.setOnLongClickListener {
+                onOptionClick(position)
+                true
+            }
+            @Suppress("ClickableViewAccessibility")
+            binding.imageViewMore.setOnTouchListener { view, motionEvent ->
+                when (motionEvent.action) {
+                    ACTION_DOWN -> {
+                        (binding.root.background as RippleDrawable).setHotspot(
+                            motionEvent.x + binding.imageViewMore.x,
+                            motionEvent.y + binding.imageViewMore.y
+                        )
+                        binding.root.isPressed = true
+                    }
+                    ACTION_UP -> {
+                        binding.root.isPressed = false
+                        if (motionEvent.y in (0F .. binding.imageViewMore.bottom.toFloat())) {
+                            onOptionClick(position)
+                        }
+                    }
+                }
+                true
+            }
         }
     }
 
@@ -37,6 +71,7 @@ class RecyclerViewAdapterUtil(recyclerView: RecyclerView,
             audioList[position].apply {
                 holder.setAudioItem(this)
                 holder.setImageBitmap(audioArtMap[id] ?: albumArtMap[album] ?: defaultArtBitmap)
+                holder.setOnClickListener(position, onClick, onOptionClick)
             }
         }
 
