161bebf82c50bfe8197851e328ca4a7cf6571c36
1552980358 <1552980358@qq.com>
2022-03-17 21:31:29 +0800

    MainViewModel.kt: Update view model for MainFragment.kt

---
+++ app/src/main/java/projekt/cloud/piece/music/player/ui/main/MainViewModel.kt
@@ -0,0 +1,11 @@
+package projekt.cloud.piece.music.player.ui.main
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel: ViewModel() {
+
+    var isExtended = false
+
+    var isDestroyed = false
+
+}
