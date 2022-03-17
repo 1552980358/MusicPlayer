420cc943b96ea935d6328e1bbfe7f6a9ca6511c8
1552980358 <1552980358@qq.com>
2022-03-17 20:55:26 +0800

    PlayFragment.kt: Implement color changed animation

--- app/src/main/java/projekt/cloud/piece/music/player/ui/play/PlayFragment.kt
+++ app/src/main/java/projekt/cloud/piece/music/player/ui/play/PlayFragment.kt
@@ -1,11 +1,15 @@
 package projekt.cloud.piece.music.player.ui.play
 
 import android.graphics.Color.TRANSPARENT
+import android.graphics.Point
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.View.GONE
+import android.view.View.VISIBLE
+import android.view.ViewAnimationUtils.createCircularReveal
 import android.view.ViewGroup
+import androidx.core.animation.doOnEnd
 import androidx.databinding.DataBindingUtil
 import androidx.transition.Transition
 import androidx.viewpager2.adapter.FragmentStateAdapter
@@ -17,6 +21,8 @@ import projekt.cloud.piece.music.player.base.BaseFragment
 import projekt.cloud.piece.music.player.database.item.AudioItem
 import projekt.cloud.piece.music.player.databinding.FragmentPlayBinding
 import projekt.cloud.piece.music.player.ui.play.playControl.PlayControlFragment
+import projekt.cloud.piece.music.player.util.Constant.ANIMATION_DURATION
+import kotlin.math.hypot
 
 class PlayFragment: BaseFragment() {
 
@@ -35,6 +41,8 @@ class PlayFragment: BaseFragment() {
     private val database get() = activityViewModel.database
     private val audioItem get() = activityViewModel.audioItem!!
 
+    private val circularRevelPoint = Point()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         sharedElementEnterTransition = MaterialContainerTransform()
@@ -52,6 +60,13 @@ class PlayFragment: BaseFragment() {
             }
         })
         requireActivity().window.navigationBarColor = TRANSPARENT
+
+        with(circularRevelPoint) {
+            x = resources.displayMetrics.widthPixels / 2
+            y = (resources.displayMetrics.heightPixels - resources.displayMetrics.widthPixels) * 2 / 5 -
+                resources.getDimensionPixelSize(R.dimen.play_control_seekbar_height) * 2 +
+                resources.displayMetrics.widthPixels
+        }
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
@@ -76,8 +91,23 @@ class PlayFragment: BaseFragment() {
         database.color.query(audioItem.id, audioItem.album).apply {
             when {
                 requireAnimation -> {
-                    binding.relativeLayout.visibility = GONE
-                    binding.relativeLayout.setBackgroundColor(backgroundColor)
+                    ui {
+                        binding.relativeLayout.visibility = GONE
+                        binding.relativeLayout.setBackgroundColor(backgroundColor)
+                    }
+                    createCircularReveal(
+                        binding.relativeLayout,
+                        circularRevelPoint.x, circularRevelPoint.y,
+                        0F,
+                        hypot(circularRevelPoint.x.toFloat(), circularRevelPoint.y.toFloat())
+                    ).apply {
+                        duration = ANIMATION_DURATION
+                        doOnEnd { binding.root.setBackgroundColor(backgroundColor) }
+                        ui {
+                            binding.relativeLayout.visibility = VISIBLE
+                            start()
+                        }
+                    }
                 }
                 else -> ui {
                     binding.root.setBackgroundColor(backgroundColor)
