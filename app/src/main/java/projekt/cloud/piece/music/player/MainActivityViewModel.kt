0856df66e03f899fe533666ec146001a6da50d74
1552980358 <1552980358@qq.com>
2022-03-17 17:44:43 +0800

    MainActivityViewModel.kt: Update and implement variables and related methods

--- app/src/main/java/projekt/cloud/piece/music/player/MainActivityViewModel.kt
+++ app/src/main/java/projekt/cloud/piece/music/player/MainActivityViewModel.kt
@@ -1,12 +1,19 @@
 package projekt.cloud.piece.music.player
 
 import android.graphics.Bitmap
+import android.os.Bundle
 import android.support.v4.media.MediaBrowserCompat
+import android.support.v4.media.MediaBrowserCompat.CustomActionCallback
 import android.support.v4.media.session.MediaControllerCompat
-import androidx.lifecycle.MutableLiveData
+import androidx.core.os.bundleOf
 import androidx.lifecycle.ViewModel
 import projekt.cloud.piece.music.player.database.AudioDatabase
 import projekt.cloud.piece.music.player.database.item.AudioItem
+import projekt.cloud.piece.music.player.service.play.Action.ACTION_PLAY_CONFIG_CHANGED
+import projekt.cloud.piece.music.player.service.play.Config.PLAY_CONFIG_REPEAT
+import projekt.cloud.piece.music.player.service.play.Config.shl
+import projekt.cloud.piece.music.player.service.play.Extra.EXTRA_LIST
+import projekt.cloud.piece.music.player.service.play.Extra.EXTRA_PLAY_CONFIG
 
 class MainActivityViewModel: ViewModel() {
 
@@ -71,6 +78,55 @@ class MainActivityViewModel: ViewModel() {
                 }
             }
         }
+    }
+
+    var playConfig = PLAY_CONFIG_REPEAT.shl
+        set(value) {
+            field = value
+            _playConfigObservers.forEach { (_, observer) -> observer(value) }
+        }
+    private val _playConfigObservers = mutableMapOf<String, (Int) -> Unit>()
+    fun setPlayConfigObserver(tag: String, needValueInstant: Boolean = true, observer: ((Int) -> Unit)?) {
+        when (observer) {
+            null -> _playConfigObservers.remove(tag)
+            else -> {
+                _playConfigObservers[tag] = observer
+                if (needValueInstant) {
+                    observer(playConfig)
+                }
+            }
+        }
+    }
+
+    var playList: List<AudioItem>? = null
+        set(value) {
+            field = value
+            value?.let { _playlistObservers.forEach { (_, observer) -> observer(it) } }
+        }
+    private val _playlistObservers = mutableMapOf<String, (List<AudioItem>) -> Unit>()
+    fun setPlaylistObserver(tag: String, needValueInstant: Boolean = true, observer: ((List<AudioItem>) -> Unit)?) {
+        when (observer) {
+            null -> _playlistObservers.remove(tag)
+            else -> {
+                _playlistObservers[tag] = observer
+                if (needValueInstant) {
+                    playList?.let { observer(it) }
+                }
+            }
+        }
+    }
+
+    fun updatePlayConfig(newConfig: Int) {
+        mediaBrowserCompat.sendCustomAction(ACTION_PLAY_CONFIG_CHANGED, bundleOf(EXTRA_PLAY_CONFIG to newConfig), object : CustomActionCallback() {
+            override fun onResult(action: String?, extras: Bundle?, resultData: Bundle?) {
+                resultData?.let { resultExtra ->
+                    @Suppress("UNCHECKED_CAST")
+                    (resultExtra.getSerializable(EXTRA_LIST) as List<AudioItem>?)?.let { list ->
+                        playList = list
+                    }
+                }
+            }
+        })
     }
 
     lateinit var mediaBrowserCompat: MediaBrowserCompat
