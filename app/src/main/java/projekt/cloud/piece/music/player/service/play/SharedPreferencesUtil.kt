604435812c54dd0b09312551075f231c227cc60f
1552980358 <1552980358@qq.com>
2022-03-17 20:56:49 +0800

    Rename SharedPreferences.kt -> SharedPreferencesUtil.kt and update methods

---
+++ app/src/main/java/projekt/cloud/piece/music/player/service/play/SharedPreferencesUtil.kt
@@ -0,0 +1,23 @@
+package projekt.cloud.piece.music.player.service.play
+
+import android.content.SharedPreferences
+import projekt.cloud.piece.music.player.service.play.Config.PLAY_CONFIG_REPEAT
+import projekt.cloud.piece.music.player.service.play.Config.SERVICE_CONFIG_FOREGROUND_SERVICE
+import projekt.cloud.piece.music.player.service.play.Config.setConfig
+import projekt.cloud.piece.music.player.service.play.Config.shl
+
+object SharedPreferencesUtil {
+
+    val DEFAULT_CONFIG = PLAY_CONFIG_REPEAT.shl
+    
+    private const val CONFIGS = "configs"
+
+    val SharedPreferences.hasConfig get() = contains(CONFIGS)
+
+    val SharedPreferences.readConfigs get() =
+        getInt(CONFIGS, DEFAULT_CONFIG)
+
+    fun SharedPreferences.writeConfigs(configs: Int) =
+        edit().putInt(CONFIGS, configs.setConfig(SERVICE_CONFIG_FOREGROUND_SERVICE, false)).commit()
+    
+}
