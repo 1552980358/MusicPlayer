979ee78963ced4017e583cdcb9dd9738bbb751be
1552980358 <1552980358@qq.com>
2022-03-18 17:04:49 +0800

    MainViewModel.kt: Update variables

--- app/src/main/java/projekt/cloud/piece/music/player/ui/main/MainViewModel.kt
+++ app/src/main/java/projekt/cloud/piece/music/player/ui/main/MainViewModel.kt
@@ -1,11 +1,18 @@
 package projekt.cloud.piece.music.player.ui.main
 
+import android.graphics.Bitmap
 import androidx.lifecycle.ViewModel
+import projekt.cloud.piece.music.player.database.item.AlbumItem
 
 class MainViewModel: ViewModel() {
 
     var isExtended = false
 
     var isDestroyed = false
+
+    var isAlbumListLoaded = false
+    lateinit var albumList: List<AlbumItem>
+
+    lateinit var defaultAlbumCover: Bitmap
 
 }
