c4c751187ec83f8a0081945d771eae014861c514
1552980358 <1552980358@qq.com>
2022-03-19 21:20:54 +0800

    MainViewModel.kt: Update variables

--- app/src/main/java/projekt/cloud/piece/music/player/ui/main/MainViewModel.kt
+++ app/src/main/java/projekt/cloud/piece/music/player/ui/main/MainViewModel.kt
@@ -3,7 +3,9 @@ package projekt.cloud.piece.music.player.ui.main
 import android.graphics.Bitmap
 import androidx.lifecycle.ViewModel
 import projekt.cloud.piece.music.player.database.item.AlbumItem
+import projekt.cloud.piece.music.player.database.item.ArtistItem
 import projekt.cloud.piece.music.player.ui.main.album.AlbumFragment
+import projekt.cloud.piece.music.player.ui.main.artist.ArtistFragment
 import projekt.cloud.piece.music.player.ui.main.home.HomeFragment
 
 class MainViewModel: ViewModel() {
@@ -15,11 +17,16 @@ class MainViewModel: ViewModel() {
     var isAlbumListLoaded = false
     lateinit var albumList: List<AlbumItem>
 
+    var isArtistListLoaded = false
+    lateinit var artistList: List<ArtistItem>
+
     lateinit var defaultAlbumCover: Bitmap
+    lateinit var defaultArtistArt: Bitmap
 
     val fragments = listOf(
         HomeFragment(),
-        AlbumFragment()
+        AlbumFragment(),
+        ArtistFragment()
     )
 
 }
