4bef394727031cee1d1973ab3c382f9022c0bd57
1552980358 <1552980358@qq.com>
2022-03-22 19:03:18 +0800

    PlayViewModel.kt: Implement PlayLyricFragment

--- app/src/main/java/projekt/cloud/piece/music/player/ui/play/PlayViewModel.kt
+++ app/src/main/java/projekt/cloud/piece/music/player/ui/play/PlayViewModel.kt
@@ -2,13 +2,13 @@ package projekt.cloud.piece.music.player.ui.play
 
 import android.graphics.Point
 import androidx.lifecycle.ViewModel
-import projekt.cloud.piece.music.player.base.BaseFragment
+import projekt.cloud.piece.music.player.ui.play.palyLyric.PlayLyricFragment
 import projekt.cloud.piece.music.player.ui.play.playControl.PlayControlFragment
 
 class PlayViewModel: ViewModel() {
 
-    val fragmentList = listOf<BaseFragment>(
-        PlayControlFragment()
+    val fragmentList = listOf(
+        PlayControlFragment(), PlayLyricFragment()
     )
 
     var isPointSet = false
