e6b8bbff8df8e669fa09c7d25576fa67fad71974
1552980358 <1552980358@qq.com>
2022-03-22 10:59:18 +0800

    PlaylistContentItemDao.kt: Update method queryAudio

--- app/src/main/java/projekt/cloud/piece/music/player/database/itemDao/PlaylistContentItemDao.kt
+++ app/src/main/java/projekt/cloud/piece/music/player/database/itemDao/PlaylistContentItemDao.kt
@@ -4,6 +4,7 @@ import androidx.room.Dao
 import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.Query
+import projekt.cloud.piece.music.player.database.item.AudioItem
 import projekt.cloud.piece.music.player.database.item.PlaylistContentItem
 
 @Dao
@@ -12,8 +13,8 @@ interface PlaylistContentItemDao {
     @Query("select * from `playlist-content`")
     fun query(): List<PlaylistContentItem>
 
-    @Query("select * from `playlist-content` where playlist=:playlist")
-    fun query(playlist: String): List<PlaylistContentItem>
+    @Query("select * from `audio` where id in (select audio from `playlist-content` where playlist=:playlist)")
+    fun queryAudio(playlist: String): List<AudioItem>
 
     @Insert
     fun insert(vararg playlistContentItems: PlaylistContentItem)
