23d89fa40a5e0f9b156936debf74fa46254436a9
1552980358 <1552980358@qq.com>
2022-03-22 10:40:51 +0800

    HomeFragment.kt: Implement popup menu with options

--- app/src/main/java/projekt/cloud/piece/music/player/ui/main/home/HomeFragment.kt
+++ app/src/main/java/projekt/cloud/piece/music/player/ui/main/home/HomeFragment.kt
@@ -6,19 +6,25 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.PopupMenu
 import androidx.activity.result.contract.ActivityResultContracts.RequestPermission
 import androidx.core.content.ContextCompat.checkSelfPermission
 import androidx.core.os.bundleOf
+import androidx.core.view.MenuCompat.setGroupDividerEnabled
 import androidx.databinding.DataBindingUtil
 import lib.github1552980358.ktExtension.android.graphics.toBitmap
 import lib.github1552980358.ktExtension.androidx.fragment.app.getDrawable
+import lib.github1552980358.ktExtension.androidx.fragment.app.show
+import lib.github1552980358.ktExtension.kotlinx.coroutines.io
 import lib.github1552980358.ktExtension.kotlinx.coroutines.ui
 import projekt.cloud.piece.music.player.R
 import projekt.cloud.piece.music.player.base.BaseFragment
 import projekt.cloud.piece.music.player.database.item.AudioItem
+import projekt.cloud.piece.music.player.database.item.PlaylistContentItem
 import projekt.cloud.piece.music.player.databinding.FragmentHomeBinding
 import projekt.cloud.piece.music.player.service.play.Extra.EXTRA_INDEX
 import projekt.cloud.piece.music.player.service.play.Extra.EXTRA_LIST
+import projekt.cloud.piece.music.player.ui.addToPlaylist.AddToPlaylistDialogFragment
 import projekt.cloud.piece.music.player.ui.main.home.util.DatabaseUtil.initializeApp
 import projekt.cloud.piece.music.player.ui.main.home.util.DatabaseUtil.launchAppCoroutine
 import projekt.cloud.piece.music.player.ui.main.home.util.RecyclerViewAdapterUtil
@@ -76,8 +82,28 @@ class HomeFragment: BaseFragment() {
                     bundleOf(EXTRA_LIST to audioList, EXTRA_INDEX to it)
                 )
             },
-            onOptionClick = {
-
+            onOptionClick = { anchorView, index ->
+                PopupMenu(requireContext(), anchorView).apply {
+                    inflate(R.menu.menu_recycler_home)
+                    val audioItem = audioList[index]
+                    menu.getItem(0).title = audioItem.title
+                    setGroupDividerEnabled(menu, true)
+                    setOnMenuItemClickListener {
+                        when (it.itemId) {
+                            R.id.menu_add_to_playlist -> {
+                                AddToPlaylistDialogFragment().apply {
+                                    setCallback {
+                                        io {
+                                            activityViewModel.database.playlistContent.insert(PlaylistContentItem(audio = audioItem.id, playlist = it.id))
+                                        }
+                                    }
+                                    show(this@HomeFragment)
+                                }
+                            }
+                        }
+                        true
+                    }
+                }.show()
             }
         )
         activityViewModel.isLoaded = true
