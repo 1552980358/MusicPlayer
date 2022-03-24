d64c7f6632b73f31a69f1c578f133698c65341dd
1552980358 <1552980358@qq.com>
2022-03-24 21:25:24 +0800

    PlaylistContentItemDao.kt: Update method queryLike

--- app/src/main/java/projekt/cloud/piece/music/player/database/itemDao/PlaylistContentItemDao.kt
+++ app/src/main/java/projekt/cloud/piece/music/player/database/itemDao/PlaylistContentItemDao.kt
@@ -6,12 +6,16 @@ import androidx.room.Insert
 import androidx.room.Query
 import projekt.cloud.piece.music.player.database.item.AudioItem
 import projekt.cloud.piece.music.player.database.item.PlaylistContentItem
+import projekt.cloud.piece.music.player.util.Constant.PLAYLIST_LIKES
 
 @Dao
 interface PlaylistContentItemDao {
 
     @Query("select * from `playlist-content`")
     fun query(): List<PlaylistContentItem>
+
+    @Query("select * from `playlist-content` where audio=:id and playlist='${PLAYLIST_LIKES}'")
+    fun queryLike(id: String): PlaylistContentItem?
 
     @Query("select * from `audio` where id in (select audio from `playlist-content` where playlist=:playlist)")
     fun queryAudio(playlist: String): List<AudioItem>
