27eefdfcc8bb2fcc965ca164f3992c2e06d51b50
1552980358 <1552980358@qq.com>
2022-03-26 03:00:50 +0800

    RecyclerViewAdapterUtil.kt: Remove unused parameter of onClick

--- app/src/main/java/projekt/cloud/piece/music/player/ui/main/playlist/util/RecyclerViewAdapterUtil.kt
+++ app/src/main/java/projekt/cloud/piece/music/player/ui/main/playlist/util/RecyclerViewAdapterUtil.kt
@@ -3,7 +3,6 @@ package projekt.cloud.piece.music.player.ui.main.playlist.util
 import android.graphics.Bitmap
 import android.graphics.drawable.BitmapDrawable
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
 import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
@@ -20,7 +19,7 @@ import projekt.cloud.piece.music.player.util.Constant.PLAYLIST_LIKES
 class RecyclerViewAdapterUtil(recyclerView: RecyclerView,
                               private val playlistMap: Map<String, Bitmap>,
                               private val defaultPlaylistArt: Bitmap,
-                              private val onClick: (View, PlaylistItem) -> Unit) {
+                              private val onClick: (PlaylistItem) -> Unit) {
 
     private class RecyclerViewHolder(private val binding: LayoutRecyclerPlaylistBinding): ViewHolder(binding.root) {
         fun setTitle(title: String) {
@@ -29,9 +28,9 @@ class RecyclerViewAdapterUtil(recyclerView: RecyclerView,
         fun setTitle(@StringRes resId: Int) =
             setTitle(binding.root.resources.getString(resId))
 
-        fun setOnClick(playlistItem: PlaylistItem, onClick: (View, PlaylistItem) -> Unit) {
+        fun setOnClick(playlistItem: PlaylistItem, onClick: (PlaylistItem) -> Unit) {
             binding.root.transitionName = playlistItem.id
-            binding.root.setOnClickListener { onClick(it, playlistItem) }
+            binding.root.setOnClickListener { onClick(playlistItem) }
         }
         fun setImage(bitmap: Bitmap) {
             binding.image = BitmapDrawable(binding.root.resources, bitmap)
