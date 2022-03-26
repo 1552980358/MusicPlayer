85dda71ff1f372067d06939dc875035170eb7418
1552980358 <1552980358@qq.com>
2022-03-26 21:23:41 +0800

    PreferenceFragmentUtil.kt: Update methods

---
+++ app/src/main/java/projekt/cloud/piece/music/player/util/PreferenceFragmentUtil.kt
@@ -0,0 +1,15 @@
+package projekt.cloud.piece.music.player.util
+
+import androidx.annotation.StringRes
+import androidx.preference.Preference
+import androidx.preference.PreferenceFragmentCompat
+
+object PreferenceFragmentUtil {
+
+    private fun <T: Preference> PreferenceFragmentCompat.preference(@StringRes resId: Int) =
+        findPreference<T>(getString(resId))
+
+    fun PreferenceFragmentCompat.preference(@StringRes resId: Int, block: Preference.() -> Unit = {}) =
+        preference<Preference>(resId)?.apply(block)
+
+}
