f3d0e17977479428e95bb961fc20bd8c7858d783
1552980358 <1552980358@qq.com>
2022-03-27 19:53:42 +0800

    SharedPreferencesUtil.kt: Update constants

--- app/src/main/java/projekt/cloud/piece/music/player/util/SharedPreferencesUtil.kt
+++ app/src/main/java/projekt/cloud/piece/music/player/util/SharedPreferencesUtil.kt
@@ -5,4 +5,7 @@ object SharedPreferencesUtil {
     const val SP_FILE_FILTER_DURATION_INITIAL = "0"
     const val SP_FILE_FILTER_DURATION_DEFAULT = "5000"
 
+    const val SP_FILE_FILTER_SIZE_INITIAL = "0"
+    const val SP_FILE_FILTER_SIZE_DEFAULT = "1024"
+
 }
