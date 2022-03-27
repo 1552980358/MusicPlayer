37437cb4bde9cc937637e5a6254ad362a0d24e53
1552980358 <1552980358@qq.com>
2022-03-27 12:07:41 +0800

    SettingsFragment.kt: Implement motion animation and navigate to FileSettingsFragment

--- app/src/main/java/projekt/cloud/piece/music/player/ui/settings/SettingsFragment.kt
+++ app/src/main/java/projekt/cloud/piece/music/player/ui/settings/SettingsFragment.kt
@@ -1,13 +1,61 @@
 package projekt.cloud.piece.music.player.ui.settings
 
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.NavController
+import androidx.navigation.fragment.FragmentNavigatorExtras
+import androidx.navigation.fragment.findNavController
+import androidx.preference.PreferenceFragmentCompat
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.transition.Hold
 import projekt.cloud.piece.music.player.R
 import projekt.cloud.piece.music.player.base.BasePreferenceFragment
+import projekt.cloud.piece.music.player.preference.util.PreferenceFragmentUtil.transitionPreference
 
 class SettingsFragment: BasePreferenceFragment() {
 
-    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
-        setPreferencesFromResource(R.xml.fragmet_settings, rootKey)
+    private lateinit var navController: NavController
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        navController = findNavController()
+        super.onCreate(savedInstanceState)
+        exitTransition = Hold()
     }
+
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+        setPreferencesFromResource(R.xml.fragment_settings, rootKey)
+        transitionPreference(R.string.key_setting_file) {
+            rootTransitionName = getString(R.string.key_setting_file)
+            setOnPreferenceClickListener {
+                navController.navigate(
+                    SettingsFragmentDirections.actionNavSettingsToNavFileSettings(),
+                    FragmentNavigatorExtras(itemView to itemView.transitionName)
+                )
+                true
+            }
+        }
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        postponeEnterTransition()
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        // Use RecyclerView as anchor to decide whether the content is pre-drawing
+        with(recyclerView) {
+            viewTreeObserver?.addOnPreDrawListener {
+                startPostponedEnterTransition()
+                true
+            }
+        }
+    }
+
+    private val recyclerView get() = PreferenceFragmentCompat::class.java.getDeclaredField("mList")
+        .apply { isAccessible = true }
+        .get(this as PreferenceFragmentCompat) as RecyclerView
 
 }
