d09c08cab8167807fa8934811c4cf4ce0fd77842
1552980358 <1552980358@qq.com>
2022-03-30 09:13:03 +0800

    Extra.kt: Update constants

--- app/src/main/java/projekt/cloud/piece/music/player/service/play/Extra.kt
+++ app/src/main/java/projekt/cloud/piece/music/player/service/play/Extra.kt
@@ -6,5 +6,10 @@ object Extra {
     const val EXTRA_INDEX = "EXTRA_INDEX"
 
     const val EXTRA_CONFIGS = "EXTRA_CONFIGS"
-    
+
+    /**
+     * Refers to [android.media.AudioManager.ACTION_HEADSET_PLUG]
+     **/
+    const val HEADSET_PLUG_STATE_EXTRA = "state"
+    const val HEADSET_PLUG_STATE_UNPLUGGED = 0
 }
