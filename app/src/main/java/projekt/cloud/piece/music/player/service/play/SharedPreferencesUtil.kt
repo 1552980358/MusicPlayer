a6fc2939d57f59db0923d1c01cad5198af1e044e
1552980358 <1552980358@qq.com>
2022-04-01 02:45:31 +0800

    SharedPreferencesUtil.kt: Update default configs

--- app/src/main/java/projekt/cloud/piece/music/player/service/play/SharedPreferencesUtil.kt
+++ app/src/main/java/projekt/cloud/piece/music/player/service/play/SharedPreferencesUtil.kt
@@ -1,6 +1,7 @@
 package projekt.cloud.piece.music.player.service.play
 
 import android.content.SharedPreferences
+import projekt.cloud.piece.music.player.service.play.Config.PLAY_CONFIG_AUDIO_FOCUS
 import projekt.cloud.piece.music.player.service.play.Config.PLAY_CONFIG_REPEAT
 import projekt.cloud.piece.music.player.service.play.Config.SERVICE_CONFIG_FOREGROUND_SERVICE
 import projekt.cloud.piece.music.player.service.play.Config.setConfig
@@ -8,7 +9,7 @@ import projekt.cloud.piece.music.player.service.play.Config.shl
 
 object SharedPreferencesUtil {
 
-    val DEFAULT_CONFIG = PLAY_CONFIG_REPEAT.shl
+    val DEFAULT_CONFIG = PLAY_CONFIG_REPEAT.shl or PLAY_CONFIG_AUDIO_FOCUS.shl
     
     private const val CONFIGS = "configs"
 
