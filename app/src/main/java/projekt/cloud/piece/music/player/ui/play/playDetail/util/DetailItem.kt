9c25004850cf63a84b230308cefb04fab39e9e68
1552980358 <1552980358@qq.com>
2022-04-09 21:10:25 +0800

    DetailItem.kt: Swap title and content

--- app/src/main/java/projekt/cloud/piece/music/player/ui/play/playDetail/util/DetailItem.kt
+++ app/src/main/java/projekt/cloud/piece/music/player/ui/play/playDetail/util/DetailItem.kt
@@ -2,10 +2,10 @@ package projekt.cloud.piece.music.player.ui.play.playDetail.util
 
 import androidx.annotation.StringRes
 
-data class DetailItem(@StringRes val content: Int) {
+data class DetailItem(@StringRes val title: Int) {
     companion object {
         private const val EMPTY_STR = ""
     }
     var onClick: (() -> Unit)? = null
-    var title = EMPTY_STR
+    var content = EMPTY_STR
 }
