468236473385721f5d0e0352742e8975d198e7d4
1552980358 <1552980358@qq.com>
2022-04-09 17:40:58 +0800

    SleepTimerDialogFragment.kt: Update SleepTimerDialogFragment

---
+++ app/src/main/java/projekt/cloud/piece/music/player/ui/play/playControl/sleepTimer/SleepTimerDialogFragment.kt
@@ -0,0 +1,39 @@
+package projekt.cloud.piece.music.player.ui.play.playControl.sleepTimer
+
+import projekt.cloud.piece.music.player.R
+import projekt.cloud.piece.music.player.base.BaseValueSelectDialogFragment
+
+class SleepTimerDialogFragment: BaseValueSelectDialogFragment() {
+
+    override fun setDialogTitle() = R.string.sleep_timer_title
+
+    override fun setSelectionTitle() = R.string.sleep_timer_unit
+
+    override fun setPositiveButtonText() = R.string.sleep_timer_start
+
+    private lateinit var onPositive: (String?) -> Unit
+    private lateinit var onStopClick: () -> Unit
+
+    override fun setSelectionList() = resources.getStringArray(R.array.sleep_timer_time).toList().apply {
+        initialValue = this[2]
+    }
+
+    override fun onPositiveClick(newValue: String?) = onPositive(newValue)
+
+    override fun setNeutralButtonText() = R.string.sleep_timer_stop
+
+    override fun onDefaultClick() = onStopClick()
+
+    fun setPositiveClick(onPositive: (String?) -> Unit) = apply {
+        this.onPositive = onPositive
+    }
+
+    fun setStop(onStopClick: () -> Unit) = apply {
+        this.onStopClick = onStopClick
+    }
+
+    fun setEnableStop(enable: Boolean) = apply {
+        hasDefaultButton = enable
+    }
+
+}
