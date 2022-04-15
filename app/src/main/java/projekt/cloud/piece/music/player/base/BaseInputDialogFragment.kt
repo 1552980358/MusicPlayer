eeea4dab29db54d5f9d04b5b4ad1f65b04d70970
1552980358 <1552980358@qq.com>
2022-04-15 21:40:42 +0800

    BaseInputDialogFragment.kt: Update BaseInputDialogFragment and dialog_fragment_base_input.xml

---
+++ app/src/main/java/projekt/cloud/piece/music/player/base/BaseInputDialogFragment.kt
@@ -0,0 +1,33 @@
+package projekt.cloud.piece.music.player.base
+
+import android.view.View
+import projekt.cloud.piece.music.player.databinding.DialogFragmentBaseInputBinding
+
+abstract class BaseInputDialogFragment: BaseAlertDialogFragment() {
+
+    abstract fun setHint(): Int
+    private val hint get() = setHint()
+
+    private var _binding: DialogFragmentBaseInputBinding? = null
+    private val binding get() = _binding!!
+
+    protected var prefix: String? = null
+    protected var suffix: String? = null
+
+    override fun createView(): View {
+        _binding = DialogFragmentBaseInputBinding.inflate(layoutInflater)
+
+        with(binding.textInputLayout) {
+            setHint(this@BaseInputDialogFragment.hint)
+            prefix?.let { prefixText = it }
+            suffix?.let { suffixText = it }
+        }
+
+        binding.textInputEditText.setText(initialValue)
+
+        return binding.root
+    }
+
+    override fun setPositiveClickValue() = binding.textInputEditText.text?.toString()
+
+}
