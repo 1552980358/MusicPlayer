49877c88cb0bf16a6f57b92ee84fadc5220bedbb
1552980358 <1552980358@qq.com>
2022-04-17 21:00:21 +0800

    MainActivity.kt: Update ui with layout

--- app/src/main/java/projekt/cloud/piece/music/player/MainActivity.kt
+++ app/src/main/java/projekt/cloud/piece/music/player/MainActivity.kt
@@ -2,12 +2,41 @@ package projekt.cloud.piece.music.player
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import projekt.cloud.piece.music.player.databinding.ActivityMainBinding
 
+/**
+ * Class [MainActivity], inherit to [AppCompatActivity]
+ *
+ * Variables:
+ *   [binding]
+ *   [navController]
+ *
+ * Methods:
+ *   [onCreate]
+ *
+ **/
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+
+    private lateinit var navController: NavController
+
+    private lateinit var viewModel: MainActivityViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        delegate.installViewFactory()
+        delegate.onCreate(savedInstanceState)
+
+        viewModel = ViewModelProvider(this)[MainActivityViewModel::class.java]
+
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        navController = binding.fragmentContainerView.getFragment<NavHostFragment>().navController
     }
 
 }
