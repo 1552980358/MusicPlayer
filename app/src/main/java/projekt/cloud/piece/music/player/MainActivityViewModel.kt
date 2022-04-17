54168138034cc334de587cd49d354f9898b45442
1552980358 <1552980358@qq.com>
2022-04-17 20:44:32 +0800

    MainActivityViewModel.kt: Update MainActivityViewModel

---
+++ app/src/main/java/projekt/cloud/piece/music/player/MainActivityViewModel.kt
@@ -0,0 +1,10 @@
+package projekt.cloud.piece.music.player
+
+import androidx.lifecycle.ViewModel
+
+/**
+ * Class [MainActivityViewModel], inherit to [ViewModel]
+ *
+ **/
+class MainActivityViewModel: ViewModel() {
+}
