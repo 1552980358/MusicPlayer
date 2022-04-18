5cc726a69fc753b089ac4f9739d2446b7862b9f7
1552980358 <1552980358@qq.com>
2022-04-18 19:05:56 +0800

    RecyclerViewAdapter.kt: Update index comment

--- app/src/main/java/projekt/cloud/piece/music/player/ui/main/audio/util/RecyclerViewAdapter.kt
+++ app/src/main/java/projekt/cloud/piece/music/player/ui/main/audio/util/RecyclerViewAdapter.kt
@@ -12,8 +12,38 @@ import projekt.cloud.piece.music.player.util.CoroutineUtil.io
 import projekt.cloud.piece.music.player.util.CoroutineUtil.ui
 import projekt.cloud.piece.music.player.util.ImageUtil.readAlbumArtLarge
 
+/**
+ * Class [RecyclerViewAdapter]
+ *
+ * Final Variable:
+ *  [adapter]
+ *   @type [RecyclerViewAdapter]
+ *   @default [RecyclerViewAdapter]
+ *
+ * Variable:
+ *  [audioList]
+ *   @type [List]<[AudioItem]>
+ *   @default null
+ *   @setter
+ *
+ * Inner class [RecyclerViewHolder]
+ * Inner class [RecyclerViewAdapter]
+ *
+ **/
 class RecyclerViewAdapter(recyclerView: RecyclerView) {
 
+    /**
+     * Inner class [RecyclerViewHolder], inherit to [ViewHolder]
+     *
+     * Variables:
+     *  [job]
+     *   @type [Job]?
+     *   @default null
+     *
+     * Methods:
+     *  [bindView]
+     *
+     **/
     private class RecyclerViewHolder(private val binding: LayoutRecyclerAudioBinding): ViewHolder(binding.root) {
         private var job: Job? = null
         fun bindView(audioItem: AudioItem) {
@@ -26,6 +56,14 @@ class RecyclerViewAdapter(recyclerView: RecyclerView) {
         }
     }
 
+    /**
+     * Inner class [RecyclerViewAdapter], inherit to [Adapter]<[RecyclerViewHolder]>
+     *
+     * Methods:
+     *  [onCreateViewHolder]
+     *  [onBindViewHolder]
+     *  [getItemCount]
+     **/
     private inner class RecyclerViewAdapter: Adapter<RecyclerViewHolder>() {
 
         override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) = RecyclerViewHolder(
