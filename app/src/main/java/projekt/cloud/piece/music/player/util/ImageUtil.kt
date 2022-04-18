7e20c7cb21b77ccf785ec9b223e7f928e21a95f0
1552980358 <1552980358@qq.com>
2022-04-18 18:47:58 +0800

    ImageUtil.kt: Update image reading

--- app/src/main/java/projekt/cloud/piece/music/player/util/ImageUtil.kt
+++ app/src/main/java/projekt/cloud/piece/music/player/util/ImageUtil.kt
@@ -7,6 +7,7 @@ import android.graphics.Bitmap.CompressFormat.JPEG
 import android.graphics.BitmapFactory
 import android.graphics.Matrix
 import android.net.Uri
+import projekt.cloud.piece.music.player.util.TryUtil.tryRun
 import projekt.cloud.piece.music.player.util.UnitUtil.dp2PxF
 import java.io.File
 
@@ -21,18 +22,23 @@ object ImageUtil {
 
     private const val SMALL_ART_SIZE_DP = 40
 
-    private const val DIR_ALBUM_ART_SMALL = "album_small"
-    private const val DIR_ALBUM_Art_LARGE = "album_large"
-
-    private fun Context.saveAlbumArtSmall(album: String, bitmap: Bitmap) =
-        File(getExternalFilesDir(DIR_ALBUM_ART_SMALL), album).outputStream().use {
-            bitmap.compress(JPEG, 100, it)
-        }
-
+    private const val DIR_ALBUM_ART_LARGE = "album_large"
+    private fun Context.getAlbumArtLargeFile(album: String) = File(getExternalFilesDir(DIR_ALBUM_ART_LARGE), album)
     private fun Context.saveAlbumArtLarge(album: String, bitmap: Bitmap) =
-        File(getExternalFilesDir(DIR_ALBUM_Art_LARGE), album).outputStream().use {
+        getAlbumArtLargeFile(album).outputStream().use {
             bitmap.compress(JPEG, 100, it)
         }
+    fun Context.readAlbumArtLarge(album: String) =
+        tryRun { getAlbumArtLargeFile(album).inputStream().use { BitmapFactory.decodeStream(it) } }
+
+    private const val DIR_ALBUM_ART_SMALL = "album_small"
+    private fun Context.getAlbumArtSmallFile(album: String) = File(getExternalFilesDir(DIR_ALBUM_ART_SMALL), album)
+    private fun Context.saveAlbumArtSmall(album: String, bitmap: Bitmap) =
+        getAlbumArtSmallFile(album).outputStream().use {
+            bitmap.compress(JPEG, 100, it)
+        }
+    fun Context.readAlbumArtSmall(album: String) =
+        tryRun { getAlbumArtSmallFile(album).inputStream().use { BitmapFactory.decodeStream(it) } }
 
     fun Context.saveAlbumArt(album: String, bitmap: Bitmap) {
         saveAlbumArtLarge(album, bitmap)
