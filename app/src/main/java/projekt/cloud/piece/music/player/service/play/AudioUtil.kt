a46e5302eff6cf53ca60482d2f3ee7426086e775
1552980358 <1552980358@qq.com>
2022-04-19 03:37:25 +0800

    AudioUtil.kt: Update methods

---
+++ app/src/main/java/projekt/cloud/piece/music/player/service/play/AudioUtil.kt
@@ -0,0 +1,13 @@
+package projekt.cloud.piece.music.player.service.play
+
+import android.net.Uri
+import android.provider.MediaStore.Audio.Media.EXTERNAL_CONTENT_URI
+import java.io.File.separator
+
+object AudioUtil {
+
+    val String.formUri get() = "$EXTERNAL_CONTENT_URI$separator$this"
+
+    val String.parseUri: Uri get() = Uri.parse(formUri)
+
+}
