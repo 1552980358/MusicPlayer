07450a91989f597c1427222c3dac6c3690ad902e
1552980358 <1552980358@qq.com>
2022-04-19 03:37:48 +0800

    Config.kt: Update constants

---
+++ app/src/main/java/projekt/cloud/piece/music/player/service/play/Config.kt
@@ -0,0 +1,11 @@
+package projekt.cloud.piece.music.player.service.play
+
+object Config {
+
+    const val CONFIG_SERVICE_FOREGROUND = 0
+
+    const val CONFIG_PLAY_REPEAT = 1
+    const val CONFIG_PLAY_REPEAT_ONE = 2
+    const val CONFIG_PLAY_SHUFFLE = 3
+
+}
