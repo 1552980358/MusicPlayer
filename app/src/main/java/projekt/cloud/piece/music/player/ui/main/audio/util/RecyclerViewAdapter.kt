f4a27a479afbc83c4e1e891f2520e9ed2a27a6d9
1552980358 <1552980358@qq.com>
2022-04-18 19:21:32 +0800

    RecyclerViewAdapter.kt: Update onClick and onLongClick

--- app/src/main/java/projekt/cloud/piece/music/player/ui/main/audio/util/RecyclerViewAdapter.kt
+++ app/src/main/java/projekt/cloud/piece/music/player/ui/main/audio/util/RecyclerViewAdapter.kt
@@ -1,6 +1,9 @@
 package projekt.cloud.piece.music.player.ui.main.audio.util
 
 import android.view.LayoutInflater
+import android.view.View
+import android.view.View.OnClickListener
+import android.view.View.OnLongClickListener
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.RecyclerView.Adapter
@@ -25,6 +28,12 @@ import projekt.cloud.piece.music.player.util.ImageUtil.readAlbumArtLarge
  *   @type [List]<[AudioItem]>
  *   @default null
  *   @setter
+ *  [onClick]
+ *   @type [List]<[AudioItem]> -> [Unit]
+ *   @default null
+ *
+ *  Methods:
+ *   [setOnClick]
  *
  * Inner class [RecyclerViewHolder]
  * Inner class [RecyclerViewAdapter]
@@ -33,7 +42,9 @@ import projekt.cloud.piece.music.player.util.ImageUtil.readAlbumArtLarge
 class RecyclerViewAdapter(recyclerView: RecyclerView) {
 
     /**
-     * Inner class [RecyclerViewHolder], inherit to [ViewHolder]
+     * Inner class [RecyclerViewHolder]
+     *   inherits to [ViewHolder]
+     *   implements [OnClickListener], [OnLongClickListener]
      *
      * Variables:
      *  [job]
@@ -42,10 +53,13 @@ class RecyclerViewAdapter(recyclerView: RecyclerView) {
      *
      * Methods:
      *  [bindView]
+     *  [onClick]
+     *  [onLongClick]
      *
      **/
-    private class RecyclerViewHolder(private val binding: LayoutRecyclerAudioBinding): ViewHolder(binding.root) {
+    private inner class RecyclerViewHolder(private val binding: LayoutRecyclerAudioBinding): ViewHolder(binding.root), OnClickListener, OnLongClickListener {
         private var job: Job? = null
+
         fun bindView(audioItem: AudioItem) {
             job?.cancel()
             job = io {
@@ -53,6 +67,16 @@ class RecyclerViewAdapter(recyclerView: RecyclerView) {
                 ui { binding.appCompatImageViewAvatar.setImageBitmap(imageBitmap) }
             }
             binding.audio = audioItem
+            binding.root.setOnClickListener(this)
+            binding.root.setOnLongClickListener(this)
+        }
+
+        override fun onClick(v: View?) {
+            onClick?.let { it(binding.audio, audioList) }
+        }
+
+        override fun onLongClick(v: View?): Boolean {
+            return true
         }
     }
 
@@ -89,8 +113,14 @@ class RecyclerViewAdapter(recyclerView: RecyclerView) {
 
     private val adapter = RecyclerViewAdapter()
 
+    private var onClick: ((AudioItem?, List<AudioItem>?) -> Unit)? = null
+
     init {
         recyclerView.adapter = adapter
+    }
+
+    fun setOnClick(onClick: (AudioItem?, List<AudioItem>?) -> Unit) {
+        this.onClick = onClick
     }
 
 }
