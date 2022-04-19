be878c75bd88707503ebfed60ea53b98c4065245
1552980358 <1552980358@qq.com>
2022-04-19 20:44:22 +0800

    Extras.kt: Update constant

--- app/src/main/java/projekt/cloud/piece/music/player/service/play/Extras.kt
+++ app/src/main/java/projekt/cloud/piece/music/player/service/play/Extras.kt
@@ -4,7 +4,7 @@ object Extras {
     
     const val EXTRA_CONFIGS = "EXTRA_CONFIGS"
     
-    const val EXTRA_INDEX = "EXTRA_INDEX"
+    const val EXTRA_AUDIO_ITEM = "EXTRA_AUDIO_ITEM"
     const val EXTRA_AUDIO_LIST = "EXTRA_AUDIO_LIST"
     
 }
