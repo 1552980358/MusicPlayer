bda2ba70e1cd611afd82b1d0a7fa2957dd773e5c
1552980358 <1552980358@qq.com>
2022-04-20 01:47:25 +0800

    AudioFragment.kt: Inherit to BaseMainFragment and implement scroll action of RecyclerView

--- app/src/main/java/projekt/cloud/piece/music/player/ui/main/audio/AudioFragment.kt
+++ app/src/main/java/projekt/cloud/piece/music/player/ui/main/audio/AudioFragment.kt
@@ -9,12 +9,14 @@ import android.view.ViewGroup.LayoutParams.MATCH_PARENT
 import androidx.core.os.bundleOf
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.SCROLL_STATE_IDLE
 import projekt.cloud.piece.music.player.MainActivityViewModel.Companion.LABEL_AUDIO_LIST
 import projekt.cloud.piece.music.player.base.BaseFragment
 import projekt.cloud.piece.music.player.database.audio.item.AudioItem
 import projekt.cloud.piece.music.player.service.play.Extras.EXTRA_AUDIO_ITEM
 import projekt.cloud.piece.music.player.service.play.Extras.EXTRA_AUDIO_LIST
 import projekt.cloud.piece.music.player.ui.main.audio.util.RecyclerViewAdapter
+import projekt.cloud.piece.music.player.ui.main.base.BaseMainFragment
 
 /**
  * Class [AudioFragment], inherit to [BaseFragment]
@@ -33,7 +35,7 @@ import projekt.cloud.piece.music.player.ui.main.audio.util.RecyclerViewAdapter
  *   [onViewCreated]
  *
  **/
-class AudioFragment: BaseFragment() {
+class AudioFragment: BaseMainFragment() {
 
     companion object {
         private const val TAG = "AudioFragment"
@@ -50,6 +52,16 @@ class AudioFragment: BaseFragment() {
             layoutParams = LayoutParams(MATCH_PARENT, MATCH_PARENT)
             layoutManager = LinearLayoutManager(requireContext())
             recyclerViewAdapter = RecyclerViewAdapter(this)
+            addOnScrollListener(object: RecyclerView.OnScrollListener() {
+                override fun onScrollStateChanged(recyclerView: RecyclerView, newState: Int) {
+                    if (recyclerViewAdapter.audioList?.isEmpty() == false
+                        && !recyclerView.canScrollVertically(1)
+                        && newState == SCROLL_STATE_IDLE) {
+                        return onScrolledToBottom()
+                    }
+                    onLeaveBottom()
+                }
+            })
         }
         return recyclerView
     }
