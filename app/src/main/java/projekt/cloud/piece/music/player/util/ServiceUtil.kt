e7299235cb0de1462f4b67179bfb4aa88fd34618
1552980358 <1552980358@qq.com>
2022-04-19 12:55:28 +0800

    ServiceUtil.kt: Update methods

---
+++ app/src/main/java/projekt/cloud/piece/music/player/util/ServiceUtil.kt
@@ -0,0 +1,19 @@
+package projekt.cloud.piece.music.player.util
+
+import android.app.Service
+import android.content.Context
+import android.content.Intent
+import android.os.Build.VERSION.SDK_INT
+import android.os.Build.VERSION_CODES.O
+
+object ServiceUtil {
+    
+    fun <S: Service> S.startSelf(intent: Intent.() -> Unit) =
+        Intent(this, this::class.java).apply(intent).run { startServiceIntent(this) }
+    
+    private fun Context.startServiceIntent(intent: Intent) = when {
+        SDK_INT >= O -> startForegroundService(intent)
+        else -> startService(intent)
+    }
+    
+}
