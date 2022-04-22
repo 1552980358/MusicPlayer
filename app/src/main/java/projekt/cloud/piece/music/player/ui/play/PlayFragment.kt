55712bb66737d32d93daea0241622521683d6159
1552980358 <1552980358@qq.com>
2022-04-23 01:14:54 +0800

    PlayFragment.kt: Update and implement dynamic background color

--- app/src/main/java/projekt/cloud/piece/music/player/ui/play/PlayFragment.kt
+++ app/src/main/java/projekt/cloud/piece/music/player/ui/play/PlayFragment.kt
@@ -1,13 +1,18 @@
 package projekt.cloud.piece.music.player.ui.play
 
+import android.animation.ValueAnimator
+import android.graphics.Color.TRANSPARENT
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.annotation.ColorInt
 import androidx.lifecycle.ViewModelProvider
 import com.google.android.material.transition.MaterialContainerTransform
+import projekt.cloud.piece.music.player.MainActivityViewModel.Companion.LABEL_COLOR_ITEM
 import projekt.cloud.piece.music.player.base.BaseFragment
 import projekt.cloud.piece.music.player.base.BasePagerViewModel
+import projekt.cloud.piece.music.player.database.audio.item.ColorItem
 import projekt.cloud.piece.music.player.databinding.FragmentPlayBinding
 import projekt.cloud.piece.music.player.ui.play.control.PlayControlFragment
 import projekt.cloud.piece.music.player.ui.play.lyric.PlayLyricFragment
@@ -31,6 +36,10 @@ import projekt.cloud.piece.music.player.util.Constant.ANIMATION_DURATION
  **/
 class PlayFragment: BaseFragment() {
 
+    companion object {
+        private const val TAG = "PlayFragment"
+    }
+
     class PlayFragmentViewModel: BasePagerViewModel<BaseFragment>() {
         override fun setFragments() =
             arrayOf(PlayControlFragment(), PlayLyricFragment())
@@ -53,11 +62,15 @@ class PlayFragment: BaseFragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         _binding = FragmentPlayBinding.inflate(inflater, container, false)
+        binding.backgroundColor = containerViewModel.colorItem?.background ?: TRANSPARENT
         return root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         viewModel.setUpViewPager2(this, viewPager2)
+        containerViewModel.register<ColorItem>(TAG, LABEL_COLOR_ITEM) { colorItem ->
+            colorItem?.let { updateBackgroundColor(binding.backgroundColor!!, it.background) }
+        }
     }
 
     override fun onBackPressed() =
@@ -66,6 +79,14 @@ class PlayFragment: BaseFragment() {
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    private fun updateBackgroundColor(@ColorInt originColor: Int, @ColorInt newColor: Int) {
+        ValueAnimator.ofArgb(originColor, newColor).apply {
+            duration = ANIMATION_DURATION
+            addUpdateListener { binding.backgroundColor = it.animatedValue as Int }
+            start()
+        }
     }
 
 }
