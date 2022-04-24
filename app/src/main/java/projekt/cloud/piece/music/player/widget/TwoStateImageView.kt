7e4fc9bfc56119bc2fc38c08d3a8242b82d3b513
1552980358 <1552980358@qq.com>
2022-04-25 01:16:11 +0800

    TwoStateImageView.kt: Update TwoStateImageView with styleable resource

---
+++ app/src/main/java/projekt/cloud/piece/music/player/widget/TwoStateImageView.kt
@@ -0,0 +1,45 @@
+package projekt.cloud.piece.music.player.widget
+
+import android.content.Context
+import android.graphics.drawable.Drawable
+import android.util.AttributeSet
+import androidx.appcompat.widget.AppCompatImageView
+import androidx.databinding.BindingAdapter
+import projekt.cloud.piece.music.player.R
+
+class TwoStateImageView(context: Context, attributeSet: AttributeSet?): AppCompatImageView(context, attributeSet) {
+
+    companion object {
+
+        @JvmStatic
+        @BindingAdapter("isChecked")
+        fun TwoStateImageView.updateState(newState: Boolean) {
+            if (state != newState) {
+                state = newState
+            }
+        }
+
+    }
+
+    private val positiveImage: Drawable
+    private val negativeImage: Drawable
+
+    private var state = false
+        set(value) {
+            field = value
+            setImageDrawable(when {
+                value -> positiveImage
+                else -> negativeImage
+            })
+        }
+
+    init {
+        context.theme.obtainStyledAttributes(
+            attributeSet, R.styleable.TwoStateImageView, 0, 0
+        ).apply {
+            positiveImage = getDrawable(R.styleable.TwoStateImageView_positiveImage)!!
+            negativeImage = getDrawable(R.styleable.TwoStateImageView_negativeImage)!!
+        }
+    }
+
+}
