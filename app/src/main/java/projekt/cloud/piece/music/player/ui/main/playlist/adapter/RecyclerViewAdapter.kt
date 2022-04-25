f69cfa73cc81f0600f3c4c45202f756e1f6d1e86
1552980358 <1552980358@qq.com>
2022-04-25 19:15:32 +0800

    RecyclerViewAdapter.kt: Update adapter, view holder and layout

---
+++ app/src/main/java/projekt/cloud/piece/music/player/ui/main/playlist/adapter/RecyclerViewAdapter.kt
@@ -0,0 +1,54 @@
+package projekt.cloud.piece.music.player.ui.main.playlist.adapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.View.OnClickListener
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.Adapter
+import androidx.recyclerview.widget.RecyclerView.ViewHolder
+import projekt.cloud.piece.music.player.database.audio.refs.PlaylistWithCountRef.PlaylistWithCount
+import projekt.cloud.piece.music.player.databinding.LayoutRecyclerPlaylistBinding
+
+class RecyclerViewAdapter(recyclerView: RecyclerView) {
+
+    private inner class RecyclerViewHolder(private val binding: LayoutRecyclerPlaylistBinding): ViewHolder(binding.root), OnClickListener {
+        fun onBind(playlistWithCount: PlaylistWithCount) {
+            binding.playlistWithCount = playlistWithCount
+            binding.root.setOnClickListener(this)
+        }
+
+        override fun onClick(v: View?) {
+            binding.playlistWithCount?.let { onClick?.invoke(it.playlistItem.id) }
+        }
+    }
+
+    private inner class RecyclerViewAdapter: Adapter<RecyclerViewHolder>() {
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) = RecyclerViewHolder(
+            LayoutRecyclerPlaylistBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        )
+
+        override fun onBindViewHolder(holder: RecyclerViewHolder, position: Int) {
+            playlistList?.get(position)
+                ?.let { holder.onBind(it) }
+        }
+
+        override fun getItemCount() = playlistList?.size ?: 0
+    }
+
+    private val adapter = RecyclerViewAdapter()
+
+    var playlistList: List<PlaylistWithCount>? = null
+        set(value) {
+            field = value
+            @Suppress("NotifyDataSetChanged")
+            adapter.notifyDataSetChanged()
+        }
+
+    private var onClick: ((String) -> Unit)? = null
+
+    init {
+        recyclerView.adapter = adapter
+    }
+
+}
