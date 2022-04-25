cd97303ab6a704eb5464d4277745902a7c9db054
1552980358 <1552980358@qq.com>
2022-04-25 13:04:47 +0800

    database: Update PlaylistAudioItem and PlaylistAudioDao

---
+++ app/src/main/java/projekt/cloud/piece/music/player/database/audio/dao/PlaylistAudioDao.kt
@@ -0,0 +1,26 @@
+package projekt.cloud.piece.music.player.database.audio.dao
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Update
+import projekt.cloud.piece.music.player.database.audio.item.AudioItem
+import projekt.cloud.piece.music.player.database.audio.item.PlaylistAudioItem
+
+@Dao
+interface PlaylistAudioDao {
+
+    @Query("select * from audio where id in (select audio from `playlist-audio` where playlist = :playlist)")
+    fun query(playlist: String): List<AudioItem>
+
+    @Insert
+    fun insert(vararg playlistAudios: PlaylistAudioItem)
+
+    @Update
+    fun update(playlistAudio: PlaylistAudioItem)
+
+    @Delete
+    fun delete(vararg playlistAudios: PlaylistAudioItem)
+
+}
