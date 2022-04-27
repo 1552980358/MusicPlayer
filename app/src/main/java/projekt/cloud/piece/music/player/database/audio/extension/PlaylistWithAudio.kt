0f6906feaf3ee10a090403ac724facef7ad6b27d
1552980358 <1552980358@qq.com>
2022-04-27 18:50:55 +0800

    PlaylistWithAudio.kt: Update PlaylistWithAudio

---
+++ app/src/main/java/projekt/cloud/piece/music/player/database/audio/extension/PlaylistWithAudio.kt
@@ -0,0 +1,34 @@
+package projekt.cloud.piece.music.player.database.audio.extension
+
+import projekt.cloud.piece.music.player.database.audio.item.AudioItem
+import projekt.cloud.piece.music.player.database.audio.item.PlaylistItem
+
+data class PlaylistWithAudio(val playlistItem: PlaylistItem, val audioList: ArrayList<AudioItem> = ArrayList()) {
+
+    constructor(playlistItem: PlaylistItem, audioList: List<AudioItem>): this(
+        playlistItem,
+        if (audioList is ArrayList<AudioItem>) audioList
+        else audioList.toMutableList() as ArrayList<AudioItem>
+    )
+
+    val title get() = playlistItem.title
+
+    val size get() = audioList.size
+
+    val sizeStr get() = size.toString()
+
+    operator fun plusAssign(audioItem: AudioItem) {
+        audioList.add(audioItem)
+    }
+
+    operator fun minusAssign(audioItem: AudioItem) {
+        audioList.remove(audioItem)
+    }
+
+    operator fun minusAssign(index: Int) {
+        if (index in 0 .. audioList.lastIndex) {
+            audioList.removeAt(index)
+        }
+    }
+
+}
