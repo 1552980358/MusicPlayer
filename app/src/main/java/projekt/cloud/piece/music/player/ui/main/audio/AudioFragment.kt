bb093b028de4daade65def51286f0a05b72da6fb
1552980358 <1552980358@qq.com>
2022-05-06 18:01:58 +0800

    AudioFragment.kt: Implement method isRecyclerViewBottom

--- app/src/main/java/projekt/cloud/piece/music/player/ui/main/audio/AudioFragment.kt
+++ app/src/main/java/projekt/cloud/piece/music/player/ui/main/audio/AudioFragment.kt
@@ -6,15 +6,11 @@ import android.view.View
 import android.view.ViewGroup
 import android.view.ViewGroup.LayoutParams
 import android.view.ViewGroup.LayoutParams.MATCH_PARENT
-import androidx.core.os.bundleOf
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import androidx.recyclerview.widget.RecyclerView.SCROLL_STATE_IDLE
 import projekt.cloud.piece.music.player.MainActivityViewModel.Companion.LABEL_AUDIO_LIST
 import projekt.cloud.piece.music.player.base.BaseFragment
 import projekt.cloud.piece.music.player.database.audio.item.AudioItem
-import projekt.cloud.piece.music.player.service.play.Extras.EXTRA_AUDIO_ITEM
-import projekt.cloud.piece.music.player.service.play.Extras.EXTRA_AUDIO_LIST
 import projekt.cloud.piece.music.player.ui.main.audio.util.RecyclerViewAdapter
 import projekt.cloud.piece.music.player.ui.main.base.BaseMainFragment
 
@@ -52,15 +48,6 @@ class AudioFragment: BaseMainFragment() {
             layoutParams = LayoutParams(MATCH_PARENT, MATCH_PARENT)
             layoutManager = LinearLayoutManager(requireContext())
             recyclerViewAdapter = RecyclerViewAdapter(this)
-            addOnScrollListener(object: RecyclerView.OnScrollListener() {
-                override fun onScrollStateChanged(recyclerView: RecyclerView, newState: Int) {
-                    if (recyclerViewAdapter.audioList?.isEmpty() == false
-                        && !recyclerView.canScrollVertically(1)) {
-                        return onScrolledToBottom()
-                    }
-                    onLeaveBottom()
-                }
-            })
         }
         return recyclerView
     }
@@ -78,5 +65,8 @@ class AudioFragment: BaseMainFragment() {
         super.onDestroyView()
         _recyclerView = null
     }
+    
+    override val isRecyclerViewBottom get() =
+        !recyclerView.canScrollVertically(1)
 
 }
