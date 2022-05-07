3d92f6d601d3e82da59231fafc3b5ee274519664
1552980358 <1552980358@qq.com>
2022-05-08 01:42:19 +0800

    mp3_header_ptr_convert.h: Implement method get_mp3_header_ptr

--- app/src/main/cpp/mp3decoder/include/mp3_header_ptr_convert.h
+++ app/src/main/cpp/mp3decoder/include/mp3_header_ptr_convert.h
@@ -3,20 +3,20 @@
 
 #include "mp3_header.h"
 
-short get_mp3_version(long mp3_header_ptr) {
-    return ((mp3_header *) mp3_header_ptr)->get_version();
+short get_mp3_version(long address) {
+    return get_mp3_header_ptr(address)->get_version();
 }
 
-short get_mp3_layer(long mp3_header_ptr) {
-    return ((mp3_header *) mp3_header_ptr)->get_layer();
+short get_mp3_layer(long address) {
+    return get_mp3_header_ptr(address)->get_layer();
 }
 
-int get_mp3_bit_rate(long mp3_header_ptr) {
-    return ((mp3_header *) mp3_header_ptr)->get_bit_rate();
+int get_mp3_bit_rate(long address) {
+    return get_mp3_header_ptr(address)->get_bit_rate();
 }
 
-int get_mp3_sample_rate(long mp3_header_ptr) {
-    return ((mp3_header *) mp3_header_ptr)->get_sample_rate();
+int get_mp3_sample_rate(long address) {
+    return get_mp3_header_ptr(address)->get_sample_rate();
 }
 
 #endif //CLOUDPIECE_MP3_HEADER_PTR_CONVERT_H
