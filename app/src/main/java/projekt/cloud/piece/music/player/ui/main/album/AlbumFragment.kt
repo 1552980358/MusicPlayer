59ca46a184d01f458b5c0ba819dd009d0112d01a
1552980358 <1552980358@qq.com>
2022-05-07 12:13:34 +0800

    AlbumFragment.kt: Implement RecyclerView and RecyclerViewAdapter

--- app/src/main/java/projekt/cloud/piece/music/player/ui/main/album/AlbumFragment.kt
+++ app/src/main/java/projekt/cloud/piece/music/player/ui/main/album/AlbumFragment.kt
@@ -1,6 +1,42 @@
 package projekt.cloud.piece.music.player.ui.main.album
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.coroutines.withContext
+import projekt.cloud.piece.music.player.ui.main.album.adapter.RecyclerViewAdapter
 import projekt.cloud.piece.music.player.ui.main.base.BaseMainFragment
+import projekt.cloud.piece.music.player.util.CoroutineUtil
+import projekt.cloud.piece.music.player.util.CoroutineUtil.ui
 
 class AlbumFragment: BaseMainFragment() {
+    
+    private var _recyclerView: RecyclerView? = null
+    private val recyclerView get() = _recyclerView!!
+    
+    private lateinit var recyclerViewAdapter: RecyclerViewAdapter
+    
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        _recyclerView = RecyclerView(requireContext())
+        with(recyclerView) {
+            layoutManager = LinearLayoutManager(requireContext())
+            recyclerViewAdapter = RecyclerViewAdapter(this)
+        }
+        return recyclerView
+    }
+    
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        ui {
+            recyclerViewAdapter.albumList = withContext(CoroutineUtil.io) {
+                audioRoom.albumDao.queryAll()
+            }
+        }
+    }
+    
+    override val isRecyclerViewBottom get() =
+        !recyclerView.canScrollVertically(1)
+    
 }
