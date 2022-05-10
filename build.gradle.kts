718e27745ed329bb970bcbb3ca784d52ec11cfc4
1552980358 <1552980358@qq.com>
2022-05-10 12:57:23 +0800

    build.gradle.kts: Update build tool version to 7.2.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 buildscript {
     extra.apply {
-        set("gradleVersion", "7.1.3")
+        set("gradleVersion", "7.2.0")
         set("kotlinVersion", "1.6.21")
         set("navVersion", "2.4.2")
     }
