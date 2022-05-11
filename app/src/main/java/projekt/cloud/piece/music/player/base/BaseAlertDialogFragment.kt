ad69c06c89f00c72f6dbf6f36f54adfb2649e62f
1552980358 <1552980358@qq.com>
2022-05-12 03:13:54 +0800

    BaseAlertDialogFragment.kt: Update BaseAlertDialogFragment

---
+++ app/src/main/java/projekt/cloud/piece/music/player/base/BaseAlertDialogFragment.kt
@@ -0,0 +1,40 @@
+package projekt.cloud.piece.music.player.base
+
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AlertDialog
+
+abstract class BaseAlertDialogFragment: BaseDialogFragment() {
+    
+    protected abstract val dialogTitle: String
+    protected abstract val contentView: View
+    
+    protected abstract val positiveText: String
+    protected abstract val onPositiveClick: () -> Unit
+    
+    protected abstract val negativeText: String
+    protected abstract val onNegativeClick: (() -> Unit)?
+    
+    protected abstract val neutralText: String?
+    protected abstract val onNeutralClick: (() -> Unit)?
+    
+    protected var defaultValue: String? = null
+    protected abstract val currentValue: String?
+    
+    protected abstract val hint: String
+    protected var prefix: String? = null
+    protected var suffix: String? = null
+    
+    override fun onCreateDialog(savedInstanceState: Bundle?) = AlertDialog.Builder(requireContext())
+        .setTitle(dialogTitle)
+        .setView(contentView)
+        .apply {
+            setPositiveButton(positiveText) { _, _ -> onPositiveClick.invoke() }
+            setNegativeButton(negativeText) { _, _ -> onNegativeClick?.invoke() }
+            neutralText?.let { neutralText ->
+                setNeutralButton(neutralText) { _, _ -> onNeutralClick?.invoke() }
+            }
+        }
+        .create()
+    
+}
