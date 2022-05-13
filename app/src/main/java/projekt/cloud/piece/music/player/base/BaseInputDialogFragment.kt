6efd46a4bd7e25a395454f1bdd535b2e06e01d48
1552980358 <1552980358@qq.com>
2022-05-13 13:14:56 +0800

    BaseInputDialogFragment.kt: Inherit to BaseItemSelectDialogFragment and implement methods

--- app/src/main/java/projekt/cloud/piece/music/player/base/BaseInputDialogFragment.kt
+++ app/src/main/java/projekt/cloud/piece/music/player/base/BaseInputDialogFragment.kt
@@ -1,44 +1,19 @@
 package projekt.cloud.piece.music.player.base
 
 import android.view.View
-import projekt.cloud.piece.music.player.databinding.DialogFragmentBaseInputBinding
+import android.view.View.GONE
 
-abstract class BaseInputDialogFragment: BaseAlertDialogFragment() {
+abstract class BaseInputDialogFragment: BaseItemSelectDialogFragment() {
     
-    private var _binding: DialogFragmentBaseInputBinding? = null
-    private val binding get() = _binding!!
-    private val root get() = binding.root
-    
-    private val textInputLayout get() = binding.textInputLayout
-    private val textInputEditText get() = binding.textInputEditText
-    
-    protected abstract val positiveClick: (String?) -> Unit
-    protected abstract val negativeClick: ((String?) -> Unit)?
-    protected abstract val neutralClick: ((String?) -> Unit)?
+    override val contentView: View get() {
+        super.contentView
+        isTextInputEnabled = true
+        textInputLayoutMenu.visibility = GONE
+        return root
+    }
     
     override val onPositiveClick: () -> Unit get() = {
         positiveClick.invoke(textInputEditText.text?.toString())
-    }
-    
-    override val onNegativeClick: (() -> Unit)? get() = when (val negativeClick = negativeClick) {
-        null -> null
-        else -> {{ negativeClick.invoke(currentValue) }}
-    }
-    
-    override val onNeutralClick: (() -> Unit)? get() = when (val neutralClick = neutralClick) {
-        null -> null
-        else -> {{ neutralClick.invoke(defaultValue) }}
-    }
-    
-    override val contentView: View get() {
-        _binding = DialogFragmentBaseInputBinding.inflate(layoutInflater)
-        with(textInputLayout) {
-            hint = this@BaseInputDialogFragment.hint
-            prefix?.let { prefixText = it }
-            suffix?.let { suffixText = it }
-        }
-        textInputEditText.setText(currentValue ?: defaultValue)
-        return root
     }
     
 }
