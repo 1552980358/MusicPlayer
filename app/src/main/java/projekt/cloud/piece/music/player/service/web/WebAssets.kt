dbaba82f3478aac7715c0909d80c586ce25f4d68
1552980358 <1552980358@qq.com>
2022-05-26 03:15:41 +0800

    WebAssets.kt: Update constants and methods

---
+++ app/src/main/java/projekt/cloud/piece/music/player/service/web/WebAssets.kt
@@ -0,0 +1,28 @@
+package projekt.cloud.piece.music.player.service.web
+
+import android.content.Context
+import java.io.File
+import projekt.cloud.piece.music.player.service.web.ServerRouting.STATIC_PATH_CSS
+import projekt.cloud.piece.music.player.service.web.ServerRouting.STATIC_PATH_IMG
+import projekt.cloud.piece.music.player.service.web.ServerRouting.STATIC_PATH_JS
+
+object WebAssets {
+    
+    private const val DIR_WEB_ASSETS = "web"
+    private const val DIR_WEB_ASSETS_CSS = STATIC_PATH_CSS
+    private const val DIR_WEB_ASSETS_IMG = STATIC_PATH_IMG
+    private const val DIR_WEB_ASSETS_JS = STATIC_PATH_JS
+    
+    @JvmStatic
+    val Context.webAssetsDir get() = getExternalFilesDir(DIR_WEB_ASSETS)
+    
+    @JvmStatic
+    val Context.webAssetsCssDir get() = File(webAssetsDir, DIR_WEB_ASSETS_CSS)
+    
+    @JvmStatic
+    val Context.webAssetsImgDir get() = File(webAssetsDir, DIR_WEB_ASSETS_IMG)
+    
+    @JvmStatic
+    val Context.webAssetsJSDir get() = File(webAssetsDir, DIR_WEB_ASSETS_JS)
+    
+}
