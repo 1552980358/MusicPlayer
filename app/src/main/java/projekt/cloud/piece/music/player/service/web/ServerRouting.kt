b5ca32e209324ed4b67339facbcec55c9108930b
1552980358 <1552980358@qq.com>
2022-05-26 03:16:05 +0800

    ServerRouting.kt: Update constants

---
+++ app/src/main/java/projekt/cloud/piece/music/player/service/web/ServerRouting.kt
@@ -0,0 +1,14 @@
+package projekt.cloud.piece.music.player.service.web
+
+object ServerRouting {
+    
+    const val STATIC_PATH_ROOT = "/"
+    const val STATIC_FILE_INDEX_HTML = "index.html"
+    const val STATIC_FILE_FAVICON_ICO = "favicon.ico"
+    
+    const val STATIC_PATH_CSS = "css"
+    const val STATIC_PATH_IMG = "img"
+    const val STATIC_PATH_JS = "js"
+    const val STATIC_FILE_ALL = "."
+
+}
