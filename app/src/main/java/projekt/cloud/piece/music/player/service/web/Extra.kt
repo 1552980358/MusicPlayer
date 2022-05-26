734a4938f373dc471a2d0a05d71bfc0807258ba9
1552980358 <1552980358@qq.com>
2022-05-26 18:51:12 +0800

    Extra.kt: Update constants

---
+++ app/src/main/java/projekt/cloud/piece/music/player/service/web/Extra.kt
@@ -0,0 +1,8 @@
+package projekt.cloud.piece.music.player.service.web
+
+object Extra {
+    
+    const val ACTION_START_SERVER = "ACTION_START_SERVER"
+    const val ACTION_STOP_SERVER = "ACTION_STOP_SERVER"
+    
+}
