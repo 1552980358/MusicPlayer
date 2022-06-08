8dfe5eaadf7920dad92852ab6a61962d39ff6b05
1552980358 <1552980358@qq.com>
2022-06-09 02:08:42 +0800

    Application.kt: Update launching web service when enabled

---
+++ app/src/main/java/projekt/cloud/piece/music/player/Application.kt
@@ -0,0 +1,22 @@
+package projekt.cloud.piece.music.player
+
+import android.app.Application
+import projekt.cloud.piece.music.player.service.WebService
+import projekt.cloud.piece.music.player.service.web.Extra.ACTION_START_COMMAND
+import projekt.cloud.piece.music.player.service.web.Extra.ACTION_START_SERVER
+import projekt.cloud.piece.music.player.util.PreferenceUtil.boolPrefs
+import projekt.cloud.piece.music.player.util.ServiceUtil.serviceIntent
+
+class Application: Application() {
+    
+    override fun onCreate() {
+        super.onCreate()
+        
+        boolPrefs(R.string.key_web_server_switch) {
+            startService(serviceIntent<WebService> {
+                putExtra(ACTION_START_COMMAND, ACTION_START_SERVER)
+            })
+        }
+    }
+    
+}
