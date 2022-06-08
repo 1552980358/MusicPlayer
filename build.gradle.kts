0ac05d968a47f3de92b701fc50047449e986b124
1552980358 <1552980358@qq.com>
2022-06-08 12:47:18 +0800

    build.gradle.kts: Update AGP to 7.2.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 buildscript {
     extra.apply {
-        set("gradleVersion", "7.2.0")
+        set("gradleVersion", "7.2.1")
         set("kotlinVersion", "1.6.21")
         set("navVersion", "2.4.2")
     }
