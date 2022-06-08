8ae01697a1267594c54788723c23ff7ab98dc061
1552980358 <1552980358@qq.com>
2022-06-08 21:43:53 +0800

    PreferenceUtil.kt: Update method strPrefs

--- app/src/main/java/projekt/cloud/piece/music/player/util/PreferenceUtil.kt
+++ app/src/main/java/projekt/cloud/piece/music/player/util/PreferenceUtil.kt
@@ -1,9 +1,13 @@
 package projekt.cloud.piece.music.player.util
 
+import android.content.Context
 import androidx.annotation.StringRes
 import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
+import androidx.preference.PreferenceManager
 import androidx.preference.SwitchPreferenceCompat
+import projekt.cloud.piece.music.player.util.CoroutineUtil.io
+import projekt.cloud.piece.music.player.util.CoroutineUtil.ui
 
 object PreferenceUtil {
     
@@ -28,5 +32,12 @@ object PreferenceUtil {
     @JvmStatic
     fun PreferenceFragmentCompat.switchPreference(@StringRes resId: Int, switchPreference: SwitchPreferenceCompat.() -> Unit) =
         switchPreference(resId)?.apply(switchPreference)
+    
+    @JvmStatic
+    fun Context.strPrefs(@StringRes resId: Int, defaultValue: String? = null, preference: (String?) -> Unit) = io {
+        PreferenceManager.getDefaultSharedPreferences(this@strPrefs).getString(getString(resId), defaultValue)?.let {
+            ui { preference.invoke(it) }
+        }
+    }
     
 }
