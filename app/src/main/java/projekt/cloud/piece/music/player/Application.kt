29f482f5e72c177c1708db3b10e5034c5f1eaae3
1552980358 <1552980358@qq.com>
2022-06-10 21:40:01 +0800

    SharedPreferencesUtil.kt: Update and implement methods

--- app/src/main/java/projekt/cloud/piece/music/player/Application.kt
+++ app/src/main/java/projekt/cloud/piece/music/player/Application.kt
@@ -4,18 +4,21 @@ import android.app.Application
 import projekt.cloud.piece.music.player.service.WebService
 import projekt.cloud.piece.music.player.service.web.Extra.ACTION_START_COMMAND
 import projekt.cloud.piece.music.player.service.web.Extra.ACTION_START_SERVER
-import projekt.cloud.piece.music.player.util.SharedPreferencesUtil.boolPrefs
+import projekt.cloud.piece.music.player.util.CoroutineUtil.ui
+import projekt.cloud.piece.music.player.util.SharedPreferencesUtil.boolPrefsUnblock
 import projekt.cloud.piece.music.player.util.ServiceUtil.serviceIntent
 
 class Application: Application() {
     
     override fun onCreate() {
         super.onCreate()
-        
-        boolPrefs(R.string.key_web_server_switch) {
-            startService(serviceIntent<WebService> {
-                putExtra(ACTION_START_COMMAND, ACTION_START_SERVER)
-            })
+    
+        boolPrefsUnblock(R.string.key_web_server_switch) {
+            ui {
+                startService(serviceIntent<WebService> {
+                    putExtra(ACTION_START_COMMAND, ACTION_START_SERVER)
+                })
+            }
         }
     }
     
--- app/src/main/java/projekt/cloud/piece/music/player/util/SharedPreferencesUtil.kt
+++ app/src/main/java/projekt/cloud/piece/music/player/util/SharedPreferencesUtil.kt
@@ -3,20 +3,29 @@ package projekt.cloud.piece.music.player.util
 import android.content.Context
 import androidx.annotation.StringRes
 import androidx.preference.PreferenceManager
+import projekt.cloud.piece.music.player.util.CoroutineUtil.io
 import projekt.cloud.piece.music.player.util.CoroutineUtil.ui
 
 object SharedPreferencesUtil {
     
     @JvmStatic
-    fun Context.strPrefs(@StringRes resId: Int, defaultValue: String? = null, preference: (String?) -> Unit) = CoroutineUtil.io {
-        PreferenceManager.getDefaultSharedPreferences(this@strPrefs).getString(getString(resId), defaultValue).let {
+    fun Context.strPrefs(@StringRes resId: Int, defaultValue: String? = null) =
+        PreferenceManager.getDefaultSharedPreferences(this@strPrefs).getString(getString(resId), defaultValue)
+    
+    @JvmStatic
+    fun Context.strPrefsUnblock(@StringRes resId: Int, defaultValue: String? = null, preference: (String?) -> Unit) = io {
+        strPrefs(resId, defaultValue).let {
             ui { preference.invoke(it) }
         }
     }
     
     @JvmStatic
-    fun Context.boolPrefs(@StringRes resId: Int, defaultValue: Boolean = false, preference: (Boolean) -> Unit) = CoroutineUtil.io {
-        PreferenceManager.getDefaultSharedPreferences(this@boolPrefs).getBoolean(getString(resId), defaultValue).let {
+    fun Context.boolPrefs(@StringRes resId: Int, defaultValue: Boolean = false) =
+        PreferenceManager.getDefaultSharedPreferences(this@boolPrefs).getBoolean(getString(resId), defaultValue)
+    
+    @JvmStatic
+    fun Context.boolPrefsUnblock(@StringRes resId: Int, defaultValue: Boolean = false, preference: (Boolean) -> Unit) = io {
+        boolPrefs(resId, defaultValue).let {
             ui { preference.invoke(it) }
         }
     }
