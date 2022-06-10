699496c5dae153c3d5271d1636d183a06683a3c7
1552980358 <1552980358@qq.com>
2022-06-10 21:45:43 +0800

    WebServerPreferenceFragment.kt: Implement link

--- app/src/main/java/projekt/cloud/piece/music/player/ui/dialog/webServer/WebServerPreferenceFragment.kt
+++ app/src/main/java/projekt/cloud/piece/music/player/ui/dialog/webServer/WebServerPreferenceFragment.kt
@@ -9,11 +9,21 @@ import projekt.cloud.piece.music.player.service.web.Extra.ACTION_START_SERVER
 import projekt.cloud.piece.music.player.service.web.Extra.ACTION_STOP_SERVER
 import projekt.cloud.piece.music.player.service.web.WebAssets.webAssetsDir
 import projekt.cloud.piece.music.player.service.web.WebAssets.webAssetsVersionFile
+import projekt.cloud.piece.music.player.service.web.WebServer.Companion.SERVER_PORT
 import projekt.cloud.piece.music.player.util.FragmentUtil.startService
+import projekt.cloud.piece.music.player.util.NetworkHelper
 import projekt.cloud.piece.music.player.util.PreferenceUtil.preference
 import projekt.cloud.piece.music.player.util.PreferenceUtil.switchPreference
+import projekt.cloud.piece.music.player.util.SharedPreferencesUtil.boolPrefs
 
 class WebServerPreferenceFragment: BasePreferenceFragment() {
+    
+    private lateinit var networkHelper: NetworkHelper
+    
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        networkHelper = NetworkHelper.create(requireContext())
+    }
     
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.xml_web_server, rootKey)
@@ -25,8 +35,13 @@ class WebServerPreferenceFragment: BasePreferenceFragment() {
             }
             summary = versionFile.readText()
         }
+    
+        preference(R.string.key_web_server_info_ip)?.summary = networkHelper.ipAddress
         
+        preference(R.string.web_server_info_port)?.summary = SERVER_PORT.toString()
+    
         switchPreference(R.string.key_web_server_switch) {
+            setLink(requireContext().boolPrefs(R.string.key_web_server_switch, false))
             setOnPreferenceChangeListener { _, newValue ->
                 startService<WebService> {
                     putExtra(
@@ -37,8 +52,16 @@ class WebServerPreferenceFragment: BasePreferenceFragment() {
                         }
                     )
                 }
+                setLink(newValue as Boolean)
                 true
             }
+        }
+    }
+    
+    private fun setLink(enabled: Boolean) {
+        preference(R.string.web_server_info_link)?.summary = when {
+            enabled -> null
+            else -> "${preference(R.string.key_web_server_info_ip)?.summary}:${preference(R.string.web_server_info_port)?.summary}"
         }
     }
     
