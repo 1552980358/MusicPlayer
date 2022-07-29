604218d3c018870b3037d71f2528a1cacadca87e
1552980358 <1552980358@qq.com>
2022-07-29 15:33:23 +0800

    app/build.gradle.kts: Update dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -54,10 +54,23 @@ android {
 dependencies {
     implementation("androidx.core:core-ktx:1.8.0")
     implementation("androidx.appcompat:appcompat:1.4.2")
-    implementation("com.google.android.material:material:1.7.0-alpha03")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("androidx.media:media:1.6.0")
     implementation("androidx.navigation:navigation-fragment-ktx:2.5.1")
     implementation("androidx.navigation:navigation-ui-ktx:2.5.1")
+    
+    val roomVersion = "2.4.2"
+    implementation( "androidx.room:room-runtime:$roomVersion")
+    ksp("androidx.room:room-compiler:$roomVersion")
+    implementation("androidx.room:room-ktx:$roomVersion")
+    
+    val exoPlayerVersion = "2.18.1"
+    implementation("com.google.android.exoplayer:exoplayer-core:$exoPlayerVersion")
+    implementation("com.google.android.exoplayer:exoplayer-hls:$exoPlayerVersion")
+    implementation("com.google.android.material:material:1.7.0-alpha03")
+    
+    implementation("com.github.1552980358:C2Pinyin:2.3.0")
+    
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.3")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.4.0")
