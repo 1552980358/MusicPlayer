0ce7ee626310d5737d79c6cce0703dd1e231c815
1552980358 <1552980358@qq.com>
2022-07-30 21:41:35 +0800

    MediaStoreUtil.kt: Update media store querying

---
+++ app/src/main/java/projekt/cloud/piece/music/player/util/MediaStoreUtil.kt
@@ -0,0 +1,45 @@
+package projekt.cloud.piece.music.player.util
+
+import android.content.ContentResolver
+import android.content.Context
+import android.provider.MediaStore
+import projekt.cloud.piece.music.player.item.Album
+import projekt.cloud.piece.music.player.item.Artist
+import projekt.cloud.piece.music.player.item.Audio
+
+object MediaStoreUtil {
+
+    fun Context.queryMediaStore(audioList: ArrayList<Audio>, artistList: ArrayList<Artist>, albumList: ArrayList<Album>) =
+        contentResolver.queryMediaStore(audioList, artistList, albumList)
+    
+    private fun ContentResolver.queryMediaStore(audioList: ArrayList<Audio>, artistList: ArrayList<Artist>, albumList: ArrayList<Album>) {
+        query(MediaStore.Audio.Media.EXTERNAL_CONTENT_URI,
+            null, null, null,
+            MediaStore.Audio.AudioColumns.IS_MUSIC)?.apply {
+            var artist: String
+            var album: String
+            while (moveToNext()) {
+                artist = getString(getColumnIndexOrThrow(MediaStore.Audio.AudioColumns.ARTIST_ID))
+                if (artistList.find { it.id == artist } == null) {
+                    artistList.add(Artist(artist, getString(getColumnIndexOrThrow(MediaStore.Audio.AudioColumns.ARTIST))))
+                }
+                album = getString(getColumnIndexOrThrow(MediaStore.Audio.AudioColumns.ALBUM_ID))
+                if (albumList.find { it.id == album } == null) {
+                    albumList.add(Album(album, getString(getColumnIndexOrThrow(MediaStore.Audio.AudioColumns.ALBUM))))
+                }
+                audioList.add(
+                    Audio(
+                        getString(getColumnIndexOrThrow(MediaStore.Audio.AudioColumns._ID)),
+                        getString(getColumnIndexOrThrow(MediaStore.Audio.AudioColumns.TITLE)),
+                        artist,
+                        album,
+                        getLong(getColumnIndexOrThrow(MediaStore.Audio.AudioColumns.DURATION)),
+                        getLong(getColumnIndexOrThrow(MediaStore.Audio.AudioColumns.SIZE)),
+                        getString(getColumnIndexOrThrow(MediaStore.Audio.AudioColumns.DATA))
+                    )
+                )
+            }
+        }?.close()
+    }
+
+}
