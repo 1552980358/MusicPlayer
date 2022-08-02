069d4f952539879f002c4f3d09982f81255f690c
1552980358 <1552980358@qq.com>
2022-08-02 21:06:13 +0800

    database/Audio: Update entity ForeignKeys

--- app/src/main/java/projekt/cloud/piece/music/player/item/Audio.kt
+++ app/src/main/java/projekt/cloud/piece/music/player/item/Audio.kt
@@ -4,11 +4,18 @@ import android.net.Uri
 import android.provider.MediaStore
 import androidx.room.ColumnInfo
 import androidx.room.Entity
+import androidx.room.ForeignKey
+import androidx.room.ForeignKey.CASCADE
 import java.io.File
 import java.io.Serializable
 import projekt.cloud.piece.music.player.item.base.BaseTitledItem
 
-@Entity
+@Entity(
+    foreignKeys = [
+        ForeignKey(entity = Artist::class, parentColumns = ["id"], childColumns = ["artist"], onDelete = CASCADE),
+        ForeignKey(entity = Album::class, parentColumns = ["id"], childColumns = ["album"], onDelete = CASCADE)
+    ]
+)
 class Audio(id: String,
             title: String,
             @ColumnInfo(name = "artist") var artist: String,
