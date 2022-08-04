43cce690e3b6bec6daf3b67346eb8b209250ab11
1552980358 <1552980358@qq.com>
2022-08-04 14:53:14 +0800

    room/AudioMetadata: Update methods

--- app/src/main/java/projekt/cloud/piece/music/player/item/AudioMetadata.kt
+++ app/src/main/java/projekt/cloud/piece/music/player/item/AudioMetadata.kt
@@ -2,17 +2,26 @@ package projekt.cloud.piece.music.player.item
 
 import androidx.room.Embedded
 import androidx.room.Relation
+import java.io.Serializable
 
 class AudioMetadata(
     @Embedded val audio: Audio,
-    @Relation(
-        parentColumn = "artist",
-        entityColumn = "id"
-    )
-    val artist: Artist,
-    @Relation(
-        parentColumn = "album",
-        entityColumn = "id"
-    )
-    val album: Album
-)
+    @Relation(parentColumn = "artist", entityColumn = "id") val artist: Artist,
+    @Relation(parentColumn = "album", entityColumn = "id") val album: Album): Serializable {
+    
+    val id: String
+        get() = audio.id
+    
+    val title: String
+        get() = audio.title
+    
+    val pinyin: String
+        get() = audio.pinyin
+    
+    val artistTitle: String
+        get() = artist.title
+    
+    val albumTitle: String
+        get() = album.title
+    
+}
