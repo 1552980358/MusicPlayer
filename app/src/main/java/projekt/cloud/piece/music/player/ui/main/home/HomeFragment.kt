4590ea2599a8bf0e36740d25e5b514e39b08729c
1552980358 <1552980358@qq.com>
2022-08-04 19:54:21 +0800

    ui/main: Update HomeFragment with resources

--- app/src/main/java/projekt/cloud/piece/music/player/ui/main/home/HomeFragment.kt
+++ app/src/main/java/projekt/cloud/piece/music/player/ui/main/home/HomeFragment.kt
@@ -1,31 +1,82 @@
 package projekt.cloud.piece.music.player.ui.main.home
 
+import android.annotation.SuppressLint
+import android.os.Build.VERSION.SDK_INT
+import android.os.Build.VERSION_CODES.Q
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.WindowInsets
+import android.view.WindowInsets.Type.systemBars
 import androidx.coordinatorlayout.widget.CoordinatorLayout
-import androidx.fragment.app.Fragment
-import androidx.navigation.NavController
-import androidx.navigation.fragment.findNavController
+import androidx.core.view.updateLayoutParams
+import androidx.recyclerview.widget.RecyclerView
 import projekt.cloud.piece.music.player.databinding.FragmentHomeBinding
+import projekt.cloud.piece.music.player.room.AudioDatabase.Companion.audioDatabase
+import projekt.cloud.piece.music.player.ui.main.base.BaseMainFragment
+import projekt.cloud.piece.music.player.util.CoroutineUtil.io
+import projekt.cloud.piece.music.player.util.CoroutineUtil.ui
 
-class HomeFragment: Fragment() {
+class HomeFragment: BaseMainFragment() {
     
     private var _binding: FragmentHomeBinding? = null
     private val binding: FragmentHomeBinding
         get() = _binding!!
     
-    private val root: CoordinatorLayout
-        get() = binding.root
+    private val root get() = binding.root
+    private val recyclerView: RecyclerView
+        get() = binding.recyclerView
     
+    private lateinit var recyclerViewAdapter: RecyclerViewAdapter
+    
+    @SuppressLint("NewApi")
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        root.setOnApplyWindowInsetsListener { _, insets ->
+            recyclerView.updateLayoutParams<CoordinatorLayout.LayoutParams> { setMargins(0, 0, 0, getInsetBottom(insets)) }
+            insets
+        }
         return root
     }
     
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+    private fun getInsetBottom(windowInsets: WindowInsets): Int {
+        if (SDK_INT > Q) {
+            windowInsets.getInsets(systemBars()).bottom
+        }
+        @Suppress("DEPRECATION")
+        return windowInsets.systemWindowInsetBottom
+    }
     
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        recyclerViewAdapter = RecyclerViewAdapter(recyclerView)
+        
+        when (viewModel.isInitialized.value) {
+            true -> io {
+                audioDatabase.audioMetadataDao().query().let {
+                    ui { recyclerViewAdapter.audioMetadataList = it }
+                }
+            }
+            else -> {
+                viewModel.isInitialized.observe(viewLifecycleOwner) {
+                    if (it) {
+                        io {
+                            audioDatabase.audioMetadataDao().query().let {
+                                ui {
+                                    recyclerViewAdapter.audioMetadataList = it
+                                    viewModel.isInitialized.removeObservers(viewLifecycleOwner)
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+    
+    override fun onDestroyView() {
+        viewModel.isInitialized.removeObservers(viewLifecycleOwner)
+        super.onDestroyView()
     }
 
 }
