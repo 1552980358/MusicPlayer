56a6422c9760105e29fe0dc3d5d0b66dde4b03be
1552980358 <1552980358@qq.com>
2022-08-06 12:03:05 +0800

    service/play/NotificationHelper: Update method createNotification()

--- app/src/main/java/projekt/cloud/piece/music/player/service/play/NotificationHelper.kt
+++ app/src/main/java/projekt/cloud/piece/music/player/service/play/NotificationHelper.kt
@@ -3,10 +3,17 @@ package projekt.cloud.piece.music.player.service.play
 import android.app.NotificationChannel
 import android.app.NotificationManager.IMPORTANCE_HIGH
 import android.content.Context
+import android.graphics.Bitmap
 import android.os.Build.VERSION.SDK_INT
 import android.os.Build.VERSION_CODES.O
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationCompat.PRIORITY_MAX
 import androidx.core.app.NotificationManagerCompat
+import androidx.media.app.NotificationCompat.MediaStyle
+import androidx.media.MediaBrowserServiceCompat
 import projekt.cloud.piece.music.player.BuildConfig.APPLICATION_ID
+import projekt.cloud.piece.music.player.R
+import projekt.cloud.piece.music.player.item.AudioMetadata
 
 class NotificationHelper(context: Context) {
     
@@ -32,5 +39,18 @@ class NotificationHelper(context: Context) {
             )
         }
     }
+    
+    fun createNotification(service: MediaBrowserServiceCompat, audioMetadata: AudioMetadata, largeBitmap: Bitmap) =
+        NotificationCompat.Builder(service, CHANNEL_ID)
+            .setPriority(PRIORITY_MAX)
+            .setOngoing(false)
+            .setVibrate(longArrayOf(0))
+            .setSilent(true)
+            .setStyle(MediaStyle().setMediaSession(service.sessionToken).setShowActionsInCompactView(0, 1, 2))
+            .setSmallIcon(R.drawable.ic_launcher_foreground)
+            .setLargeIcon(largeBitmap)
+            .setContentTitle(audioMetadata.title)
+            .setContentText("${audioMetadata.artistName} - ${audioMetadata.albumTitle}")
+            .build()
     
 }
