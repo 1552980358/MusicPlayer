f42d6759c9c1a78aee5892ffbef7cd3796d414e3
1552980358 <1552980358@qq.com>
2022-08-06 15:09:39 +0800

    base/BaseFragment: Update BaseFragment for fragments

---
+++ app/src/main/java/projekt/cloud/piece/music/player/base/BaseFragment.kt
@@ -0,0 +1,24 @@
+package projekt.cloud.piece.music.player.base
+
+import android.media.session.MediaController
+import androidx.core.os.bundleOf
+import androidx.fragment.app.Fragment
+import projekt.cloud.piece.music.player.item.AudioMetadata
+import projekt.cloud.piece.music.player.service.play.ServiceConstants.EXTRA_AUDIO_METADATA_LIST
+
+open class BaseFragment: Fragment() {
+    
+    private val transportControls: MediaController.TransportControls
+        get() = requireActivity().mediaController.transportControls
+    
+    protected fun play() = transportControls.play()
+    
+    protected fun pause() = transportControls.pause()
+    
+    protected fun playAudio(audioId: String, audioMetadataList: ArrayList<AudioMetadata>) =
+        transportControls.playFromMediaId(audioId, bundleOf(EXTRA_AUDIO_METADATA_LIST to audioMetadataList))
+    
+    protected fun sendCustomAction(action: String, vararg pair: Pair<String, Any?>) =
+        transportControls.sendCustomAction(action, bundleOf(*pair))
+    
+}
