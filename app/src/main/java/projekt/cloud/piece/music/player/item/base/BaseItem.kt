c5c6904eccf9903bdf94bfb8450d48aaefe8330f
1552980358 <1552980358@qq.com>
2022-08-06 17:24:59 +0800

    item: Fix type parsing exception

--- app/src/main/java/projekt/cloud/piece/music/player/item/base/BaseItem.kt
+++ app/src/main/java/projekt/cloud/piece/music/player/item/base/BaseItem.kt
@@ -2,8 +2,9 @@ package projekt.cloud.piece.music.player.item.base
 
 import androidx.room.ColumnInfo
 import androidx.room.PrimaryKey
+import java.io.Serializable
 
-abstract class BaseItem(@PrimaryKey @ColumnInfo(name = "id") val id: String) {
+abstract class BaseItem(@PrimaryKey @ColumnInfo(name = "id") val id: String): Serializable {
     
     val idLong: Long
         get() = id.toLong()
--- app/src/main/java/projekt/cloud/piece/music/player/item/base/BaseTitledItem.kt
+++ app/src/main/java/projekt/cloud/piece/music/player/item/base/BaseTitledItem.kt
@@ -1,11 +1,12 @@
 package projekt.cloud.piece.music.player.item.base
 
 import androidx.room.ColumnInfo
+import java.io.Serializable
 import projekt.cloud.piece.c2.pinyin.C2Pinyin.pinyin
 
 abstract class BaseTitledItem(id: String,
                               @ColumnInfo(name = "title") var title: String,
-                              @ColumnInfo(name = "pinyin") var pinyin: String = title.pinyin): BaseItem(id) {
+                              @ColumnInfo(name = "pinyin") var pinyin: String = title.pinyin): BaseItem(id), Serializable {
     
     fun updateTitle(newTitle: String) {
         title = newTitle
