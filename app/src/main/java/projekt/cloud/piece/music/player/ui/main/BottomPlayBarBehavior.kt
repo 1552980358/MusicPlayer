a5d147ff0a6e6db5e4468e7d0e1aba3dfb1cb6c1
1552980358 <1552980358@qq.com>
2022-08-07 12:36:19 +0800

    ui/main/BottomPlayBarBehavior: Update BottomPlayBarBehavior

---
+++ app/src/main/java/projekt/cloud/piece/music/player/ui/main/BottomPlayBarBehavior.kt
@@ -0,0 +1,40 @@
+package projekt.cloud.piece.music.player.ui.main
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+import androidx.coordinatorlayout.widget.CoordinatorLayout
+import com.google.android.material.behavior.HideBottomViewOnScrollBehavior
+
+class BottomPlayBarBehavior<V: View>: HideBottomViewOnScrollBehavior<V> {
+    
+    constructor(): super()
+    constructor(context: Context, attributeSet: AttributeSet?): super(context, attributeSet)
+    
+    private var isAllowMoving = false
+    
+    fun setEnableAllowMoving(child: V) {
+        if (!isAllowMoving) {
+            isAllowMoving = true
+            // Reset state
+            slideDown(child, false)
+            // Show it
+            slideUp(child)
+        }
+    }
+    
+    override fun onNestedScroll(coordinatorLayout: CoordinatorLayout,
+                                child: V,
+                                target: View,
+                                dxConsumed: Int,
+                                dyConsumed: Int,
+                                dxUnconsumed: Int,
+                                dyUnconsumed: Int,
+                                type: Int,
+                                consumed: IntArray) {
+        if (isAllowMoving) {
+            super.onNestedScroll(coordinatorLayout, child, target, dxConsumed, dyConsumed, dxUnconsumed, dyUnconsumed, type, consumed)
+        }
+    }
+
+}
