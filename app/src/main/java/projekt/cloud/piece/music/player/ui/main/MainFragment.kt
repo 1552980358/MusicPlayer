c255146fc6f513e0aedcdda1b1e3f97d858d2e0d
1552980358 <1552980358@qq.com>
2022-08-09 13:17:41 +0800

    ui/main/MainFragment: Implement moving to PlayFragment

--- app/src/main/java/projekt/cloud/piece/music/player/ui/main/MainFragment.kt
+++ app/src/main/java/projekt/cloud/piece/music/player/ui/main/MainFragment.kt
@@ -12,11 +12,14 @@ import androidx.fragment.app.FragmentContainerView
 import androidx.fragment.app.activityViewModels
 import androidx.fragment.app.viewModels
 import androidx.navigation.NavController
+import androidx.navigation.fragment.FragmentNavigatorExtras
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.appbar.MaterialToolbar
 import com.google.android.material.navigation.NavigationView
+import com.google.android.material.transition.platform.Hold
 import projekt.cloud.piece.music.player.MainActivityViewModel
 import projekt.cloud.piece.music.player.R
 import projekt.cloud.piece.music.player.base.BaseFragment
@@ -46,10 +49,17 @@ class MainFragment: BaseFragment() {
     private val relativeLayoutBottomPlayBarContainer: RelativeLayout
         get() = binding.relativeLayoutBottomPlayBarContainer
 
+    private lateinit var childNavController: NavController
     private lateinit var navController: NavController
     
     private val viewModel: MainViewModel by viewModels()
     private val activityViewModel: MainActivityViewModel by activityViewModels()
+    
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        navController = findNavController()
+        exitTransition = Hold()
+    }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         _binding = FragmentMainBinding.inflate(inflater, container, false)
@@ -59,17 +69,22 @@ class MainFragment: BaseFragment() {
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        navController = fragmentContainerView.getFragment<NavHostFragment>().navController
+        childNavController = fragmentContainerView.getFragment<NavHostFragment>().navController
         with(requireActivity() as AppCompatActivity) {
             setSupportActionBar(materialToolbar)
         }
         materialToolbar.setupWithNavController(
-            navController,
+            childNavController,
             AppBarConfiguration(setOf(R.id.home_fragment, R.id.artist_fragment), drawerLayout)
         )
-        navigationView.setupWithNavController(navController)
+        navigationView.setupWithNavController(childNavController)
     
-        relativeLayoutBottomPlayBarContainer.setOnClickListener {  }
+        relativeLayoutBottomPlayBarContainer.setOnClickListener {
+            navController.navigate(
+                MainFragmentDirections.actionMainFragmentToPlayFragment(),
+                FragmentNavigatorExtras(relativeLayoutBottomPlayBar to relativeLayoutBottomPlayBar.transitionName)
+            )
+        }
         
         when {
             isInitialized(requireContext()) -> viewModel.setInitialized(true)
