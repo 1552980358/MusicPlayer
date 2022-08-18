c618119d09e0edf63df4b3d0514548de7ca08931
1552980358 <1552980358@qq.com>
2022-08-18 18:24:39 +0800

    BindingAdapter: Update method View.startAnimatedAlpha

--- app/src/main/java/projekt/cloud/piece/music/player/util/BindingAdapter.kt
+++ app/src/main/java/projekt/cloud/piece/music/player/util/BindingAdapter.kt
@@ -1,8 +1,10 @@
 package projekt.cloud.piece.music.player.util
 
 import android.graphics.Bitmap
+import android.view.View
 import androidx.appcompat.widget.AppCompatImageView
 import androidx.databinding.BindingAdapter
+import projekt.cloud.piece.music.player.util.Values.ANIMATION_DURATION_LONG
 
 object BindingAdapter {
     
@@ -13,5 +15,12 @@ object BindingAdapter {
             setImageBitmap(bitmap)
         }
     }
+    
+    @JvmStatic
+    @BindingAdapter("animatedAlpha")
+    fun View.startAnimatedAlpha(alpha: Float) = animate()
+        .alpha(alpha)
+        .setDuration(ANIMATION_DURATION_LONG)
+        .start()
     
 }
