805435c358ef11879928ab3436317d8c939fd70f
1552980358 <1552980358@qq.com>
2022-08-18 13:41:51 +0800

    widget/ProgressSeekbar: Refactor and add backing animation

--- app/src/main/java/projekt/cloud/piece/music/player/widget/ProgressSeekbar.kt
+++ app/src/main/java/projekt/cloud/piece/music/player/widget/ProgressSeekbar.kt
@@ -4,9 +4,12 @@ import android.animation.ValueAnimator
 import android.content.Context
 import android.graphics.Canvas
 import android.graphics.Color.BLACK
+import android.graphics.Color.DKGRAY
+import android.graphics.Color.LTGRAY
 import android.graphics.Color.WHITE
 import android.graphics.Paint
 import android.graphics.Path
+import android.graphics.PointF
 import android.util.AttributeSet
 import android.view.KeyEvent.ACTION_DOWN
 import android.view.MotionEvent
@@ -15,7 +18,7 @@ import android.view.MotionEvent.ACTION_UP
 import android.view.View
 import android.view.View.MeasureSpec.EXACTLY
 import androidx.core.animation.doOnEnd
-import androidx.core.content.ContextCompat
+import androidx.core.animation.doOnStart
 import androidx.databinding.BindingAdapter
 import kotlin.math.min
 import projekt.cloud.piece.music.player.R
@@ -42,57 +45,68 @@ class ProgressSeekbar(context: Context, attributeSet: AttributeSet? = null): Vie
         
         private const val CORNERS_SIZE = 8
         
-        private const val PAINT_START_POINT_X_Y = 0F
+        private val PAINT_START_POINT = PointF(0F, 0F)
         
         private const val ANIMATOR_DURATION_GRADUALLY = 200L
         private const val ANIMATOR_DURATION_RAPIDLY = 100L
     }
     
-    private val corners = FloatArray(CORNERS_SIZE)
-    
-    private val durationPaint = Paint().apply {
-        isAntiAlias = true
-        style = Paint.Style.FILL
-    }
-    private val durationPath = Path()
-    
-    private val progressPaint = Paint().apply {
-        isAntiAlias = true
-        style = Paint.Style.FILL
-    }
-    private val progressPath = Path()
-    
-    private val circlePaint = Paint().apply {
-        isAntiAlias = true
-        style = Paint.Style.STROKE
-    }
-    
+    private var duration = 1L
+        set(value) {
+            field = value
+            updateProgress()
+        }
     private var progress = 0L
         set(value) {
             field = value
-            graduallyMoveProgress(currentProgressPos, getWidthPosition(width.toFloat(), height.toFloat()))
+            updateProgressAnimated()
         }
-    
     private var touchProgress = 0L
         set(value) {
             field = value
             listener?.invoke(value, isReleased)
         }
     
-    private var duration = 1L
-        set(value) {
-            field = value
-            graduallyMoveProgress(currentProgressPos, getWidthPosition(width.toFloat(), height.toFloat()))
-        }
+    private val corners = FloatArray(CORNERS_SIZE)
+    
+    /** Draw Duration bar **/
+    private val durationPaint = Paint().apply {
+        isAntiAlias = true
+        style = Paint.Style.FILL
+    }
+    private val durationPath = Path()
     
     private var circleRadius = 0F
+    /** Draw Circle **/
+    private val circlePaint = Paint().apply {
+        isAntiAlias = true
+        style = Paint.Style.STROKE
+    }
     
-    private var animator: ValueAnimator? = null
-    private var currentProgressPos = 0F
-    private var touchedProgressPos = 0F
+    /** Primary paint: user seeking / non-seeking progress **/
+    private val primaryPaint = Paint().apply {
+        isAntiAlias = true
+        style = Paint.Style.FILL
+    }
+    /** Secondary paint: seeking progress **/
+    private val secondaryPaint = Paint().apply {
+        isAntiAlias = true
+        style = Paint.Style.FILL
+    }
     
+    private val primaryPath = Path()
+    private val secondaryPath = Path()
+    
+    private var primaryPos = 0F
+    private var secondaryPos = 0F
+    
+    /** Flags **/
     private var isTouchMode = false
-    private var isReleased = false
+    private var isReleased = true
+    private var isSecondaryResetting = false
+    
+    private var primaryAnimator: ValueAnimator? = null
+    private var secondaryAnimator: ValueAnimator? = null
     
     private var listener: ((Long, Boolean) -> Unit)? = null
     
@@ -108,13 +122,10 @@ class ProgressSeekbar(context: Context, attributeSet: AttributeSet? = null): Vie
                 R.styleable.ProgressSeekbar_circleWidth,
                 resources.getDimension(R.dimen.progress_seekbar_circle_width)
             )
-    
-            progressPaint.color = it.getColor(R.styleable.ProgressSeekbar_progressColor, BLACK)
-    
-            durationPaint.color = it.getColor(
-                R.styleable.ProgressSeekbar_durationColor,
-                ContextCompat.getColor(context, R.color.progress_bar_duration_color)
-            )
+            
+            primaryPaint.color = it.getColor(R.styleable.ProgressSeekbar_primaryColor, BLACK)
+            secondaryPaint.color = it.getColor(R.styleable.ProgressSeekbar_secondaryColor, DKGRAY)
+            durationPaint.color = it.getColor(R.styleable.ProgressSeekbar_durationColor, LTGRAY)
         }
         
         setOnTouchListener(this)
@@ -134,62 +145,98 @@ class ProgressSeekbar(context: Context, attributeSet: AttributeSet? = null): Vie
         super.onMeasure(widthMeasureSpec, MeasureSpec.makeMeasureSpec(height, EXACTLY))
         
         durationPath.reset()
-        durationPath.addRoundRect(PAINT_START_POINT_X_Y, PAINT_START_POINT_X_Y, measuredWidth.toFloat(), measuredHeight.toFloat(), corners, Path.Direction.CW)
+        durationPath.addRoundRect(PAINT_START_POINT.x, PAINT_START_POINT.y, measuredWidth.toFloat(), measuredHeight.toFloat(), corners, Path.Direction.CW)
     }
     
     override fun onDraw(canvas: Canvas?) {
         canvas ?: return
         
+        // Draw duration
+        canvas.drawPath(durationPath, durationPaint)
+    
         val height = height.toFloat()
         val halfHeight = height / 2
         
-        // Draw duration
-        canvas.drawPath(durationPath, durationPaint)
-        
-        // Check pos
-        val progressPos = when {
-            isTouchMode -> touchedProgressPos
-            currentProgressPos == 0F && progress > 0 && animator == null -> getWidthPosition(width.toFloat(), height)
-            else -> currentProgressPos
+        if (isTouchMode || isSecondaryResetting) {
+            secondaryPath.reset()
+            secondaryPath.addRoundRect(PAINT_START_POINT.x, PAINT_START_POINT.y, secondaryPos + height, height, corners, Path.Direction.CW)
+            canvas.drawPath(secondaryPath, secondaryPaint)
         }
     
-        // Draw path
-        progressPath.reset()
-        progressPath.addRoundRect(PAINT_START_POINT_X_Y, PAINT_START_POINT_X_Y, progressPos + height, height, corners, Path.Direction.CW)
-        canvas.drawPath(progressPath, progressPaint)
+        primaryPath.reset()
+        primaryPath.addRoundRect(PAINT_START_POINT.x, PAINT_START_POINT.y, primaryPos + height, height, corners, Path.Direction.CW)
+        canvas.drawPath(primaryPath, primaryPaint)
         
-        canvas.drawCircle(progressPos + halfHeight, halfHeight, circleRadius, circlePaint)
+        canvas.drawCircle(primaryPos + halfHeight, halfHeight, circleRadius, circlePaint)
     }
     
     override fun onTouch(v: View, event: MotionEvent): Boolean {
         val height = height.toFloat()
         val halfHeight = height / 2F
+    
+        val touchPos = event.getTouchedProgressPos(height, halfHeight)
+        
         when (event.action) {
             ACTION_DOWN -> {
                 isTouchMode = true
                 isReleased = false
-                val target = event.getTouchedProgressPos(height, halfHeight)
-                rapidlyMoveProgress(currentProgressPos, target)
-                touchProgress = (target / (width - height) * duration).toLong()
+                // Update secondary
+                secondaryPos = primaryPos
+                
+                // Update primary
+                movePrimaryPaint(primaryPos, touchPos, ANIMATOR_DURATION_RAPIDLY)
+                // Update touched progress
+                touchProgress = (touchPos * duration / (width - height)).toLong()
             }
             ACTION_MOVE -> {
-                touchedProgressPos = event.getTouchedProgressPos(height, halfHeight)
+                primaryPos = touchPos
                 invalidate()
-                touchProgress = (touchedProgressPos / (width - height) * duration).toLong()
             }
             ACTION_UP -> {
                 isReleased = true
-                touchedProgressPos = event.getTouchedProgressPos(height, halfHeight)
-                currentProgressPos = touchedProgressPos
-                invalidate()
-                touchProgress = (touchedProgressPos / (width - height) * duration).toLong()
+                // Update touched progress
+                touchProgress = (touchPos * duration / (width - height)).toLong()
+                moveSecondaryPaint(
+                    secondaryPos,
+                    primaryPos,
+                    ANIMATOR_DURATION_RAPIDLY ,
+                    onStart = { isSecondaryResetting = true },
+                    onEnd = { isSecondaryResetting = false }
+                )
                 isTouchMode = false
             }
         }
         return true
     }
     
-    private fun getWidthPosition(width: Float, height: Float) =
+    fun setProgressControlledListener(listener: (Long, Boolean) -> Unit) {
+        this.listener = listener
+    }
+    
+    private fun updateProgress() {
+        val width = width.toFloat()
+        val height = height.toFloat()
+        if (isTouchMode) {
+            // Just move secondary
+            secondaryPos = getProgressX(progress, duration, width, height)
+            return
+        }
+        // Move primary
+        primaryPos = getProgressX(progress, duration, width, height)
+    }
+    
+    private fun updateProgressAnimated() {
+        val width = width.toFloat()
+        val height = height.toFloat()
+        if (isTouchMode) {
+            // Just move secondary
+            return moveSecondaryPaint(secondaryPos, getProgressX(progress, duration, width, height), ANIMATOR_DURATION_GRADUALLY)
+        }
+        // Move primary
+        movePrimaryPaint(primaryPos, getProgressX(progress, duration, width, height), ANIMATOR_DURATION_GRADUALLY)
+    }
+    
+    private fun getProgressX(progress: Long, duration: Long, width: Float, height: Float) =
         (width - height) * progress / duration
     
     private fun MotionEvent.getTouchedProgressPos(height: Float, halfHeight: Float) = when {
@@ -198,35 +245,41 @@ class ProgressSeekbar(context: Context, attributeSet: AttributeSet? = null): Vie
         else -> x - halfHeight
     }
     
-    private fun graduallyMoveProgress(from: Float, to: Float) =
-        moveProgress(from, to, ANIMATOR_DURATION_GRADUALLY) {
-            currentProgressPos = it.animatedValue as Float
-            invalidate()
-        }
-    
-    private fun rapidlyMoveProgress(from: Float, to: Float) =
-        moveProgress(from, to, ANIMATOR_DURATION_RAPIDLY) {
-            touchedProgressPos = it.animatedValue as Float
-            invalidate()
-        }
-    
-    private fun moveProgress(from: Float, to: Float, duration: Long, listener: ValueAnimator.AnimatorUpdateListener) {
-        animator?.end()
+    private fun moveSecondaryPaint(from: Float, to: Float, duration: Long, onStart: () -> Unit = {}, onEnd: () -> Unit = {}) {
+        secondaryAnimator?.end()
         if (from == to) {
-            animator = null
+            secondaryPos = to
             return invalidate()
         }
-        animator = ValueAnimator.ofFloat(from, to)
+        secondaryAnimator = ValueAnimator.ofFloat(from, to)
             .setDuration(duration)
             .apply {
-                addUpdateListener(listener)
-                doOnEnd { animator = null }
+                addUpdateListener {
+                    secondaryPos = it.animatedValue as Float
+                    invalidate()
+                }
+                doOnStart { onStart.invoke() }
+                doOnEnd { onEnd.invoke() }
+                start()
             }
-        animator?.start()
     }
     
-    fun setProgressControlledListener(listener: (Long, Boolean) -> Unit) {
-        this.listener = listener
+    private fun movePrimaryPaint(from: Float, to: Float, duration: Long, onEnd: () -> Unit = {}) {
+        primaryAnimator?.end()
+        if (from == to) {
+            primaryPos = to
+            return invalidate()
+        }
+        primaryAnimator = ValueAnimator.ofFloat(from, to)
+            .setDuration(duration)
+            .apply {
+                addUpdateListener {
+                    primaryPos = it.animatedValue as Float
+                    invalidate()
+                }
+                doOnEnd { onEnd.invoke() }
+                start()
+            }
     }
     
 }
