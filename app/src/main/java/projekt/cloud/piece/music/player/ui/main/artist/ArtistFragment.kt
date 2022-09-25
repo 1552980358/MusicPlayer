93e8e8bcb0dd9a20106a6404605543544f95ba24
1552980358 <1552980358@qq.com>
2022-09-25 21:12:01 +0800

    main/ArtistFragment: Implement change of BaseMainFragment

--- app/src/main/java/projekt/cloud/piece/music/player/ui/main/artist/ArtistFragment.kt
+++ app/src/main/java/projekt/cloud/piece/music/player/ui/main/artist/ArtistFragment.kt
@@ -1,12 +1,7 @@
 package projekt.cloud.piece.music.player.ui.main.artist
 
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
-import androidx.coordinatorlayout.widget.CoordinatorLayout
-import androidx.recyclerview.widget.RecyclerView
-import projekt.cloud.piece.music.player.databinding.FragmentArtistBinding
 import projekt.cloud.piece.music.player.item.Artist
 import projekt.cloud.piece.music.player.room.AudioDatabase.Companion.audioDatabase
 import projekt.cloud.piece.music.player.ui.main.base.BaseMainFragment
@@ -15,23 +10,10 @@ import projekt.cloud.piece.music.player.util.CoroutineUtil.ui
 
 class ArtistFragment: BaseMainFragment() {
     
-    private var _binding: FragmentArtistBinding? = null
-    private val binding: FragmentArtistBinding
-        get() = _binding!!
-    
-    private val root: CoordinatorLayout
-        get() = binding.root
-    private val recyclerView: RecyclerView
-        get() = binding.recyclerView
-    
     private lateinit var recyclerViewAdapter: RecyclerViewAdapter
     
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
-        _binding = FragmentArtistBinding.inflate(inflater, container, false)
-        return root
-    }
-    
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
         recyclerViewAdapter = RecyclerViewAdapter(recyclerView) {}
         io {
             val artistList = audioDatabase.artistDao.query()
