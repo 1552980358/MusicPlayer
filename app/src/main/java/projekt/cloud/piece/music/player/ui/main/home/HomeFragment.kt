c4bacdfc6cfdc8f375aeb3529eb113484d29ba97
1552980358 <1552980358@qq.com>
2022-09-25 21:16:14 +0800

    main/HomeFragment: Implement change of BaseMainFragment

--- app/src/main/java/projekt/cloud/piece/music/player/ui/main/home/HomeFragment.kt
+++ app/src/main/java/projekt/cloud/piece/music/player/ui/main/home/HomeFragment.kt
@@ -1,11 +1,7 @@
 package projekt.cloud.piece.music.player.ui.main.home
 
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
-import androidx.recyclerview.widget.RecyclerView
-import projekt.cloud.piece.music.player.databinding.FragmentHomeBinding
 import projekt.cloud.piece.music.player.room.AudioDatabase.Companion.audioDatabase
 import projekt.cloud.piece.music.player.ui.main.base.BaseMainFragment
 import projekt.cloud.piece.music.player.util.CoroutineUtil.io
@@ -13,20 +9,7 @@ import projekt.cloud.piece.music.player.util.CoroutineUtil.ui
 
 class HomeFragment: BaseMainFragment() {
     
-    private var _binding: FragmentHomeBinding? = null
-    private val binding: FragmentHomeBinding
-        get() = _binding!!
-    
-    private val root get() = binding.root
-    private val recyclerView: RecyclerView
-        get() = binding.recyclerView
-    
     private lateinit var recyclerViewAdapter: RecyclerViewAdapter
-    
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
-        _binding = FragmentHomeBinding.inflate(inflater, container, false)
-        return root
-    }
     
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
