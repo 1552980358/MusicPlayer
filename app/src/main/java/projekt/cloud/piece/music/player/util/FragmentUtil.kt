dbee9c6873d3acab66d00cea2757f328fe1d59ca
1552980358 <1552980358@qq.com>
2023-02-27 04:18:09 +0800

    [util] Update FragmentUtil

---
+++ app/src/main/java/projekt/cloud/piece/music/player/util/FragmentUtil.kt
@@ -0,0 +1,20 @@
+package projekt.cloud.piece.music.player.util
+
+import androidx.fragment.app.Fragment
+
+object FragmentUtil {
+
+    fun <T> Fragment.viewLifecycleProperty(): LifecycleProperty<T> =
+        ViewLifecycleProperty(this)
+
+    class ViewLifecycleProperty<T>(fragment: Fragment): LifecycleProperty<T>() {
+
+        init {
+            fragment.viewLifecycleOwnerLiveData.observe(fragment) { viewLifecycleOwner ->
+                viewLifecycleOwner.lifecycle.addObserver(this)
+            }
+        }
+
+    }
+
+}
