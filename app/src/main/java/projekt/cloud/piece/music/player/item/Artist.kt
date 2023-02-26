dcf854725eb44a21d042e6f44e0f79a63d895a18
1552980358 <1552980358@qq.com>
2023-02-27 00:55:46 +0800

    Reset Project
    Signed-off-by: 1552980358 <1552980358@qq.com>

--- app/src/main/java/projekt/cloud/piece/music/player/item/Artist.kt
+++
@@ -1,8 +0,0 @@
-package projekt.cloud.piece.music.player.item
-
-import androidx.room.Entity
-import java.io.Serializable
-import projekt.cloud.piece.music.player.item.base.BaseTitledItem
-
-@Entity
-class Artist(id: String, title: String): BaseTitledItem(id, title), Serializable
--- app/src/main/java/projekt/cloud/piece/music/player/util/Values.kt
+++
@@ -1,8 +0,0 @@
-package projekt.cloud.piece.music.player.util
-
-object Values {
-    
-    const val ANIMATION_DURATION_LONG = 400L
-    const val ANIMATION_DURATION_HALF_LONG = 200L
-    
-}
