d2559f21d1ae2a2112b8bf26c9fb7353b9334a24
1552980358 <1552980358@qq.com>
2023-02-27 08:49:59 +0800

    [base] Update BaseMultiDensityFragment

---
+++ app/src/main/java/projekt/cloud/piece/music/player/base/BaseMultiDensityFragment.kt
@@ -0,0 +1,31 @@
+package projekt.cloud.piece.music.player.base
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.LifecycleOwner
+import androidx.viewbinding.ViewBinding
+import projekt.cloud.piece.music.player.util.FragmentUtil.ViewLifecycleProperty
+import projekt.cloud.piece.music.player.util.LifecycleProperty
+
+abstract class BaseMultiDensityFragment<VB: ViewBinding, LC: BaseLayoutCompat<VB>>: BaseFragment<VB>() {
+
+    private var layoutCompat: LC by layoutCompatProperty()
+
+    private fun layoutCompatProperty(): LifecycleProperty<LC> =
+        LayoutCompatProperty(this)
+
+    private class LayoutCompatProperty<VB: ViewBinding, LC: BaseLayoutCompat<VB>>(fragment: Fragment): ViewLifecycleProperty<LC>(fragment) {
+        override fun onDestroy(owner: LifecycleOwner) {
+            field.onDestroy()
+            super.onDestroy(owner)
+        }
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        layoutCompat = onCreateLayoutCompat(binding)
+    }
+
+    protected abstract fun onCreateLayoutCompat(binding: VB): LC
+
+}
