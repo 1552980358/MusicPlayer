6ae0689033f70a8ee980b1d53fabba0043704063
1552980358 <1552980358@qq.com>
2023-03-21 18:38:27 +0800

    [util/TimeUtil] Update `Long.leadingZero`

--- app/src/main/java/projekt/cloud/piece/music/player/util/TimeUtil.kt
+++ app/src/main/java/projekt/cloud/piece/music/player/util/TimeUtil.kt
@@ -11,7 +11,13 @@ object TimeUtil {
     private val Long.millsToSecond: Long
         get() = this / SECOND % MINUTE
 
+    private val Long.leadingZero: String
+        get() = when {
+            this < 10 -> "0$this"
+            else -> "$this"
+        }
+
     val Long.durationStr: String
-        get() = "${millsToMinute}:${millsToSecond}"
+        get() = "${millsToMinute}:${millsToSecond.leadingZero}"
 
 }
