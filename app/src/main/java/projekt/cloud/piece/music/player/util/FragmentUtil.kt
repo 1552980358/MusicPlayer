b0366a41a07160587402b87e0e84fc50e94a2806
1552980358 <1552980358@qq.com>
2023-04-04 10:18:18 +0800

    [util/FragmentUtil] Add doOnDestroy

--- app/src/main/java/projekt/cloud/piece/music/player/util/FragmentUtil.kt
+++ app/src/main/java/projekt/cloud/piece/music/player/util/FragmentUtil.kt
@@ -1,18 +1,37 @@
 package projekt.cloud.piece.music.player.util
 
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.Observer
 
 object FragmentUtil {
 
-    fun <T> Fragment.viewLifecycleProperty(): LifecycleProperty<Fragment, T> =
-        ViewLifecycleProperty(this)
+    @JvmOverloads
+    fun <T> Fragment.viewLifecycleProperty(doOnDestroy: ((T) -> Unit)? = null): LifecycleProperty<Fragment, T> =
+        ViewLifecycleProperty(this, doOnDestroy)
 
-    open class ViewLifecycleProperty<T>(fragment: Fragment): LifecycleProperty<Fragment, T>() {
+    open class ViewLifecycleProperty<T>(
+        fragment: Fragment, private val doOnDestroy: ((T) -> Unit)?
+    ): LifecycleProperty<Fragment, T>() {
 
         init {
-            fragment.viewLifecycleOwnerLiveData.observe(fragment) { viewLifecycleOwner ->
-                viewLifecycleOwner.lifecycle.addObserver(this)
-            }
+            fragment.viewLifecycleOwnerLiveData.observe(
+                fragment,
+                object: Observer<LifecycleOwner> {
+                    override fun onChanged(viewLifecycleOwner: LifecycleOwner?) {
+                        viewLifecycleOwner?.lifecycle?.let { viewLifecycle ->
+                            viewLifecycle.addObserver(this@ViewLifecycleProperty)
+                            fragment.viewLifecycleOwnerLiveData
+                                .removeObserver(this)
+                        }
+                    }
+                }
+            )
+        }
+
+        override fun onDestroy(owner: LifecycleOwner) {
+            doOnDestroy?.invoke(field)
+            super.onDestroy(owner)
         }
 
     }
