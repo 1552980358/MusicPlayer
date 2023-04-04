74b412b28d301a3209caad9e3153f2ae6d76a5a7
1552980358 <1552980358@qq.com>
2023-04-04 10:18:51 +0800

    [base/BaseMultiDensityFragment] Remove LayoutCompatProperty

--- app/src/main/java/projekt/cloud/piece/music/player/base/BaseMultiDensityFragment.kt
+++ app/src/main/java/projekt/cloud/piece/music/player/base/BaseMultiDensityFragment.kt
@@ -3,12 +3,9 @@ package projekt.cloud.piece.music.player.base
 import android.graphics.Rect
 import android.os.Bundle
 import android.view.View
-import androidx.fragment.app.Fragment
-import androidx.lifecycle.LifecycleOwner
 import androidx.viewbinding.ViewBinding
 import projekt.cloud.piece.music.player.util.ContextUtil.requireWindowInsets
-import projekt.cloud.piece.music.player.util.FragmentUtil.ViewLifecycleProperty
-import projekt.cloud.piece.music.player.util.LifecycleProperty
+import projekt.cloud.piece.music.player.util.FragmentUtil.viewLifecycleProperty
 
 private class NullRequireWindowInsetsListenerException: IllegalArgumentException(MESSAGE) {
     companion object {
@@ -18,18 +15,8 @@ private class NullRequireWindowInsetsListenerException: IllegalArgumentException
 
 abstract class BaseMultiDensityFragment<VB: ViewBinding, LC: BaseLayoutCompat<VB>>: BaseFragment<VB>() {
 
-    protected var layoutCompat: LC by layoutCompatProperty()
+    protected var layoutCompat: LC by viewLifecycleProperty()
         private set
-
-    private fun layoutCompatProperty(): LifecycleProperty<Fragment, LC> =
-        LayoutCompatProperty(this)
-
-    private class LayoutCompatProperty<VB: ViewBinding, LC: BaseLayoutCompat<VB>>(fragment: Fragment): ViewLifecycleProperty<LC>(fragment) {
-        override fun onDestroy(owner: LifecycleOwner) {
-            field.onDestroy()
-            super.onDestroy(owner)
-        }
-    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         layoutCompat = onCreateLayoutCompat(binding)
