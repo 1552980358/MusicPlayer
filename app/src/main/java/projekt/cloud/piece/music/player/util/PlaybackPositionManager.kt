a4b1912b945e46fc6c353d11c4ba66ca02df89e8
1552980358 <1552980358@qq.com>
2023-04-05 13:00:11 +0800

    [util] Update PlaybackPositionManager managing playback position

---
+++ app/src/main/java/projekt/cloud/piece/music/player/util/PlaybackPositionManager.kt
@@ -0,0 +1,61 @@
+package projekt.cloud.piece.music.player.util
+
+import java.io.Closeable
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.delay
+import projekt.cloud.piece.music.player.util.CoroutineUtil.default
+import projekt.cloud.piece.music.player.util.CoroutineUtil.main
+
+class PlaybackPositionManager @JvmOverloads constructor(
+    coroutineScope: CoroutineScope,
+    position: Long = DEFAULT_POSITION,
+    doOnUpdate: (Long) -> Unit
+): Closeable {
+
+    private companion object {
+        const val DEFAULT_POSITION = 0L
+        const val DELAY_SEPARATION = 200L
+        const val ROUNDING_SEPARATION = DELAY_SEPARATION
+    }
+
+    var position = position
+        @Synchronized
+        private set
+
+    @Volatile
+    private var isContinue = true
+
+    private var job: Job? = null
+
+    init {
+        startPlaybackLoop(coroutineScope, position, doOnUpdate)
+    }
+
+    private fun startPlaybackLoop(coroutineScope: CoroutineScope, initPosition: Long, doOnUpdate: (Long) -> Unit) {
+        job = coroutineScope.default {
+            main { doOnUpdate.invoke(initPosition) }
+            roundUpPosition(initPosition % ROUNDING_SEPARATION)
+
+            while (isContinue) {
+                main { doOnUpdate.invoke(position) }
+                delay(DELAY_SEPARATION)
+                position += DELAY_SEPARATION
+            }
+        }
+    }
+
+    private suspend fun roundUpPosition(separation: Long) {
+        if (separation != 0L) {
+            position += separation
+            delay(ROUNDING_SEPARATION - separation)
+        }
+    }
+
+    override fun close() {
+        isContinue = false
+        job?.cancel()
+        job = null
+    }
+
+}
