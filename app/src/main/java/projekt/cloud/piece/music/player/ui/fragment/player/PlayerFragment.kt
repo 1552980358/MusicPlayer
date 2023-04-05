bc9c3c88b17e150b9fadd238e336e27989c63e3d
1552980358 <1552980358@qq.com>
2023-04-05 13:03:36 +0800

    [fragment] Update PlayerFragment

---
+++ app/src/main/java/projekt/cloud/piece/music/player/ui/fragment/player/PlayerFragment.kt
@@ -0,0 +1,160 @@
+package projekt.cloud.piece.music.player.ui.fragment.player
+
+import android.graphics.Color.TRANSPARENT
+import android.net.Uri
+import android.os.Bundle
+import android.support.v4.media.MediaMetadataCompat
+import android.support.v4.media.MediaMetadataCompat.METADATA_KEY_ALBUM
+import android.support.v4.media.MediaMetadataCompat.METADATA_KEY_ARTIST
+import android.support.v4.media.MediaMetadataCompat.METADATA_KEY_ART_URI
+import android.support.v4.media.MediaMetadataCompat.METADATA_KEY_DURATION
+import android.support.v4.media.MediaMetadataCompat.METADATA_KEY_TITLE
+import android.support.v4.media.session.MediaControllerCompat
+import android.support.v4.media.session.PlaybackStateCompat
+import android.support.v4.media.session.PlaybackStateCompat.STATE_NONE
+import android.view.View
+import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.lifecycleScope
+import androidx.navigation.fragment.navArgs
+import com.google.android.material.transition.platform.MaterialContainerTransform
+import com.google.android.material.transition.platform.MaterialSharedAxis
+import projekt.cloud.piece.music.player.R
+import projekt.cloud.piece.music.player.base.BaseLayoutCompat.BaseLayoutCompatUtil.layoutCompat
+import projekt.cloud.piece.music.player.base.BaseMultiDensityFragment
+import projekt.cloud.piece.music.player.databinding.FragmentPlayerBinding
+import projekt.cloud.piece.music.player.ui.activity.main.MainViewModel
+import projekt.cloud.piece.music.player.util.FragmentUtil.viewLifecycleProperty
+import projekt.cloud.piece.music.player.util.PlaybackPositionManager
+import projekt.cloud.piece.music.player.util.PlaybackStateManager
+import projekt.cloud.piece.music.player.util.ScreenDensity.ScreenDensityUtil.screenDensity
+
+class PlayerFragment: BaseMultiDensityFragment<FragmentPlayerBinding, PlayerLayoutCompat>() {
+
+    override val viewBindingClass: Class<FragmentPlayerBinding>
+        get() = FragmentPlayerBinding::class.java
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        resources.getInteger(R.integer.anim_duration_400).toLong().let { transitionDuration ->
+            setupTransition(transitionDuration, transitionDuration * 2)
+        }
+    }
+
+    private fun setupTransition(transitionDurationShort: Long, transitionDurationLong: Long) {
+        sharedElementEnterTransition = MaterialContainerTransform().apply {
+            duration = transitionDurationShort       /** 400 **/
+            scrimColor = TRANSPARENT
+        }
+        enterTransition = MaterialSharedAxis(MaterialSharedAxis.Y, true).apply {
+            duration = transitionDurationLong       /** 800 **/
+            addTarget(R.id.drawer_layout_root)
+        }
+        exitTransition = MaterialSharedAxis(MaterialSharedAxis.Y, false).apply {
+            duration = transitionDurationLong       /** 800 **/
+            addTarget(R.id.drawer_layout_root)
+        }
+    }
+
+    override fun onCreateLayoutCompat(binding: FragmentPlayerBinding): PlayerLayoutCompat {
+        return binding.layoutCompat(requireContext().screenDensity)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        layoutCompat.setupSlider()
+
+        updateMetadataFromArgs()
+
+        val mainViewModel: MainViewModel by activityViewModels()
+        mainViewModel.isMediaBrowserCompatConnected.observe(viewLifecycleOwner) { isConnected ->
+            if (isConnected) {
+                MediaControllerCompat.getMediaController(requireActivity())
+                    ?.let { mediaControllerCompat -> setupMediaController(mediaControllerCompat) }
+            }
+        }
+    }
+
+    private var callback: MediaControllerCompat.Callback by viewLifecycleProperty { callback ->
+        MediaControllerCompat.getMediaController(requireActivity())
+            ?.unregisterCallback(callback)
+    }
+
+    private fun setupMediaController(mediaControllerCompat: MediaControllerCompat) {
+        val playbackStateManager = PlaybackStateManager()
+
+        var playbackPositionManager: PlaybackPositionManager? = null
+        val doOnPositionUpdate = { position: Long ->
+            layoutCompat.updatePlaybackPosition(position)
+        }
+        callback = object: MediaControllerCompat.Callback() {
+            override fun onPlaybackStateChanged(playbackStateCompat: PlaybackStateCompat) {
+                playbackPositionManager = setupPlaybackPositionManager(
+                    playbackPositionManager, playbackStateCompat.position, doOnPositionUpdate
+                )
+                layoutCompat.notifyUpdatePlaybackController(
+                    this@PlayerFragment,
+                    playbackStateManager.updatePlaybackState(playbackStateCompat.state)
+                )
+            }
+            override fun onMetadataChanged(mediaMetadataCompat: MediaMetadataCompat) {
+                updateMetadataFromMediaMetadataCompat(mediaMetadataCompat)
+            }
+        }
+        mediaControllerCompat.registerCallback(callback)
+        // Recover state
+        mediaControllerCompat.playbackState?.let { playbackStateCompat ->
+            if (playbackStateCompat.state != STATE_NONE) {
+                playbackPositionManager = setupPlaybackPositionManager(
+                    playbackPositionManager, playbackStateCompat.position, doOnPositionUpdate
+                )
+                layoutCompat.notifyUpdatePlaybackController(
+                    this,
+                    playbackStateManager.updatePlaybackState(playbackStateCompat.state)
+                )
+            }
+        }
+
+        layoutCompat.setupPlaybackControls(
+            playbackStateManager, mediaControllerCompat.transportControls
+        )
+    }
+
+    private fun setupPlaybackPositionManager(
+        playbackPositionManager: PlaybackPositionManager?, playbackPosition: Long, doOnPositionUpdate: (Long) -> Unit
+    ): PlaybackPositionManager {
+        playbackPositionManager?.close()
+        return PlaybackPositionManager(lifecycleScope, playbackPosition, doOnPositionUpdate)
+    }
+
+    private fun updateMetadataFromArgs() {
+        val playerFragmentArgs: PlayerFragmentArgs by navArgs()
+        updateMetadataFromArgs(playerFragmentArgs)
+    }
+
+    private fun updateMetadataFromArgs(playerFragmentArgs: PlayerFragmentArgs) {
+        layoutCompat.updateMetadata(
+            playerFragmentArgs.title,
+            playerFragmentArgs.artist,
+            playerFragmentArgs.album,
+            playerFragmentArgs.duration
+        )
+        layoutCompat.updateCoverImage(
+            this,
+            Uri.parse(playerFragmentArgs.coverUri)
+        )
+    }
+
+    private fun updateMetadataFromMediaMetadataCompat(mediaMetadataCompat: MediaMetadataCompat) {
+        layoutCompat.updateMetadata(
+            mediaMetadataCompat.getString(METADATA_KEY_TITLE),
+            mediaMetadataCompat.getString(METADATA_KEY_ARTIST),
+            mediaMetadataCompat.getString(METADATA_KEY_ALBUM),
+            mediaMetadataCompat.getLong(METADATA_KEY_DURATION)
+        )
+        layoutCompat.updateCoverImage(
+            this,
+            Uri.parse(mediaMetadataCompat.getString(METADATA_KEY_ART_URI))
+        )
+    }
+
+}
---
+++ app/src/main/java/projekt/cloud/piece/music/player/ui/fragment/player/PlayerLayoutCompat.kt
@@ -0,0 +1,128 @@
+package projekt.cloud.piece.music.player.ui.fragment.player
+
+import android.graphics.Rect
+import android.graphics.drawable.AnimatedVectorDrawable
+import android.net.Uri
+import android.support.v4.media.session.MediaControllerCompat.TransportControls
+import android.support.v4.media.session.PlaybackStateCompat.STATE_PAUSED
+import android.support.v4.media.session.PlaybackStateCompat.STATE_PLAYING
+import android.view.View.OnClickListener
+import androidx.annotation.DrawableRes
+import androidx.appcompat.widget.AppCompatImageButton
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.content.ContextCompat
+import androidx.core.view.updatePadding
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.lifecycleScope
+import com.bumptech.glide.Glide
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.imageview.ShapeableImageView
+import com.google.android.material.slider.Slider
+import kotlin.reflect.KClass
+import kotlinx.coroutines.withContext
+import projekt.cloud.piece.music.player.base.BaseLayoutCompat
+import projekt.cloud.piece.music.player.databinding.FragmentPlayerBinding
+import projekt.cloud.piece.music.player.util.CoroutineUtil.default
+import projekt.cloud.piece.music.player.util.CoroutineUtil.main
+import projekt.cloud.piece.music.player.util.PlaybackStateManager
+import projekt.cloud.piece.music.player.util.TimeUtil.durationStr
+import projekt.cloud.piece.music.player.util.TimeUtil.timeStr
+
+open class PlayerLayoutCompat: BaseLayoutCompat<FragmentPlayerBinding> {
+
+    constructor(): super(null)
+    constructor(binding: FragmentPlayerBinding): super(binding)
+
+    override val compatImpl: KClass<*>
+        get() = CompatImpl::class
+    override val w600dpImpl: KClass<*>
+        get() = W600dpImpl::class
+    override val w1240dpImpl: KClass<*>
+        get() = W1240dpImpl::class
+
+    private val constantRoot: ConstraintLayout
+        get() = binding.constraintLayoutRoot
+    private val position: Slider
+        get() = binding.sliderPosition
+    private val cover: ShapeableImageView
+        get() = binding.shapeableImageViewImageCover
+    private val playbackControl: FloatingActionButton
+        get() = binding.floatingActionButtonPlaybackControl
+    private val prev: AppCompatImageButton
+        get() = binding.appCompatImageButtonPrev
+    private val next: AppCompatImageButton
+        get() = binding.appCompatImageButtonNext
+
+    override val requireWindowInsets: Boolean
+        get() = true
+
+    override fun onSetupRequireWindowInsets() = { insets: Rect ->
+        constantRoot.updatePadding(top = insets.top, bottom = insets.bottom)
+    }
+
+    fun setupSlider() {
+        position.setLabelFormatter { value ->
+            value.toLong().durationStr
+        }
+    }
+
+    fun updateMetadata(title: String, artist: String, album: String, duration: Long) {
+        binding.title = title
+        binding.artist = artist
+        binding.album = album
+        binding.duration = duration.timeStr
+        position.valueTo = duration.toFloat()
+    }
+
+    fun updateCoverImage(fragment: Fragment, uri: Uri) {
+        Glide.with(fragment)
+            .load(uri)
+            .placeholder(cover.drawable)
+            // .transition(DrawableTransitionOptions.withCrossFade())
+            .into(cover)
+    }
+
+    fun updatePlaybackPosition(positionLong: Long) {
+        binding.position = positionLong.timeStr
+        position.value = positionLong.toFloat()
+    }
+
+    fun notifyUpdatePlaybackController(fragment: Fragment, @DrawableRes resId: Int) {
+        fragment.lifecycleScope.main {
+            val drawable = withContext(default) {
+                ContextCompat.getDrawable(fragment.requireContext(), resId)
+            }
+            if (drawable != null) {
+                playbackControl.setImageDrawable(drawable)
+                if (drawable is AnimatedVectorDrawable) {
+                    drawable.start()
+                }
+            }
+        }
+    }
+
+    fun setupPlaybackControls(
+        playbackStateManager: PlaybackStateManager, transportControls: TransportControls
+    ) {
+        val onClickListener = OnClickListener {
+            when (it) {
+                playbackControl -> {
+                    when (playbackStateManager.playbackState) {
+                        STATE_PLAYING -> { transportControls.pause() }
+                        STATE_PAUSED -> { transportControls.play() }
+                    }
+                }
+                prev -> { transportControls.skipToPrevious() }
+                next -> { transportControls.skipToNext() }
+            }
+        }
+        playbackControl.setOnClickListener(onClickListener)
+        prev.setOnClickListener(onClickListener)
+        next.setOnClickListener(onClickListener)
+    }
+
+    private class CompatImpl(binding: FragmentPlayerBinding): PlayerLayoutCompat(binding)
+    private class W600dpImpl(binding: FragmentPlayerBinding): PlayerLayoutCompat(binding)
+    private class W1240dpImpl(binding: FragmentPlayerBinding): PlayerLayoutCompat(binding)
+
+}
