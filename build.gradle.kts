787764a5dad5fcd79a615dcfa6113407023a063e
1552980358 <1552980358@qq.com>
2023-04-05 13:13:14 +0800

    [build.gradle.kts] Update Kotlin => 1.8.20

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@
 plugins {
     id("com.android.application") version "8.1.0-alpha11" apply false
     id("com.android.library") version "8.1.0-alpha11" apply false
-    id("org.jetbrains.kotlin.android") version "1.8.10" apply false
+    id("org.jetbrains.kotlin.android") version "1.8.20" apply false
     id("com.google.devtools.ksp") version "1.8.0-1.0.9" apply false
 }
 
