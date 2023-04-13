60fe41810e1895a9929d54eb4f7186a245f9c234
1552980358 <1552980358@qq.com>
2023-04-13 13:00:18 +0800

    [base/interface/WindowInsetsInterface] Update method `requireWindowInset()`
    Signed-off-by: 1552980358 <1552980358@qq.com>

--- app/src/main/java/projekt/cloud/piece/music/player/base/interfaces/WindowInsetsInterface.kt
+++ app/src/main/java/projekt/cloud/piece/music/player/base/interfaces/WindowInsetsInterface.kt
@@ -1,10 +1,16 @@
 package projekt.cloud.piece.music.player.base.interfaces
 
+import android.content.Context
 import android.graphics.Rect
+import projekt.cloud.piece.music.player.util.ContextUtil.requireWindowInsets
 
 interface WindowInsetsInterface {
 
-    val windowInsetsRequireListener: (Rect) -> Unit
+    fun requireWindowInset(context: Context) {
+        context.requireWindowInsets(windowInsetsRequireListener)
+    }
+
+    private val windowInsetsRequireListener: (Rect) -> Unit
         get() = onSetupRequireWindowInsets()
 
     fun onSetupRequireWindowInsets(): (Rect) -> Unit
