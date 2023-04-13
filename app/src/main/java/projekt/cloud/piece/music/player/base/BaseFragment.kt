501d240c4db869c0c18d4653c1c2dd3810163fca
1552980358 <1552980358@qq.com>
2023-04-13 12:59:31 +0800

    [base/BaseFragment] Update method `onSetupBinding()`
    Signed-off-by: 1552980358 <1552980358@qq.com>

--- app/src/main/java/projekt/cloud/piece/music/player/base/BaseFragment.kt
+++ app/src/main/java/projekt/cloud/piece/music/player/base/BaseFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.annotation.CallSuper
 import androidx.databinding.ViewDataBinding
 import androidx.fragment.app.Fragment
 import androidx.viewbinding.ViewBinding
@@ -24,5 +25,16 @@ abstract class BaseFragment<VB: ViewBinding>: Fragment() {
         }
         return binding.root
     }
+
+    /**
+     * Don't override [onViewCreated],
+     * go and override [onSetupBinding]!
+     **/
+    @CallSuper
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        onSetupBinding(binding, savedInstanceState)
+    }
+
+    protected open fun onSetupBinding(binding: VB, savedInstanceState: Bundle?) = Unit
 
 }
