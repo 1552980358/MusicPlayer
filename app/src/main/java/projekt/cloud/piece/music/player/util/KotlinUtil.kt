36421c360ad4c65c9dc230baf2734145f4a059bb
1552980358 <1552980358@qq.com>
2023-04-16 04:48:48 +0800

    [util/KotlinUtil] Update methods `to()` and `tryTo()`
    Signed-off-by: 1552980358 <1552980358@qq.com>

---
+++ app/src/main/java/projekt/cloud/piece/music/player/util/KotlinUtil.kt
@@ -0,0 +1,12 @@
+package projekt.cloud.piece.music.player.util
+
+
+object KotlinUtil {
+
+    @Suppress("UNCHECKED_CAST")
+    fun <T> Any.to(): T = this as T
+
+    @Suppress("UNCHECKED_CAST")
+    fun <T> Any.tryTo(): T? = this as? T
+
+}
