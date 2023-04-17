a27b712b538bbcec4e5767d05e8d415296d7f729
1552980358 <1552980358@qq.com>
2023-04-17 09:58:14 +0800

    [storage/runtime/AudioMetadataDao] Add method `queryAlbumOfArtist()`
    Signed-off-by: 1552980358 <1552980358@qq.com>

--- app/src/main/java/projekt/cloud/piece/music/player/storage/runtime/dao/AudioMetadataDao.kt
+++ app/src/main/java/projekt/cloud/piece/music/player/storage/runtime/dao/AudioMetadataDao.kt
@@ -2,9 +2,12 @@ package projekt.cloud.piece.music.player.storage.runtime.dao
 
 import androidx.room.Dao
 import androidx.room.Insert
+import androidx.room.MapInfo
 import androidx.room.Query
 import projekt.cloud.piece.music.player.storage.audio.databaseView.AudioMetadata
 import projekt.cloud.piece.music.player.storage.runtime.entity.AudioMetadataEntity
+import projekt.cloud.piece.music.player.storage.runtime.entity.AudioMetadataEntity.AudioMetadataEntityUtil.AUDIO_METADATA_COLUMN_ALBUM
+import projekt.cloud.piece.music.player.storage.runtime.entity.AudioMetadataEntity.AudioMetadataEntityUtil.AUDIO_METADATA_COLUMN_ARTIST
 import projekt.cloud.piece.music.player.storage.runtime.entity.AudioMetadataEntity.AudioMetadataEntityUtil.AUDIO_METADATA_COLUMN_ID
 import projekt.cloud.piece.music.player.storage.runtime.entity.AudioMetadataEntity.AudioMetadataEntityUtil.AUDIO_METADATA_TABLE_NAME
 
@@ -24,5 +27,12 @@ interface AudioMetadataDao {
 
     @Query("SELECT * FROM $AUDIO_METADATA_TABLE_NAME")
     suspend fun query(): List<AudioMetadataEntity>
+
+    @MapInfo(keyColumn = "artist", valueColumn = "album")
+    @Query("SELECT " +
+            "$AUDIO_METADATA_COLUMN_ARTIST AS artist, " +
+            "$AUDIO_METADATA_COLUMN_ALBUM AS album " +
+            "FROM $AUDIO_METADATA_TABLE_NAME")
+    suspend fun queryAlbumOfArtist(): Map<String, List<String>>
 
 }
