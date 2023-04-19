517750323ba6dea88fa217757239d5c4563048ce
1552980358 <1552980358@qq.com>
2023-04-19 20:40:31 +0800

    [base/BaseLayoutCompat] Remove redundant companion object methods
    Signed-off-by: 1552980358 <1552980358@qq.com>

--- app/src/main/java/projekt/cloud/piece/music/player/base/BaseLayoutCompat.kt
+++ app/src/main/java/projekt/cloud/piece/music/player/base/BaseLayoutCompat.kt
@@ -1,57 +1,11 @@
 package projekt.cloud.piece.music.player.base
 
 import androidx.viewbinding.ViewBinding
-import kotlin.reflect.KClass
-import kotlin.reflect.full.primaryConstructor
-import kotlin.reflect.jvm.isAccessible
-import projekt.cloud.piece.music.player.util.ScreenDensity
-import projekt.cloud.piece.music.player.util.ScreenDensity.COMPACT
-import projekt.cloud.piece.music.player.util.ScreenDensity.EXPANDED
-import projekt.cloud.piece.music.player.util.ScreenDensity.MEDIUM
 
 abstract class BaseLayoutCompat<VB: ViewBinding>(private var _binding: VB?) {
 
     protected val binding: VB
         get() = _binding!!
-
-    companion object BaseLayoutCompatUtil {
-
-        fun <VB: ViewBinding, LC: BaseLayoutCompat<VB>> VB.reflectLayoutCompat(
-            layoutCompatClass: KClass<LC>, screenDensity: ScreenDensity
-        ): LC {
-            return getLayoutCompatInstance(
-                layoutCompatClass.getLayoutCompatImplClass(screenDensity)
-            )
-        }
-
-        private fun <LC: BaseLayoutCompat<*>> KClass<LC>.getLayoutCompatImplClass(
-            screenDensity: ScreenDensity
-        ): KClass<out LC> {
-            @Suppress("UNCHECKED_CAST")
-            return Class.forName("${java.name}$${screenDensity.layoutCompat}")
-                .kotlin as KClass<out LC>
-        }
-
-        private const val IMPL_COMPAT = "CompatImpl"
-        private const val IMPL_W600DP = "W600dpImpl"
-        private const val IMPL_W1240DP = "W1240dpImpl"
-
-        private val ScreenDensity.layoutCompat: String
-            get() = when (this) {
-                COMPACT -> IMPL_COMPAT
-                MEDIUM -> IMPL_W600DP
-                EXPANDED -> IMPL_W1240DP
-            }
-
-        private fun <VB: ViewBinding, LC: BaseLayoutCompat<VB>> VB.getLayoutCompatInstance(
-            layoutCompatClass: KClass<out LC>
-        ): LC {
-            return layoutCompatClass.primaryConstructor!!
-                .apply { isAccessible = true }
-                .call(this)
-        }
-
-    }
 
     fun onDestroy() {
         _binding = null
