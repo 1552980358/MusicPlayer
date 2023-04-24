c1ea8179d5503f0faa71440bdc8db7edcc2ba33d
1552980358 <1552980358@qq.com>
2023-04-24 11:00:29 +0800

    [storage/runtime/DatabaseViewDao] Add method `queryAlbum()`
    Signed-off-by: 1552980358 <1552980358@qq.com>

--- app/src/main/java/projekt/cloud/piece/music/player/storage/runtime/dao/DatabaseViewDao.kt
+++ app/src/main/java/projekt/cloud/piece/music/player/storage/runtime/dao/DatabaseViewDao.kt
@@ -2,6 +2,8 @@ package projekt.cloud.piece.music.player.storage.runtime.dao
 
 import androidx.room.Dao
 import androidx.room.Query
+import projekt.cloud.piece.music.player.storage.runtime.databaseView.AlbumView
+import projekt.cloud.piece.music.player.storage.runtime.databaseView.AlbumView.AlbumViewConstant.ALBUM_VIEW_NAME
 import projekt.cloud.piece.music.player.storage.runtime.databaseView.ArtistView
 import projekt.cloud.piece.music.player.storage.runtime.databaseView.ArtistView.AlbumViewConstant.ARTIST_VIEW_NAME
 
@@ -10,5 +12,8 @@ interface DatabaseViewDao {
 
     @Query("SELECT * FROM $ARTIST_VIEW_NAME")
     suspend fun queryArtist(): List<ArtistView>
+
+    @Query("SELECT * FROM $ALBUM_VIEW_NAME")
+    suspend fun queryAlbum(): List<AlbumView>
 
 }
