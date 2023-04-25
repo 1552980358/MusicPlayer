d2f7abc562e0d78c73111fb02ca0dff83fba1406
1552980358 <1552980358@qq.com>
2023-04-25 20:55:25 +0800

    [fragment/mainHost] Fix crash after window size changed
    Signed-off-by: 1552980358 <1552980358@qq.com>

--- app/src/main/java/projekt/cloud/piece/music/player/ui/fragment/mainHost/MainHostFragment.kt
+++ app/src/main/java/projekt/cloud/piece/music/player/ui/fragment/mainHost/MainHostFragment.kt
@@ -1,5 +1,6 @@
 package projekt.cloud.piece.music.player.ui.fragment.mainHost
 
+import android.content.Context
 import android.os.Bundle
 import android.support.v4.media.MediaMetadataCompat
 import android.support.v4.media.session.MediaControllerCompat
@@ -23,14 +24,7 @@ class MainHostFragment: BaseMultiDensityFragment<FragmentMainHostBinding, MainHo
     override val layoutCompatInflater: LayoutCompatInflater<FragmentMainHostBinding, MainHostLayoutCompat>
         get() = LibraryLayoutCompatUtil::inflate
 
-    private val mediaControllerCallback = object: MediaControllerCompat.Callback() {
-        override fun onPlaybackStateChanged(state: PlaybackStateCompat) {
-            layoutCompat.notifyPlaybackStateChanged(requireContext(), state.state)
-        }
-        override fun onMetadataChanged(metadata: MediaMetadataCompat) {
-            layoutCompat.notifyMetadataChanged(requireContext(), metadata)
-        }
-    }
+    private lateinit var mediaControllerCallback: MediaControllerCompat.Callback
 
     override fun onSetupLayoutCompat(layoutCompat: MainHostLayoutCompat, savedInstanceState: Bundle?) {
         val childNavController = binding.fragmentContainerView
@@ -46,7 +40,7 @@ class MainHostFragment: BaseMultiDensityFragment<FragmentMainHostBinding, MainHo
             if (isConnected) {
                 MediaControllerCompat.getMediaController(requireActivity())
                     ?.let { mediaControllerCompat ->
-                        registerCallback(mediaControllerCompat)
+                        registerCallback(layoutCompat, mediaControllerCompat)
                         layoutCompat.setupPlaybackControl(mediaControllerCompat.transportControls)
                         layoutCompat.setupNavigatingToPlayer(mediaControllerCompat, findNavController())
                     }
@@ -54,17 +48,46 @@ class MainHostFragment: BaseMultiDensityFragment<FragmentMainHostBinding, MainHo
         }
     }
 
-    private fun registerCallback(mediaControllerCompat: MediaControllerCompat) {
-        mediaControllerCompat.registerCallback(mediaControllerCallback)
+    private fun registerCallback(layoutCompat: MainHostLayoutCompat, mediaControllerCompat: MediaControllerCompat) {
+        mediaControllerCompat.registerCallback(
+            createMediaControllerCallback(layoutCompat, mediaControllerCompat)
+        )
         if (mediaControllerCompat.playbackState.state != STATE_NONE) {
             layoutCompat.recoverPlaybackBar(requireContext(), mediaControllerCompat)
         }
     }
 
-    override fun onDestroyView() {
+    private fun createMediaControllerCallback(
+        layoutCompat: MainHostLayoutCompat, mediaControllerCompat: MediaControllerCompat
+    ): MediaControllerCompat.Callback {
+        mediaControllerCallback = object: MediaControllerCompat.Callback() {
+            override fun onPlaybackStateChanged(state: PlaybackStateCompat) {
+                unregisterIfDetached(mediaControllerCompat, this) { context ->
+                    layoutCompat.notifyPlaybackStateChanged(context, state.state)
+                }
+            }
+            override fun onMetadataChanged(metadata: MediaMetadataCompat) {
+                unregisterIfDetached(mediaControllerCompat, this) { context ->
+                    layoutCompat.notifyMetadataChanged(context, metadata)
+                }
+            }
+        }
+        return mediaControllerCallback
+    }
+
+    private fun unregisterIfDetached(
+        mediaControllerCompat: MediaControllerCompat, callback: MediaControllerCompat.Callback, block: (Context) -> Unit
+    ) {
+        when (val context = context) {
+            null -> mediaControllerCompat.unregisterCallback(callback)
+            else -> block.invoke(context)
+        }
+    }
+
+    override fun onStop() {
         MediaControllerCompat.getMediaController(requireActivity())
             ?.unregisterCallback(mediaControllerCallback)
-        super.onDestroyView()
+        super.onStop()
     }
 
 }
