62b979db13e8868136f85e0f76291833b32546d1
1552980358 <1552980358@qq.com>
2023-05-02 02:27:31 +0800

    [app/build.gradle.kts] Update `androidx.core:core-splashscreen` => 1.0.1
    Signed-off-by: 1552980358 <1552980358@qq.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -57,7 +57,7 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("androidx.core:core-ktx:1.10.0")
-    implementation("androidx.core:core-splashscreen:1.0.0")
+    implementation("androidx.core:core-splashscreen:1.0.1")
     implementation("androidx.media:media:1.6.0")
     implementation("androidx.navigation:navigation-fragment-ktx:2.5.3")
     implementation("androidx.navigation:navigation-ui-ktx:2.5.3")
