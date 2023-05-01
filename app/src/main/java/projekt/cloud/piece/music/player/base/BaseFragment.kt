3ed37127d7e88e25876ca99fd3a1dde105e2356c
1552980358 <1552980358@qq.com>
2023-05-02 01:58:57 +0800

    [base/BaseFragment] Update back pressed listener
    Signed-off-by: 1552980358 <1552980358@qq.com>

--- app/src/main/java/projekt/cloud/piece/music/player/base/BaseFragment.kt
+++ app/src/main/java/projekt/cloud/piece/music/player/base/BaseFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.OnBackPressedCallback
 import androidx.annotation.CallSuper
 import androidx.databinding.ViewDataBinding
 import androidx.fragment.app.Fragment
@@ -14,11 +15,31 @@ import projekt.cloud.piece.music.player.util.KotlinUtil.tryTo
 
 typealias ViewBindingInflater<VB> = (LayoutInflater, ViewGroup?, Boolean) -> VB
 
+typealias OnBackPressedListener = () -> Boolean
+
 abstract class BaseFragment<VB: ViewBinding>: Fragment() {
 
     protected var binding: VB by viewLifecycleProperty()
 
     protected abstract val viewBindingInflater: ViewBindingInflater<VB>
+
+    protected open val onBackPressed: OnBackPressedListener? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        onBackPressed?.let { onBackPressed ->
+            requireActivity().onBackPressedDispatcher
+                .addCallback(this, object: OnBackPressedCallback(true) {
+                    override fun handleOnBackPressed() {
+                        if (onBackPressed.invoke()) {
+                            isEnabled = false
+                            remove()
+                        }
+                    }
+                })
+        }
+    }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         binding = viewBindingInflater.invoke(layoutInflater, container, false)
