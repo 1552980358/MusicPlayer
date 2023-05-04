ae24168e0ece49eab549ef1562400401360a379e
1552980358 <1552980358@qq.com>
2023-05-05 04:59:16 +0800

    [storage/runtime/ArtistView] Update field `songCount`
    Signed-off-by: 1552980358 <1552980358@qq.com>

--- app/src/main/java/projekt/cloud/piece/music/player/storage/runtime/databaseView/ArtistView.kt
+++ app/src/main/java/projekt/cloud/piece/music/player/storage/runtime/databaseView/ArtistView.kt
@@ -3,19 +3,23 @@ package projekt.cloud.piece.music.player.storage.runtime.databaseView
 import androidx.room.ColumnInfo
 import androidx.room.DatabaseView
 import androidx.room.Ignore
+import projekt.cloud.piece.music.player.storage.runtime.databaseView.ArtistView.AlbumViewConstant
 import projekt.cloud.piece.music.player.storage.runtime.databaseView.ArtistView.AlbumViewConstant.ARTIST_VIEW_COLUMN_DURATION
 import projekt.cloud.piece.music.player.storage.runtime.databaseView.ArtistView.AlbumViewConstant.ARTIST_VIEW_COLUMN_ID
 import projekt.cloud.piece.music.player.storage.runtime.databaseView.ArtistView.AlbumViewConstant.ARTIST_VIEW_COLUMN_NAME
+import projekt.cloud.piece.music.player.storage.runtime.databaseView.ArtistView.AlbumViewConstant.ARTIST_VIEW_COLUMN_SONG_COUNT
 import projekt.cloud.piece.music.player.storage.runtime.databaseView.ArtistView.AlbumViewConstant.ARTIST_VIEW_NAME
 import projekt.cloud.piece.music.player.storage.runtime.entity.AudioMetadataEntity.AudioMetadataEntityUtil.AUDIO_METADATA_COLUMN_ARTIST_NAME
 import projekt.cloud.piece.music.player.storage.runtime.entity.AudioMetadataEntity.AudioMetadataEntityUtil.AUDIO_METADATA_COLUMN_ARTIST
 import projekt.cloud.piece.music.player.storage.runtime.entity.AudioMetadataEntity.AudioMetadataEntityUtil.AUDIO_METADATA_COLUMN_DURATION
+import projekt.cloud.piece.music.player.storage.runtime.entity.AudioMetadataEntity.AudioMetadataEntityUtil.AUDIO_METADATA_COLUMN_ID
 import projekt.cloud.piece.music.player.storage.runtime.entity.AudioMetadataEntity.AudioMetadataEntityUtil.AUDIO_METADATA_TABLE_NAME
 
 @DatabaseView(
     value = "SELECT " +
             "$AUDIO_METADATA_COLUMN_ARTIST AS $ARTIST_VIEW_COLUMN_ID, " +
             "$AUDIO_METADATA_COLUMN_ARTIST_NAME AS $ARTIST_VIEW_COLUMN_NAME, " +
+            "COUNT($AUDIO_METADATA_COLUMN_ID) AS $ARTIST_VIEW_COLUMN_SONG_COUNT, " +
             "SUM($AUDIO_METADATA_COLUMN_DURATION) AS $ARTIST_VIEW_COLUMN_DURATION " +
             "FROM $AUDIO_METADATA_TABLE_NAME " +
             "GROUP BY $AUDIO_METADATA_COLUMN_ARTIST",
@@ -26,6 +30,8 @@ data class ArtistView(
     val id: String,
     @ColumnInfo(ARTIST_VIEW_COLUMN_NAME)
     val title: String,
+    @ColumnInfo(ARTIST_VIEW_COLUMN_SONG_COUNT)
+    val songCount: Int,
     @ColumnInfo(ARTIST_VIEW_COLUMN_DURATION)
     val duration: Long
 ) {
@@ -34,6 +40,7 @@ data class ArtistView(
         const val ARTIST_VIEW_NAME = "artist_view"
         const val ARTIST_VIEW_COLUMN_ID = "id"
         const val ARTIST_VIEW_COLUMN_NAME = "name"
+        const val ARTIST_VIEW_COLUMN_SONG_COUNT = "song_count"
         const val ARTIST_VIEW_COLUMN_DURATION = "duration"
     }
 
