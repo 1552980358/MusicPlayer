670f650a9f32ac5186fd1a47cbe1e72a76e08af2
1552980358 <1552980358@qq.com>
2023-05-05 05:02:24 +0800

    [storage/runtime/AudioMetadataDao] Update method `queryForArtist()`
    Signed-off-by: 1552980358 <1552980358@qq.com>

--- app/src/main/java/projekt/cloud/piece/music/player/storage/runtime/dao/AudioMetadataDao.kt
+++ app/src/main/java/projekt/cloud/piece/music/player/storage/runtime/dao/AudioMetadataDao.kt
@@ -2,11 +2,9 @@ package projekt.cloud.piece.music.player.storage.runtime.dao
 
 import androidx.room.Dao
 import androidx.room.Insert
-import androidx.room.MapInfo
 import androidx.room.Query
 import projekt.cloud.piece.music.player.storage.audio.databaseView.AudioMetadata
 import projekt.cloud.piece.music.player.storage.runtime.entity.AudioMetadataEntity
-import projekt.cloud.piece.music.player.storage.runtime.entity.AudioMetadataEntity.AudioMetadataEntityUtil.AUDIO_METADATA_COLUMN_ALBUM
 import projekt.cloud.piece.music.player.storage.runtime.entity.AudioMetadataEntity.AudioMetadataEntityUtil.AUDIO_METADATA_COLUMN_ARTIST
 import projekt.cloud.piece.music.player.storage.runtime.entity.AudioMetadataEntity.AudioMetadataEntityUtil.AUDIO_METADATA_COLUMN_ID
 import projekt.cloud.piece.music.player.storage.runtime.entity.AudioMetadataEntity.AudioMetadataEntityUtil.AUDIO_METADATA_TABLE_NAME
@@ -22,17 +20,15 @@ interface AudioMetadataDao {
     @Insert
     suspend fun insert(audioMetadataEntity: AudioMetadataEntity)
 
-    @Query("SELECT * FROM $AUDIO_METADATA_TABLE_NAME WHERE $AUDIO_METADATA_COLUMN_ID = :id")
+    @Query("SELECT * FROM $AUDIO_METADATA_TABLE_NAME " +
+            "WHERE $AUDIO_METADATA_COLUMN_ID = :id")
     suspend fun query(id: String): AudioMetadataEntity
 
     @Query("SELECT * FROM $AUDIO_METADATA_TABLE_NAME")
     suspend fun query(): List<AudioMetadataEntity>
 
-    @MapInfo(keyColumn = "artist", valueColumn = "album")
-    @Query("SELECT " +
-            "$AUDIO_METADATA_COLUMN_ARTIST AS artist, " +
-            "$AUDIO_METADATA_COLUMN_ALBUM AS album " +
-            "FROM $AUDIO_METADATA_TABLE_NAME")
-    suspend fun queryAlbumOfArtist(): Map<String, List<String>>
+    @Query("SELECT * FROM $AUDIO_METADATA_TABLE_NAME " +
+            "WHERE $AUDIO_METADATA_COLUMN_ARTIST = :id")
+    suspend fun queryForArtist(id: String): List<AudioMetadataEntity>
 
 }
