818de00b66e1f5a47e729cb2ddfb4c245d657467
1552980358 <1552980358@qq.com>
2023-05-06 05:11:30 +0800

    [storage/runtime/AudioMetadataDao] Update method `queryForAlbum()`
    Signed-off-by: 1552980358 <1552980358@qq.com>

--- app/src/main/java/projekt/cloud/piece/music/player/storage/runtime/dao/AudioMetadataDao.kt
+++ app/src/main/java/projekt/cloud/piece/music/player/storage/runtime/dao/AudioMetadataDao.kt
@@ -5,6 +5,7 @@ import androidx.room.Insert
 import androidx.room.Query
 import projekt.cloud.piece.music.player.storage.audio.databaseView.AudioMetadata
 import projekt.cloud.piece.music.player.storage.runtime.entity.AudioMetadataEntity
+import projekt.cloud.piece.music.player.storage.runtime.entity.AudioMetadataEntity.AudioMetadataEntityUtil.AUDIO_METADATA_COLUMN_ALBUM
 import projekt.cloud.piece.music.player.storage.runtime.entity.AudioMetadataEntity.AudioMetadataEntityUtil.AUDIO_METADATA_COLUMN_ARTIST
 import projekt.cloud.piece.music.player.storage.runtime.entity.AudioMetadataEntity.AudioMetadataEntityUtil.AUDIO_METADATA_COLUMN_ID
 import projekt.cloud.piece.music.player.storage.runtime.entity.AudioMetadataEntity.AudioMetadataEntityUtil.AUDIO_METADATA_TABLE_NAME
@@ -30,5 +31,9 @@ interface AudioMetadataDao {
     @Query("SELECT * FROM $AUDIO_METADATA_TABLE_NAME " +
             "WHERE $AUDIO_METADATA_COLUMN_ARTIST = :id")
     suspend fun queryForArtist(id: String): List<AudioMetadataEntity>
+
+    @Query("SELECT * FROM $AUDIO_METADATA_TABLE_NAME " +
+            "WHERE $AUDIO_METADATA_COLUMN_ALBUM = :id")
+    suspend fun queryForAlbum(id: String): List<AudioMetadataEntity>
 
 }
