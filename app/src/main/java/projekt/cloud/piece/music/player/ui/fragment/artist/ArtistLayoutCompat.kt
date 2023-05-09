4ecbeee3e0565353bc3398632634a5d88f3f9ea3
1552980358 <1552980358@qq.com>
2023-05-09 08:47:30 +0800

    [fragment/artist] Replace collapsed layout into dynamic
    Signed-off-by: 1552980358 <1552980358@qq.com>

--- app/src/main/java/projekt/cloud/piece/music/player/ui/fragment/artist/ArtistLayoutCompat.kt
+++ app/src/main/java/projekt/cloud/piece/music/player/ui/fragment/artist/ArtistLayoutCompat.kt
@@ -3,6 +3,11 @@ package projekt.cloud.piece.music.player.ui.fragment.artist
 import androidx.annotation.UiThread
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.constraintlayout.widget.ConstraintSet
+import androidx.constraintlayout.widget.ConstraintSet.BOTTOM
+import androidx.constraintlayout.widget.ConstraintSet.END
+import androidx.constraintlayout.widget.ConstraintSet.START
+import androidx.constraintlayout.widget.ConstraintSet.TOP
+import androidx.constraintlayout.widget.ConstraintSet.VISIBLE
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
@@ -82,10 +87,8 @@ abstract class ArtistLayoutCompat(
                 fragment,
                 constraintLayout = avatarContainer,
                 isExpandedLiveData = _isExpandedLiveData,
-                expandedConstraintSet = ConstraintSet().apply { clone(avatarContainer) },
-                collapsedConstraintSet = ConstraintSet().apply {
-                    clone(fragment.requireContext(), R.layout.artist_avatar_collapsed)
-                },
+                expandedConstraintSet = createExpandedConstraintSet(avatarContainer),
+                collapsedConstraintSet = createCollapsedConstraintSet(fragment, avatarContainer),
                 /**
                  * Should not directly set as `appBarLayout.totalScrollRange / 2`
                  * because [setupCollapsingAppBar] is called
@@ -95,6 +98,38 @@ abstract class ArtistLayoutCompat(
                  **/
                 transitionLimit = { appBarLayout.totalScrollRange / 2 }
             )
+        }
+
+        private fun createExpandedConstraintSet(container: ConstraintLayout): ConstraintSet {
+            return ConstraintSet().apply { clone(container) }
+        }
+
+        private fun createCollapsedConstraintSet(fragment: Fragment, container: ConstraintLayout): ConstraintSet {
+            return createExpandedConstraintSet(container).apply {
+                // Set size to ShapeableImageView
+                val imageSize = fragment.resources.getDimensionPixelSize(R.dimen.md_spec_size_image_40)
+                constrainHeight(R.id.shapeable_image_view_art, imageSize)
+                constrainWidth(R.id.shapeable_image_view_art, imageSize)
+
+                fragment.resources.getDimensionPixelSize(R.dimen.md_spec_spacing_hor_16).let { marginHorizontal ->
+                    // Set horizontal margin to ShapeableImageView
+                    setMargin(R.id.shapeable_image_view_art, START, marginHorizontal)
+                    setMargin(R.id.shapeable_image_view_art, END, marginHorizontal)
+
+                    // Connect ShapeableImageView and MaterialTextView
+                    connect(R.id.shapeable_image_view_art, END, R.id.material_text_view_name, START, marginHorizontal)
+                }
+
+                // Set horizontal margin to ShapeableImageView
+                fragment.resources.getDimensionPixelSize(R.dimen.md_spec_spacing_ver_8).let { marginVertical ->
+                    setMargin(R.id.shapeable_image_view_art, TOP, marginVertical)
+                    setMargin(R.id.shapeable_image_view_art, BOTTOM, marginVertical)
+                }
+
+                // Show texts
+                setVisibility(R.id.material_text_view_name, VISIBLE)
+                setVisibility(R.id.material_text_view_metadata, VISIBLE)
+            }
         }
 
         override fun setupNavigation(fragment: Fragment) {
