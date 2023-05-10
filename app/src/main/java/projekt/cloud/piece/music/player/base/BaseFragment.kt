29f31437ee0c35942a165b623b06ea205e027121
1552980358 <1552980358@qq.com>
2023-05-10 17:26:04 +0800

    [base/BaseFragment] Update property `screenDensity`
    Signed-off-by: 1552980358 <1552980358@qq.com>

--- app/src/main/java/projekt/cloud/piece/music/player/base/BaseFragment.kt
+++ app/src/main/java/projekt/cloud/piece/music/player/base/BaseFragment.kt
@@ -13,6 +13,7 @@ import projekt.cloud.piece.music.player.base.interfaces.BackPressedInterface
 import projekt.cloud.piece.music.player.base.interfaces.WindowInsetsInterface
 import projekt.cloud.piece.music.player.util.FragmentUtil.viewLifecycleProperty
 import projekt.cloud.piece.music.player.util.KotlinUtil.tryTo
+import projekt.cloud.piece.music.player.util.ScreenDensity.ScreenDensityUtil.screenDensity
 
 typealias ViewBindingInflater<VB> = (LayoutInflater, ViewGroup?, Boolean) -> VB
 
@@ -21,6 +22,8 @@ abstract class BaseFragment<VB: ViewBinding>: Fragment() {
     protected var binding: VB by viewLifecycleProperty()
 
     protected abstract val viewBindingInflater: ViewBindingInflater<VB>
+
+    protected val screenDensity by screenDensity()
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         binding = viewBindingInflater.invoke(layoutInflater, container, false)
