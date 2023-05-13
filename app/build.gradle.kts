227b05d17664428a2e554c08637fda3d11a579e5
1552980358 <1552980358@qq.com>
2023-05-14 05:34:11 +0800

    [app/build.gradle.kts] Update android test dependency `androidx.test:rules`

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -75,4 +75,5 @@ dependencies {
 
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    androidTestImplementation("androidx.test:rules:1.6.0-alpha01")
 }
