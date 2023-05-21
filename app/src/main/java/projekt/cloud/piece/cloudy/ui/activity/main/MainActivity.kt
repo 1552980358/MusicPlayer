6c0b9f069a4fd8f0ef38b06b5cc2d147cdd7c699
1552980358 <1552980358@qq.com>
2023-05-22 00:54:33 +0800

    [activity/MainActivity] Update methods

--- app/src/main/java/projekt/cloud/piece/cloudy/ui/activity/main/MainActivity.kt
+++ app/src/main/java/projekt/cloud/piece/cloudy/ui/activity/main/MainActivity.kt
@@ -1,5 +1,7 @@
 package projekt.cloud.piece.cloudy.ui.activity.main
 
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.WindowCompat
@@ -7,7 +9,17 @@ import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import projekt.cloud.piece.cloudy.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity: AppCompatActivity() {
+
+    companion object MainActivityUtil {
+
+        private val mainActivityClass: Class<MainActivity>
+            get() = MainActivity::class.java
+
+        val Context.mainActivityIntent: Intent
+            get() = Intent(this, mainActivityClass)
+
+    }
 
     private lateinit var navController: NavController
 
