d8c5153bfed55446a809f8693b72355d0e7e0b26
1552980358 <1552980358@qq.com>
2023-05-22 18:53:45 +0800

    [util/LifecycleOwnerUtil] Update comments

--- app/src/main/java/projekt/cloud/piece/cloudy/util/LifecycleOwnerUtil.kt
+++ app/src/main/java/projekt/cloud/piece/cloudy/util/LifecycleOwnerUtil.kt
@@ -1,5 +1,9 @@
 package projekt.cloud.piece.cloudy.util
 
+import android.app.Activity
+import android.app.Service
+import android.content.Context
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.lifecycleScope
 import kotlinx.coroutines.Job
@@ -9,12 +13,29 @@ import projekt.cloud.piece.cloudy.util.CoroutineUtil.main
 
 object LifecycleOwnerUtil {
 
+    /**
+     * [androidx.lifecycle.LifecycleOwner.main]
+     * @param block [SuspendScopedBlock]
+     * @return [kotlinx.coroutines.Job]
+     **/
     fun LifecycleOwner.main(block: SuspendScopedBlock): Job {
         return lifecycleScope.main(block)
     }
+
+    /**
+     * [androidx.lifecycle.LifecycleOwner.default]
+     * @param block [SuspendScopedBlock]
+     * @return [kotlinx.coroutines.Job]
+     **/
     fun LifecycleOwner.default(block: SuspendScopedBlock): Job {
         return lifecycleScope.default(block)
     }
+
+    /**
+     * [androidx.lifecycle.LifecycleOwner.io]
+     * @param block [SuspendScopedBlock]
+     * @return [kotlinx.coroutines.Job]
+     **/
     fun LifecycleOwner.io(block: SuspendScopedBlock): Job {
         return lifecycleScope.io(block)
     }
