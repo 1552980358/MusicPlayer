16525760f8b1b168154a0510d2269c442d19084d
1552980358 <1552980358@qq.com>
2023-05-22 23:52:38 +0800

    [fragment/main_container] Support updating metadata to mini player

--- app/src/main/java/projekt/cloud/piece/cloudy/ui/fragment/main_container/MainContainerFragment.kt
+++ app/src/main/java/projekt/cloud/piece/cloudy/ui/fragment/main_container/MainContainerFragment.kt
@@ -1,12 +1,19 @@
 package projekt.cloud.piece.cloudy.ui.fragment.main_container
 
 import android.os.Bundle
+import androidx.media3.common.MediaItem
+import androidx.media3.common.Player
 import projekt.cloud.piece.cloudy.base.BaseMultiLayoutFragment
 import projekt.cloud.piece.cloudy.base.LayoutAdapterBuilder
 import projekt.cloud.piece.cloudy.databinding.FragmentMainContainerBinding
+import projekt.cloud.piece.cloudy.ui.fragment.main_container.MainContainerViewModel.MainContainerViewModelUtil.mainContainerViewModel
+import projekt.cloud.piece.cloudy.util.MediaControllerHelper
 import projekt.cloud.piece.cloudy.util.ViewBindingInflater
 
-class MainContainerFragment: BaseMultiLayoutFragment<FragmentMainContainerBinding, MainContainerLayoutAdapter>() {
+private typealias BaseMainContainerFragment =
+    BaseMultiLayoutFragment<FragmentMainContainerBinding, MainContainerLayoutAdapter>
+
+class MainContainerFragment: BaseMainContainerFragment(), Player.Listener {
 
     override val viewBindingInflater: ViewBindingInflater<FragmentMainContainerBinding>
         get() = FragmentMainContainerBinding::inflate
@@ -14,9 +21,27 @@ class MainContainerFragment: BaseMultiLayoutFragment<FragmentMainContainerBindin
     override val layoutAdapterBuilder: LayoutAdapterBuilder<FragmentMainContainerBinding, MainContainerLayoutAdapter>
         get() = MainContainerLayoutAdapter.builder
 
+    private val viewModel by mainContainerViewModel()
+
+    private val mediaControllerHelper = MediaControllerHelper()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        mediaControllerHelper.setupWithLifecycleOwner(this)
+    }
+
+    override fun onSetupBinding(binding: FragmentMainContainerBinding, savedInstanceState: Bundle?) {
+        binding.viewModel = viewModel
+        super.onSetupBinding(binding, savedInstanceState)
+    }
+
     override fun onSetupLayoutAdapter(layoutAdapter: MainContainerLayoutAdapter, savedInstanceState: Bundle?) {
         layoutAdapter.setupFragmentContainerViewMargins()
         layoutAdapter.setupNavigation()
+    }
+
+    override fun onMediaItemTransition(mediaItem: MediaItem?, reason: Int) {
+
     }
 
 }
--- app/src/main/java/projekt/cloud/piece/cloudy/ui/fragment/main_container/MainContainerViewModel.kt
+++ app/src/main/java/projekt/cloud/piece/cloudy/ui/fragment/main_container/MainContainerViewModel.kt
@@ -1,9 +1,14 @@
 package projekt.cloud.piece.cloudy.ui.fragment.main_container
 
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.media3.common.MediaItem
+import projekt.cloud.piece.cloudy.storage.audio.view.MetadataView
+import projekt.cloud.piece.cloudy.util.CoroutineUtil.defaultBlocking
+import projekt.cloud.piece.cloudy.util.LifecycleOwnerUtil.main
 
 class MainContainerViewModel: ViewModel() {
 
@@ -21,6 +26,21 @@ class MainContainerViewModel: ViewModel() {
 
     fun updateMiniPlayerHeight(newHeight: Int) {
         _miniPlayerHeight.value = newHeight
+    }
+
+    private var _metadata = MutableLiveData<MetadataView>()
+    val metadata: LiveData<MetadataView>
+        get() = _metadata
+    fun setMetadata(fragment: Fragment, mediaItem: MediaItem) {
+        fragment.main {
+            _metadata.value = getMetadataFromMediaItem(mediaItem)
+        }
+    }
+
+    private suspend fun getMetadataFromMediaItem(mediaItem: MediaItem): MetadataView {
+        return defaultBlocking {
+            MetadataView.fromMediaItem(mediaItem)
+        }
     }
 
 }
