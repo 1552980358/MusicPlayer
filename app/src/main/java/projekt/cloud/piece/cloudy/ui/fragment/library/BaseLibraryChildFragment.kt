6839c641a2080a784dc6e1baf13d1a0c4c579a29
1552980358 <1552980358@qq.com>
2023-06-03 16:13:04 +0800

    [fragment/library] Update animation to library child fragments

--- app/src/main/java/projekt/cloud/piece/cloudy/ui/fragment/library/BaseLibraryChildFragment.kt
+++ app/src/main/java/projekt/cloud/piece/cloudy/ui/fragment/library/BaseLibraryChildFragment.kt
@@ -1,11 +1,17 @@
 package projekt.cloud.piece.cloudy.ui.fragment.library
 
+import android.os.Bundle
 import android.view.View
+import androidx.core.view.doOnPreDraw
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.transition.platform.MaterialFadeThrough
 import projekt.cloud.piece.cloudy.base.BaseLayoutAdapter
 import projekt.cloud.piece.cloudy.base.BaseMultiLayoutFragment
 import projekt.cloud.piece.cloudy.databinding.LibraryChildFragmentBinding
 import projekt.cloud.piece.cloudy.R
+import projekt.cloud.piece.cloudy.base.BaseFragment
+import projekt.cloud.piece.cloudy.ui.fragment.library.album.AlbumLibraryFragment
+import projekt.cloud.piece.cloudy.util.FragmentUtil.getLong
 import projekt.cloud.piece.cloudy.util.ViewBindingInflater
 
 /**
@@ -33,6 +39,44 @@ where LC: BaseLayoutAdapter<LibraryChildFragmentBinding> {
      **/
     private val recyclerView: RecyclerView
         get() = binding.nonnull().recyclerView
+
+    /**
+     * [androidx.fragment.app.Fragment.onCreate]
+     * @param savedInstanceState [android.os.Bundle]
+     **/
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setupTransitions(getLong(R.integer.md_spec_transition_duration_400))
+    }
+
+    /**
+     * [AlbumLibraryFragment.setupTransitions]
+     * @param transitionDuration [Long]
+     **/
+    private fun setupTransitions(transitionDuration: Long) {
+        enterTransition = MaterialFadeThrough().apply {
+            duration = transitionDuration
+        }
+        exitTransition = MaterialFadeThrough().apply {
+            duration = transitionDuration
+        }
+        reenterTransition = MaterialFadeThrough().apply {
+            duration = transitionDuration
+        }
+    }
+
+    /**
+     * [BaseFragment.onSetupBinding]
+     * @param binding [LibraryChildFragmentBinding]
+     * @param savedInstanceState [android.os.Bundle]
+     **/
+    override fun onSetupBinding(binding: LibraryChildFragmentBinding, savedInstanceState: Bundle?) {
+        postponeEnterTransition()
+        super.onSetupBinding(binding, savedInstanceState)
+        binding.recyclerView.doOnPreDraw {
+            startPostponedEnterTransition()
+        }
+    }
 
     /**
      * [BaseLibraryChildFragment.requestItemAtPos]
