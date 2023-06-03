932d7caaac13ddeeeeef6bc7f8feb3b6efa4ed71
1552980358 <1552980358@qq.com>
2023-06-03 16:08:18 +0800

    [util/FragmentUtil] Update method `Fragment.getLong`

--- app/src/main/java/projekt/cloud/piece/cloudy/util/FragmentUtil.kt
+++ app/src/main/java/projekt/cloud/piece/cloudy/util/FragmentUtil.kt
@@ -1,15 +1,32 @@
 package projekt.cloud.piece.cloudy.util
 
+import androidx.annotation.IntegerRes
 import androidx.fragment.app.Fragment
+import projekt.cloud.piece.cloudy.util.ResourcesUtil.getLong
 
 object FragmentUtil {
 
+    /**
+     * [FragmentUtil.findParent]
+     * @typeParam [T] extends [androidx.fragment.app.Fragment]
+     * @return [T]
+     **/
     inline fun <reified T: Fragment> Fragment.findParent(): T? {
         var parent = parentFragment
         while (parent != null && parent !is T) {
             parent = parent.parentFragment
         }
         return parent as? T
+    }
+
+    /**
+     * [FragmentUtil.getLong]
+     * @extends [androidx.fragment.app.Fragment]
+     * @param resId
+     * @return [Long]
+     **/
+    fun Fragment.getLong(@IntegerRes resId: Int): Long {
+        return resources.getLong(resId)
     }
 
 }
